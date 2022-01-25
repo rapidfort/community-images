ce4989e40a46d4fb9755f7b3e87df129dd27b474
Rajeev Thakur <rajeev@rapidfort.com>
2022-01-25 06:35:59 +0000

    working code

--- samples/workload-tags/nginx-multiple-use-cases/api-server/app.py
+++ samples/workload-tags/nginx-multiple-use-cases/api-server/app.py
@@ -3,14 +3,19 @@ import time
 import tornado.ioloop
 import tornado.web
 
+os.environ['TZ'] = 'US/Pacific'
+time.tzset()
+
+_SERVER = os.getenv("SERVER_ID","")
+
 class HealthCheckHandler(tornado.web.RequestHandler):
     def get(self):
         self.set_status(200)
 
+
 class DateHandler(tornado.web.RequestHandler):
     def get(self):
-        output = os.getenv("SERVER_ID","") + ": " + time.ctime()
-        self.write({"output": output})
+        self.write({"server": _SERVER, "date": time.ctime()})
 
 def make_app():
     return tornado.web.Application([

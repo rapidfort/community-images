e4528377e131d8b221ad7bc7832b84d5b957e040
Vinod Gupta <vinodgupta@Vinods-MacBook-Air.local>
2022-02-06 16:20:16 -0800

    add cert manager in setup

--- setup.sh
+++ setup.sh
@@ -18,3 +18,16 @@ kubectl --namespace ci-dev create secret generic rf-regcred --from-file=.dockerc
 
 # remove file
 rm -f get_helm.sh
+
+# add cert manager
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.7.1 \
+  --set installCRDs=true
+
+# create CA issuer
+kubectl apply -f cert_manager.yml

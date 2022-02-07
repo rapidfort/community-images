d0857c47af09f8d6a6ae686caaefba8903f1eb72
Vinod Gupta <vinodgupta@Vinods-MacBook-Air.local>
2022-02-06 20:04:23 -0800

    fix tls testing

--- bitnami/redis/run.sh
+++ bitnami/redis/run.sh
@@ -44,7 +44,7 @@ test1()
     REDIS_PASSWORD=$(kubectl get secret --namespace ${NAMESPACE} ${HELM_RELEASE}-redis -o jsonpath="{.data.redis-password}" | base64 --decode)
 
     #exec into container
-    kubectl -n ${NAMESPACE} exec -it ${HELM_RELEASE}-redis-master-0 -- /bin/bash -c "redis-cli -a ${REDIS_PASSWORD} EVAL \"return {KEYS[1],KEYS[2],ARGV[1],ARGV[2]}\" 2 key1 key2 first second"
+    kubectl -n ${NAMESPACE} exec -it ${HELM_RELEASE}-redis-master-0 -- /bin/bash -c "REDISCLI_AUTH=${REDIS_PASSWORD} redis-cli -h localhost --tls --cert /opt/bitnami/redis/certs/tls.crt --key /opt/bitnami/redis/certs/tls.key --cacert /opt/bitnami/redis/certs/ca.crt  EVAL \"return {KEYS[1],KEYS[2],ARGV[1],ARGV[2]}\" 2 key1 key2 first second"
 
     # sleep for 30 sec
     echo "waiting for 30 sec"

b66270511060434649559d37cbdce76f7d87a3c9
Vinod Gupta <vg@vinodgupta.org>
2022-02-10 22:26:27 -0800

    disable liveness probe due to https://github.com/bitnami/charts/issues/8978

--- bitnami/redis-cluster/run.sh
+++ bitnami/redis-cluster/run.sh
@@ -36,7 +36,8 @@ test_no_tls()
 {
     echo "Testing redis without TLS"
     # Install redis
-    helm install ${HELM_RELEASE}  ${IREPO} --namespace ${NAMESPACE} --set image.tag=${TAG} -f overrides.yml
+    helm install ${HELM_RELEASE}  ${IREPO} --namespace ${NAMESPACE} --set image.tag=${TAG} --set redis.livenessProbe.enabled=false -f overrides.yml
+    # disabled liveness check due to https://github.com/bitnami/charts/issues/8978
 
     # sleep for 2 min
     echo "waiting for 2 min for setup"
@@ -45,7 +46,7 @@ test_no_tls()
     # get Redis passwordk
     REDIS_PASSWORD=$(kubectl get secret --namespace ${NAMESPACE} ${HELM_RELEASE} -o jsonpath="{.data.redis-password}" | base64 --decode)
 
-    #exec into container
+    # exec into container
     kubectl -n ${NAMESPACE} exec -it ${HELM_RELEASE}-0 -- /bin/bash -c "REDISCLI_AUTH=${REDIS_PASSWORD} redis-cli -h ${HELM_RELEASE} -c EVAL \"return {KEYS[1],KEYS[2],ARGV[1],ARGV[2]}\" 2 {user1}:key1 {user1}:key2 first second"
 
     # sleep for 30 sec
@@ -68,12 +69,13 @@ test_tls()
     # Install certs
     kubectl apply -f tls_certs.yml
 
-    #sleep 1 min
+    # sleep 1 min
     echo "waiting for 1 min for setup"
     sleep 1m
 
     # Install redis
-    helm install ${HELM_RELEASE} ${IREPO} --namespace ${NAMESPACE} --set image.tag=${TAG} --set tls.enabled=true --set tls.existingSecret=${HELM_RELEASE}-tls --set tls.certCAFilename=ca.crt --set tls.certFilename=tls.crt --set tls.certKeyFilename=tls.key -f overrides.yml
+    helm install ${HELM_RELEASE} ${IREPO} --namespace ${NAMESPACE} --set image.tag=${TAG} --set tls.enabled=true --set tls.existingSecret=${HELM_RELEASE}-tls --set tls.certCAFilename=ca.crt --set tls.certFilename=tls.crt --set tls.certKeyFilename=tls.key --set redis.livenessProbe.enabled=false -f overrides.yml
+    # disabled liveness check due to https://github.com/bitnami/charts/issues/8978
 
     # sleep for 2 min
     echo "waiting for 2 min for setup"
@@ -82,7 +84,7 @@ test_tls()
     # get Redis passwordk
     REDIS_PASSWORD=$(kubectl get secret --namespace ${NAMESPACE} ${HELM_RELEASE} -o jsonpath="{.data.redis-password}" | base64 --decode)
 
-    #exec into container
+    # exec into container
     kubectl -n ${NAMESPACE} exec -it ${HELM_RELEASE}-0 -- /bin/bash -c "REDISCLI_AUTH=\"${REDIS_PASSWORD}\" redis-cli -h ${HELM_RELEASE} --tls --cert /opt/bitnami/redis/certs/tls.crt --key /opt/bitnami/redis/certs/tls.key --cacert /opt/bitnami/redis/certs/ca.crt -c EVAL \"return {KEYS[1],KEYS[2],ARGV[1],ARGV[2]}\" 2 {user1}:key1 {user1}:key2 first second"
 
     # sleep for 30 sec

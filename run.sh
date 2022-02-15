417de519c78e59522f30600b427b64ae615164d4
Vinod Gupta <vinod.gupta@nutanix.com>
2022-02-14 20:50:58 -0800

    Add postgresql (#3)
    * Add postgresql
    
    * fix ovverrides
    
    * run test.sql
    
    * Fix by releasing pvc
    
    * add postgresql image creation
    
    * fix redis scripts
    
    * fix pvc path
    
    Co-authored-by: Vinod Gupta <vg@vinodgupta.org>

--- run.sh
+++ run.sh
@@ -10,4 +10,9 @@ echo "Creating hardened image for redis-cluster"
 cd ../redis-cluster
 ./run.sh ci-dev 6.2.6-debian-10-r95
 
+
+echo "Creating hardened image for postgresql"
+cd ../postgresql
+./run.sh ci-dev 14.1.0-debian-10-r80
+
 echo "Image generation completed"

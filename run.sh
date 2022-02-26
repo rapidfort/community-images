1f64e124ed7abc0c36a16ad42451f0cd46908999
Vinod Gupta <vinod.gupta@nutanix.com>
2022-02-26 15:13:38 -0800

    Add huggingface pytorch (#5)
    * add hugging face pytorch CPU
    
    * fix permissions
    
    * add python script
    
    * add huggingface pytorch transformer image
    
    * fix run script
    
    * stop using relative paths
    
    * remove docker kill
    
    Co-authored-by: Vinod Gupta <vg@vinodgupta.org>

--- run.sh
+++ run.sh
@@ -2,20 +2,26 @@
 
 set -x
 
+BASEDIR=$(pwd)
+
 echo "Creating hardened image for redis"
-cd bitnami/redis
+cd ${BASEDIR}/bitnami/redis
 ./run.sh ci-dev 6.2.6-debian-10-r103
 
 echo "Creating hardened image for redis-cluster"
-cd ../redis-cluster
+cd ${BASEDIR}/bitnami/redis-cluster
 ./run.sh ci-dev 6.2.6-debian-10-r95
 
 echo "Creating hardened image for postgresql"
-cd ../postgresql
+cd ${BASEDIR}/bitnami/postgresql
 ./run.sh ci-dev 14.1.0-debian-10-r80
 
 echo "Creating hardened image for yugabyte"
-cd ../postgresql
+cd ${BASEDIR}/yugabytedb
 ./run.sh 2.8.1.1-b5
+
+echo "Creating hardened image for huggingface/transformers-pytorch-cpu"
+cd ${BASEDIR}/huggingface/transformers-pytorch-cpu
+./run.sh 4.9.1
 
 echo "Image generation completed"

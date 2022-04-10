d1a8e895500a00e5b808760a150eedb444506e4d
Vinod Gupta <vg@vinodgupta.org>
2022-04-09 20:05:15 -0700

    try another push

--- community_images/postgresql/bitnami/run.sh
+++ community_images/postgresql/bitnami/run.sh
@@ -32,7 +32,7 @@ test()
     # copy test.sql into container
     kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/../../common/tests/test.sql ${HELM_RELEASE}-0:/tmp/test.sql
 
-    # exec into container
+    # run script
     kubectl -n ${NAMESPACE} exec -i ${HELM_RELEASE}-0 -- /bin/bash -c "PGPASSWORD=${POSTGRES_PASSWORD} psql --host ${HELM_RELEASE} -U postgres -d postgres -p 5432 -f /tmp/test.sql"
 
     # bring down helm install

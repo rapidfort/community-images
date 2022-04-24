282e03ad8ea9c9af4719f25fd6be4dd5b274bec6
Vinod Gupta <vg@vinodgupta.org>
2022-04-24 14:25:39 -0700

    add tls prefix

--- community_images/common/tests/redis_cluster_runner.sh
+++ community_images/common/tests/redis_cluster_runner.sh
@@ -4,11 +4,15 @@ set -x
 set -e
 
 REDIS_PASSWORD=$1
-HELM_RELEASE=$2
-REDIS_TEST_FILE=$3
+shift
+HELM_RELEASE=$1
+shift
+REDIS_TEST_FILE=$1
+shift
+TLS_PREFILX=$@
 
 input=${REDIS_TEST_FILE}
 while IFS= read -r line
 do
-    REDISCLI_AUTH=$REDIS_PASSWORD redis-cli -h ${HELM_RELEASE} -c $line
+    REDISCLI_AUTH=$REDIS_PASSWORD redis-cli -h ${HELM_RELEASE} ${TLS_PREFILX} -c $line
 done < "$input"

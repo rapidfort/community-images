ef5e4cf927187d755e250e7d98ebf827cfdba981
Vinod Gupta <vg@vinodgupta.org>
2022-04-29 15:26:34 -0700

    add mysql

---
+++ community_images/mysql/README.md
@@ -0,0 +1,19 @@
+[![Rapidfort](https://assets.website-files.com/6102f7f1589f985b19197b3d/61082629d82d1361e5835b58_rapidfort_logo-new.svg)](https://rapidfort.com) 
+
+[![Msql Build](https://github.com/rapidfort/plenum/actions/workflows/mysql_bitnami.yml/badge.svg)](https://github.com/rapidfort/plenum/actions/workflows/mysql_bitnami.yml)
+# Rapidfort hardened image for Postgresql
+
+## What is MySQL?
+
+> MySQL is a fast, reliable, scalable, and easy to use open source relational database system. Designed to handle mission-critical, heavy-load production applications.
+
+[Overview of MySQL](https://www.mysql.com/)
+
+Trademarks: This software listing is packaged by Rapidfort. The respective trademarks mentioned in the offering are owned by the respective companies, and use of them does not imply any affiliation or endorsement.
+
+<br>
+<br>
+
+### Original Image: https://hub.docker.com/r/bitnami/mysql/ 
+### Sign up now: https://rapidfort.com
+### Github: https://github.com/rapidfort/plenum
---
+++ community_images/mysql/run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -x
+set -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+. ${SCRIPTPATH}/../../common/helpers.sh
+
+
+BASE_TAG=8.0.29-debian-10-r
+INPUT_REGISTRY=docker.io
+INPUT_ACCOUNT=bitnami
+REPOSITORY=mysql
+
+test()
+{
+    local IMAGE_REPOSITORY=$1
+    local TAG=$2
+    local HELM_RELEASE=postgresql-release
+    
+    echo "Testing postgresql"
+
+    # upgrade helm
+    helm repo update
+
+    # Install postgresql
+    helm install ${HELM_RELEASE} ${INPUT_ACCOUNT}/${REPOSITORY} --namespace ${NAMESPACE} --set image.tag=${TAG} --set image.repository=${IMAGE_REPOSITORY} -f ${SCRIPTPATH}/overrides.yml
+
+    # waiting for pod to be ready
+    echo "waiting for pod to be ready"
+    kubectl wait pods ${HELM_RELEASE}-0 -n ${NAMESPACE} --for=condition=ready --timeout=10m
+
+    # get postgresql passwordk
+    # POSTGRES_PASSWORD=$(kubectl get secret --namespace ${NAMESPACE} ${HELM_RELEASE} -o jsonpath="{.data.postgres-password}" | base64 --decode)
+
+    # # copy test.sql into container
+    # kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/../../common/tests/test.sql ${HELM_RELEASE}-0:/tmp/test.sql
+
+    # # run script
+    # kubectl -n ${NAMESPACE} exec -i ${HELM_RELEASE}-0 -- /bin/bash -c "PGPASSWORD=${POSTGRES_PASSWORD} psql --host ${HELM_RELEASE} -U postgres -d postgres -p 5432 -f /tmp/test.sql"
+
+    # # bring down helm install
+    # helm delete ${HELM_RELEASE} --namespace ${NAMESPACE}
+
+    # # delete the PVC associated
+    # kubectl -n ${NAMESPACE} delete pvc --all
+}
+
+build_images ${INPUT_REGISTRY} ${INPUT_ACCOUNT} ${REPOSITORY} ${BASE_TAG} test

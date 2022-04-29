1f746026ef29bfda857499251f3cc8859a2be3c5
Vinod Gupta <codervinod@gmail.com>
2022-04-29 16:20:30 -0700

    Add mysql (#19)
    * add mysql
    
    * move under bitnami
    
    * fix mysql
    
    * separate file for mysql
    
    * fix typo
    
    * fix mysql
    
    * fix mysql
    
    * fix mysql script finally
    
    * fix mysql path
    
    Co-authored-by: Vinod Gupta <vg@vinodgupta.org>

--- community_images/mysql/bitnami/run.sh
+++ community_images/mysql/bitnami/run.sh
@@ -7,37 +7,37 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 . ${SCRIPTPATH}/../../common/helpers.sh
 
 
-BASE_TAG=14.1.0-debian-10-r
+BASE_TAG=8.0.29-debian-10-r
 INPUT_REGISTRY=docker.io
 INPUT_ACCOUNT=bitnami
-REPOSITORY=postgresql
+REPOSITORY=mysql
 
 test()
 {
     local IMAGE_REPOSITORY=$1
     local TAG=$2
-    local HELM_RELEASE=postgresql-release
+    local HELM_RELEASE=mysql-release
     
-    echo "Testing postgresql"
+    echo "Testing mysql"
 
     # upgrade helm
     helm repo update
 
-    # Install postgresql
+    # Install mysql
     helm install ${HELM_RELEASE} ${INPUT_ACCOUNT}/${REPOSITORY} --namespace ${NAMESPACE} --set image.tag=${TAG} --set image.repository=${IMAGE_REPOSITORY} -f ${SCRIPTPATH}/overrides.yml
 
     # waiting for pod to be ready
     echo "waiting for pod to be ready"
     kubectl wait pods ${HELM_RELEASE}-0 -n ${NAMESPACE} --for=condition=ready --timeout=10m
 
-    # get postgresql passwordk
-    POSTGRES_PASSWORD=$(kubectl get secret --namespace ${NAMESPACE} ${HELM_RELEASE} -o jsonpath="{.data.postgres-password}" | base64 --decode)
+    # get mysql password
+    MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace ${NAMESPACE} ${HELM_RELEASE} -o jsonpath="{.data.mysql-root-password}" | base64 --decode)
 
-    # copy test.psql into container
-    kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/../../common/tests/test.psql ${HELM_RELEASE}-0:/tmp/test.psql
+    # copy test.sql into container
+    kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/../../common/tests/test.my_sql ${HELM_RELEASE}-0:/tmp/test.my_sql
 
     # run script
-    kubectl -n ${NAMESPACE} exec -i ${HELM_RELEASE}-0 -- /bin/bash -c "PGPASSWORD=${POSTGRES_PASSWORD} psql --host ${HELM_RELEASE} -U postgres -d postgres -p 5432 -f /tmp/test.psql"
+    kubectl -n ${NAMESPACE} exec -i ${HELM_RELEASE}-0 -- /bin/bash -c "mysql -h ${HELM_RELEASE}.${NAMESPACE}.svc.cluster.local -uroot -p\"$MYSQL_ROOT_PASSWORD\" mysql < /tmp/test.my_sql"
 
     # bring down helm install
     helm delete ${HELM_RELEASE} --namespace ${NAMESPACE}

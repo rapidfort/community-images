45eb15ef51d52f9418a50557b6f060080ba6858c
Vinod Gupta <vg@vinodgupta.org>
2022-05-13 11:25:49 -0700

    add functions

--- community_images/redis/bitnami/functional_test.sh
+++ community_images/redis/bitnami/functional_test.sh
@@ -6,9 +6,23 @@ set -e
 HELM_RELEASE=my-redis
 NAMESPACE=ci-test
 
-helm install ${HELM_RELEASE} bitnami/redis --set image.repository=rapidfort/redis --namespace ${NAMESPACE}
-kubectl wait pods ${HELM_RELEASE}-master-0 -n ${NAMESPACE} --for=condition=ready --timeout=10m
-kubectl -n ${NAMESPACE} get pods
+test()
+{
+    helm install ${HELM_RELEASE} bitnami/redis --set image.repository=rapidfort/redis --namespace ${NAMESPACE}
+    kubectl wait pods ${HELM_RELEASE}-master-0 -n ${NAMESPACE} --for=condition=ready --timeout=10m
+    kubectl -n ${NAMESPACE} get pods
+}
 
-helm delete ${HELM_RELEASE} --namespace ${NAMESPACE}
-kubectl -n ${NAMESPACE} delete pvc --all
+clean()
+{
+    helm delete ${HELM_RELEASE} --namespace ${NAMESPACE}
+    kubectl -n ${NAMESPACE} delete pvc --all
+}
+
+main()
+{
+    test
+    clean
+}
+
+main

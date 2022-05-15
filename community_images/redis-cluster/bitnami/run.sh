e9b60bdf70caae0e22cd023e26932d587deb3d50
Vinod Gupta <codervinod@gmail.com>
2022-05-15 11:29:46 -0700

    Add redis-benchmark for redis-cluster (#28)
    * add redis coverage on redis-cluster
    
    * fix redis benchmark
    
    Co-authored-by: Vinod Gupta <vg@vinodgupta.org>

--- community_images/redis-cluster/bitnami/run.sh
+++ community_images/redis-cluster/bitnami/run.sh
@@ -75,14 +75,20 @@ test()
     # copy redis_cluster_runner.sh into container
     kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/redis_cluster_runner.sh ${HELM_RELEASE}-0:/tmp/redis_cluster_runner.sh
 
-    # run command on cluster
+    # run redis_cluster_runner on cluster
     kubectl -n ${NAMESPACE} exec -i ${HELM_RELEASE}-0 -- /bin/bash -c "/tmp/redis_cluster_runner.sh ${REDIS_PASSWORD} ${HELM_RELEASE} /tmp/test.redis --tls --cert /opt/bitnami/redis/certs/tls.crt --key /opt/bitnami/redis/certs/tls.key --cacert /opt/bitnami/redis/certs/ca.crt"
 
     # copy common_commands.sh into container
     kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/../../common/tests/common_commands.sh ${HELM_RELEASE}-0:/tmp/common_commands.sh
 
-    # run command on cluster
+    # run common_commands on cluster
     kubectl -n ${NAMESPACE} exec -i ${HELM_RELEASE}-0 -- /bin/bash -c "/tmp/common_commands.sh"
+
+    # copy redis_coverage.sh into container
+    kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/../../common/tests/redis_coverage.sh ${HELM_RELEASE}-0:/tmp/redis_coverage.sh
+
+    # run redis_coverage command on cluster
+    kubectl -n ${NAMESPACE} exec -i ${HELM_RELEASE}-0 -- /bin/bash -c "/tmp/redis_coverage.sh"
 
     # bring down helm install
     helm delete ${HELM_RELEASE} --namespace ${NAMESPACE}

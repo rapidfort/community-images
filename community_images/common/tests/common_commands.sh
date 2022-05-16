5c556f203e3a263e356ef26986defcda6355759d
Vinod Gupta <vg@vinodgupta.org>
2022-05-16 11:42:38 -0700

    add basic commands

--- community_images/common/tests/common_commands.sh
+++ community_images/common/tests/common_commands.sh
@@ -6,3 +6,13 @@ set -e
 # add common commands here which should be present in all hardened images
 
 sleep 1
+cp --version
+mkdir --version
+chmod --version
+ls --version
+mv --version
+rm --version
+ln --version
+rmdir --version
+chgrp --version
+chown --version

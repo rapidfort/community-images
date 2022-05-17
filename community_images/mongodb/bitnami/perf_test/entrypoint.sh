c9ec664ae74baba99343feb48a982a2994cf2345
Vinod Gupta <vg@vinodgupta.org>
2022-05-17 16:32:23 -0700

    try script fix

--- community_images/mongodb/bitnami/perf_test/entrypoint.sh
+++ community_images/mongodb/bitnami/perf_test/entrypoint.sh
@@ -4,11 +4,5 @@ set -x
 set -e
 
 java -jar /mongodb-performance-test/latest-version/mongodb-performance-test.jar \
-    -m ${MONGODB_OPERATION} \
-    -o 1000000 \ 
-    -t 100 \ 
-    -db test \ 
-    -c perf \ 
-    -port ${MONGODB_PORT} \
-    -h ${MONGODB_HOST} \
-    -u root -p ${MONGODB_ROOT_PASSWORD} -adb ${MONGODB_AUTHDB}
+    -m "${MONGODB_OPERATION}" -o 1000000 -t 100 -db test -c perf -port "${MONGODB_PORT}" \
+    -h "${MONGODB_HOST}" -u root -p "${MONGODB_ROOT_PASSWORD}" -adb "${MONGODB_AUTHDB}"

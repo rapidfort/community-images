be4af8566914384fd7a9d2ebdb70392e9c56643e
Vinod Gupta <vg@vinodgupta.org>
2022-05-17 16:29:32 -0700

    create entrypoint script

---
+++ community_images/mongodb/bitnami/perf_test/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+set -e
+
+java -jar /mongodb-performance-test/latest-version/mongodb-performance-test.jar \
+    -m ${MONGODB_OPERATION} \
+    -o 1000000 \ 
+    -t 100 \ 
+    -db test \ 
+    -c perf \ 
+    -port ${MONGODB_PORT} \
+    -h ${MONGODB_HOST} \
+    -u root -p ${MONGODB_ROOT_PASSWORD} -adb ${MONGODB_AUTHDB}

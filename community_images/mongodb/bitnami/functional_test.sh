c12bd588be7b0713666b3b1878834540d13d75b2
Vinod Gupta <vg@vinodgupta.org>
2022-05-17 16:44:09 -0700

    improve mongo test

--- community_images/mongodb/bitnami/functional_test.sh
+++ community_images/mongodb/bitnami/functional_test.sh
@@ -15,11 +15,34 @@ test()
     # get mongodb password
     MONGODB_ROOT_PASSWORD=$(kubectl get secret --namespace ${NAMESPACE} ${HELM_RELEASE} -o jsonpath="{.data.mongodb-root-password}" | base64 --decode)
 
-    # create MongoDB client
+    # run MongoDB client INSERT
     kubectl run -n ${NAMESPACE} mongodb-perf \
-        --restart='Never' \
-        --env="MONGODB_OPERATION=INSERT_MANY" \
-        --env="MONGODB_PORT=27017" \
+        --rm -i --restart='Never' \
+        --env="MONGODB_OPERATION=INSERT" \
+        --env="MONGODB_HOST=${HELM_RELEASE}" \
+        --env="MONGODB_ROOT_PASSWORD=${MONGODB_ROOT_PASSWORD}" \
+        --image rapidfort/mongodb-perfomance-test:latest
+
+    # run MongoDB client UPDATE_MANY
+    kubectl run -n ${NAMESPACE} mongodb-perf \
+        --rm -i --restart='Never' \
+        --env="MONGODB_OPERATION=UPDATE_MANY" \
+        --env="MONGODB_HOST=${HELM_RELEASE}" \
+        --env="MONGODB_ROOT_PASSWORD=${MONGODB_ROOT_PASSWORD}" \
+        --image rapidfort/mongodb-perfomance-test:latest
+
+    # run MongoDB client ITERATE_MANY
+    kubectl run -n ${NAMESPACE} mongodb-perf \
+        --rm -i --restart='Never' \
+        --env="MONGODB_OPERATION=ITERATE_MANY" \
+        --env="MONGODB_HOST=${HELM_RELEASE}" \
+        --env="MONGODB_ROOT_PASSWORD=${MONGODB_ROOT_PASSWORD}" \
+        --image rapidfort/mongodb-perfomance-test:latest
+
+    # run MongoDB client DELETE_MANY
+    kubectl run -n ${NAMESPACE} mongodb-perf \
+        --rm -i --restart='Never' \
+        --env="MONGODB_OPERATION=DELETE_MANY" \
         --env="MONGODB_HOST=${HELM_RELEASE}" \
         --env="MONGODB_ROOT_PASSWORD=${MONGODB_ROOT_PASSWORD}" \
         --image rapidfort/mongodb-perfomance-test:latest

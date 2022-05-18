a8a345a5d74e902031de06d78155643ada4e44c0
Vinod Gupta <vg@vinodgupta.org>
2022-05-17 21:08:37 -0700

    add dc test

--- community_images/mongodb/bitnami/functional_test.sh
+++ community_images/mongodb/bitnami/functional_test.sh
@@ -19,6 +19,33 @@ k8s_perf_runner()
         --image rapidfort/mongodb-perfomance-test:latest
 }
 
+run_mongodb_test_op()
+{
+    MONGODB_HOST=$1
+    MONGODB_ROOT_PASSWORD=$2
+    DOCKER_NETWORK=$3
+    OPERATION=$4
+
+    docker run --rm -i --network=${DOCKER_NETWORK} --name mongodb-perf \
+        --rm -i --restart='Never' \
+        -e "MONGODB_OPERATION=${OPERATION}" \
+        -e "MONGODB_HOST=${MONGODB_HOST}" \
+        -e "MONGODB_ROOT_PASSWORD=${MONGODB_ROOT_PASSWORD}" \
+        rapidfort/mongodb-perfomance-test:latest
+}
+
+run_mongodb_test()
+{
+    MONGODB_HOST=$1
+    MONGODB_ROOT_PASSWORD=$2
+    DOCKER_NETWORK=$3
+
+    run_mongodb_test_op "${MONGODB_HOST}" "${MONGODB_ROOT_PASSWORD}" "${DOCKER_NETWORK}" INSERT
+    run_mongodb_test_op "${MONGODB_HOST}" "${MONGODB_ROOT_PASSWORD}" "${DOCKER_NETWORK}" UPDATE_MANY
+    run_mongodb_test_op "${MONGODB_HOST}" "${MONGODB_ROOT_PASSWORD}" "${DOCKER_NETWORK}" ITERATE_MANY
+    run_mongodb_test_op "${MONGODB_HOST}" "${MONGODB_ROOT_PASSWORD}" "${DOCKER_NETWORK}" DELETE_MANY
+}
+
 k8s_test()
 {
     # install mongodb
@@ -44,6 +71,39 @@ k8s_test()
 
     # delete pvc
     kubectl -n ${NAMESPACE} delete pvc --all
+}
+
+docker_compose_test()
+{
+    # update image in docker-compose yml
+    sed "s#@IMAGE#rapidfort/mongodb#g" ${SCRIPTPATH}/docker-compose.yml.base > ${SCRIPTPATH}/docker-compose.yml
+
+    # install postgresql container
+    docker-compose -f ${SCRIPTPATH}/docker-compose.yml up -d
+
+    # sleep for 30 sec
+    sleep 30
+
+    # password
+    MONGODB_ROOT_PASSWORD=password123
+
+    # logs for tracking
+    docker-compose -f ${SCRIPTPATH}/docker-compose.yml logs
+
+    # run pg benchmark container
+    run_mongodb_test mongodb-primary $MONGODB_ROOT_PASSWORD bitnami_default
+
+    # kill docker-compose setup container
+    docker-compose -f ${SCRIPTPATH}/docker-compose.yml down
+
+    # clean up docker file
+    rm -rf ${SCRIPTPATH}/docker-compose.yml
+
+    # prune containers
+    docker image prune -a -f
+
+    # prune volumes
+    docker volume prune -f
 }
 
 main()

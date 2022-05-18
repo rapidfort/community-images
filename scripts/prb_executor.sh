60f8de3e4da2d0e7c2224f54ec9d2f8ac27a6a71
Vinod Gupta <vg@vinodgupta.org>
2022-05-18 13:31:35 -0700

    fix script path

--- scripts/prb_executor.sh
+++ scripts/prb_executor.sh
@@ -4,4 +4,4 @@ set -x
 set -e
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cat ${SCRIPTPATH}/scripts/image_list.txt | parallel "${SCRIPTPATH}/../community_images/{.}/run.sh"
+cat ${SCRIPTPATH}/image_list.txt | parallel "${SCRIPTPATH}/../community_images/{.}/run.sh"

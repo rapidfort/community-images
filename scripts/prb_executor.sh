9432cba32f3b0303e93981a0f6db3ba72e85593f
Vinod Gupta <vg@vinodgupta.org>
2022-05-18 10:46:46 -0700

    integrate main branch

--- scripts/prb_executor.sh
+++ scripts/prb_executor.sh
@@ -3,9 +3,19 @@
 set -x
 set -e
 
-ci_list = (mariadb/bitnami mongodb/bitnami mysql/bitnami postgresql/bitnami redis/bitnami redis-cluster/bitnami)
+ci_list=("mariadb/bitnami" "mongodb/bitnami" "mysql/bitnami" "postgresql/bitnami" "redis/bitnami" "redis-cluster/bitnami")
 
 for i in "${ci_list[@]}"
 do
    echo "${i}"
+done
+
+for i in "${ci_list[@]}"; do
+    ./community_images/"${i}"/run.sh &
+    pids[${i}]=$!
+done
+
+# wait for all pids
+for pid in ${pids[*]}; do
+    wait $pid
 done

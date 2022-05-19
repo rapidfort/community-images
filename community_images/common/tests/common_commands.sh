a6460a526fa2b530b369d92df6bcabb7e6722329
Vinod Gupta <vg@vinodgupta.org>
2022-05-19 13:09:10 -0700

    fix mongodb stub changes

--- community_images/common/tests/common_commands.sh
+++ community_images/common/tests/common_commands.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-# add common commands here which should be present in all hardened images
+# Add common commands here which should be present in all hardened images
 
 sleep 1
 cp --version

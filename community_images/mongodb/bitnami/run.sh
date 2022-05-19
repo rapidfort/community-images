7ac0c4d72b8d1d0480f5b16f993d5a36805fd1c6
Vinod Gupta <vg@vinodgupta.org>
2022-05-19 12:05:38 -0700

    fix client base path

--- community_images/mongodb/bitnami/run.sh
+++ community_images/mongodb/bitnami/run.sh
@@ -50,7 +50,7 @@ test()
     kubectl -n ${NAMESPACE} exec -i ${POD_NAME} -- /bin/bash -c "/tmp/common_commands.sh"
 
     # create MongoDB client
-    envsubst < ${SCRIPTPATH}/client.yml | kubectl -n ${NAMESPACE} apply -f -
+    envsubst < ${SCRIPTPATH}/client.yml.base | kubectl -n ${NAMESPACE} apply -f -
 
     # kubectl run -n ${NAMESPACE} ${HELM_RELEASE}-client \
     #     --restart='Never' \

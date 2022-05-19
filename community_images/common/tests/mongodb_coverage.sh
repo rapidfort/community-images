ce61b606606bcb0f71473511cceb8ae2f299c0ca
Vinod Gupta <codervinod@gmail.com>
2022-05-19 14:45:03 -0700

    Add utils for mongo and mysql, mariadb (#42)
    * add utils in db containers
    
    * use pod name for mongodb
    
    * whitelist few utils
    
    * Trigger Buildn
    
    Co-authored-by: Vinod Gupta <vg@vinodgupta.org>

---
+++ community_images/common/tests/mongodb_coverage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# add common commands here which should be present in all hardened images
+# ref: https://www.mongodb.com/docs/database-tools/
+
+mongodump --version
+mongorestore --version
+bsondump --version
+mongoimport --version
+mongoexport --version
+mongostat --version
+mongotop --version
+mongofiles --version
---
+++ community_images/common/tests/mysql_coverage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# add common commands here which should be present in all hardened images
+# ref: https://dev.mysql.com/doc/refman/8.0/en/programs-client.html
+
+mysql --version
+mysqladmin --version
+mysqlcheck --version
+mysqldump --version
+mysqlimport --version
+# mysqlpump --version # not present on mariadb
+mysqlshow --version
+# mysqlslap --version # doesnt work on mariadb due to char set issue

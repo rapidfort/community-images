ec41169ecbf1805bd60205608ad8cdf822798c6d
Vinod Gupta <vg@vinodgupta.org>
2022-05-18 22:41:14 -0700

    add delete tag

--- community_images/common/delete_tag.sh
+++ community_images/common/delete_tag.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-USERNAME=$0
-PASSWORD=$1
+USERNAME=rapidfortbot
+PASSWORD="figure_out"
 ORGANIZATION="rapidfort"
-IMAGE=$2
-TAG=$3
+IMAGE=$1
+TAG=$2
 
 login_data() {
 cat <<EOF
 {
-  "username": "$USERNAME",
-  "password": "$PASSWORD"
+  "username": "${USERNAME}",
+  "password": "${PASSWORD}"
 }
 EOF
 }
 
 TOKEN=`curl -s -H "Content-Type: application/json" -X POST -d "$(login_data)" "https://hub.docker.com/v2/users/login/" | jq -r .token`
 
-curl "https://hub.docker.com/v2/${ORGANIZATION}/${IMAGE}/manifests/${TAG}" \
+curl "https://hub.docker.com/v2/repositories/${ORGANIZATION}/${IMAGE}/tags/${TAG}/" \
 -X DELETE \
 -H "Authorization: JWT ${TOKEN}"

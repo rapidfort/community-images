0140eab6f3b5eabd467e5619595328b814d57995
Vinod Gupta <codervinod@gmail.com>
2022-05-18 22:02:35 -0700

    Parallel exec (#40)
    * add parallel executor
    
    * integrate main branch
    
    * try gnu parallel
    
    * later
    
    * update image list
    
    * add run script
    
    * use project for docker-compose
    
    * fix script path
    
    * add some messages on scripts
    
    * images to be installed
    
    * add namespace to all docker-compose
    
    * namespace function
    
    * add dyn namespace
    
    * add create and cleanup namespace
    
    * use prb executor
    
    * fix setup namespace
    
    * fix network
    
    * fix quotes
    
    * fix network
    
    * clean up docker network
    
    * fix ft
    
    * remove port export
    
    * remove port exports
    
    * remove hardcoded docker names
    
    * fix script
    
    * increase mysql sleep as it takes around 40 sec to boot up.
    
    * remove sb name
    
    * try mysql fix
    
    * add error handling
    
    * add mysql sleep
    
    Co-authored-by: Vinod Gupta <vg@vinodgupta.org>

--- scripts/cleanup_readme_dh.sh
+++ scripts/cleanup_readme_dh.sh
@@ -1,6 +1,8 @@
 set -x
 set -e
 
+# This script deletes all temp README.md.dockerhub created by update_readme_dh.sh script
+
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 FILE_LIST=$(find ${SCRIPTPATH}/../community_images/ -type f -name "README.md.dockerhub")

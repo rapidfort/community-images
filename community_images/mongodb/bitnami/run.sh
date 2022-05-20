d05da3dee6431b0d7b132f018426fb7b9ebadc77
Vinod Gupta <vinod@rapidfort.com>
2022-05-19 22:00:54 -0700

    fix mongodb

--- community_images/mongodb/bitnami/run.sh
+++ community_images/mongodb/bitnami/run.sh
@@ -4,7 +4,9 @@ set -x
 set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-. ${SCRIPTPATH}/../../common/helpers.sh
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/../../common/helpers.sh
 
 
 BASE_TAG=5.0.8-debian-10-r
@@ -31,72 +33,72 @@ test()
     helm repo update
 
     # Install mongodb
-    helm install ${HELM_RELEASE} ${INPUT_ACCOUNT}/${REPOSITORY} --namespace ${NAMESPACE} --set image.tag=${TAG} --set image.repository=${IMAGE_REPOSITORY} -f ${SCRIPTPATH}/overrides.yml
+    helm install "${HELM_RELEASE}" "${INPUT_ACCOUNT}"/"${REPOSITORY}" --namespace "${NAMESPACE}" --set image.tag="${TAG}" --set image.repository="${IMAGE_REPOSITORY}" -f "${SCRIPTPATH}"/overrides.yml
 
     # waiting for pod to be ready
     echo "waiting for pod to be ready"
-    kubectl wait deployments ${HELM_RELEASE} -n ${NAMESPACE} --for=condition=Available=True --timeout=10m
+    kubectl wait deployments "${HELM_RELEASE}" -n "${NAMESPACE}" --for=condition=Available=True --timeout=10m
 
     # get mongodb password
-    MONGODB_ROOT_PASSWORD=$(kubectl get secret --namespace ${NAMESPACE} ${HELM_RELEASE} -o jsonpath="{.data.mongodb-root-password}" | base64 --decode)
+    MONGODB_ROOT_PASSWORD=$(kubectl get secret --namespace "${NAMESPACE}" "${HELM_RELEASE}" -o jsonpath="{.data.mongodb-root-password}" | base64 --decode)
 
     # get pod name
-    POD_NAME=`kubectl -n ${NAMESPACE} get pods -l app.kubernetes.io/name=mongodb -o jsonpath="{.items[0].metadata.name}"`
+    POD_NAME=$(kubectl -n "${NAMESPACE}" get pods -l app.kubernetes.io/name=mongodb -o jsonpath="{.items[0].metadata.name}")
 
     # copy common_commands.sh into container
-    kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/../../common/tests/common_commands.sh ${POD_NAME}:/tmp/common_commands.sh
+    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/../../common/tests/common_commands.sh "${POD_NAME}":/tmp/common_commands.sh
 
     # run command on cluster
-    kubectl -n ${NAMESPACE} exec -i ${POD_NAME} -- /bin/bash -c "/tmp/common_commands.sh"
+    kubectl -n "${NAMESPACE}" exec -i "${POD_NAME}" -- /bin/bash -c "/tmp/common_commands.sh"
 
     # copy mongodb_coverage.sh into container
-    kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/../../common/tests/mongodb_coverage.sh ${POD_NAME}:/tmp/mongodb_coverage.sh
+    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/../../common/tests/mongodb_coverage.sh "${POD_NAME}":/tmp/mongodb_coverage.sh
 
     # run mongodb_coverage on cluster
-    kubectl -n ${NAMESPACE} exec -i ${POD_NAME} -- /bin/bash -c "/tmp/mongodb_coverage.sh"
+    kubectl -n "${NAMESPACE}" exec -i "${POD_NAME}" -- /bin/bash -c "/tmp/mongodb_coverage.sh"
 
     # create MongoDB client
-    MONGODB_ROOT_PASSWORD=${MONGODB_ROOT_PASSWORD} \
-        IMAGE_REPOSITORY=${IMAGE_REPOSITORY} \
-        TAG=${TAG} envsubst < ${SCRIPTPATH}/client.yml.base | kubectl -n ${NAMESPACE} apply -f -
+    MONGODB_ROOT_PASSWORD="${MONGODB_ROOT_PASSWORD}" \
+        IMAGE_REPOSITORY="${IMAGE_REPOSITORY}" \
+        TAG="${TAG}" envsubst < "${SCRIPTPATH}"/client.yml.base | kubectl -n "${NAMESPACE}" apply -f -
 
     # wait for mongodb client to be ready
-    kubectl wait pods ${HELM_RELEASE}-client -n ${NAMESPACE} --for=condition=ready --timeout=10m
+    kubectl wait pods "${HELM_RELEASE}"-client -n "${NAMESPACE}" --for=condition=ready --timeout=10m
 
     # copy test.mongo into container
-    kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/../../common/tests/test.mongo ${HELM_RELEASE}-client:/tmp/test.mongo
+    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/../../common/tests/test.mongo "${HELM_RELEASE}"-client:/tmp/test.mongo
 
     # run script
-    kubectl -n ${NAMESPACE} exec -i ${HELM_RELEASE}-client \
+    kubectl -n "${NAMESPACE}" exec -i "${HELM_RELEASE}"-client \
         -- /bin/bash -c "mongosh admin --host \"mongodb-release\" \
         --authenticationDatabase admin -u root -p ${MONGODB_ROOT_PASSWORD} --file /tmp/test.mongo"
 
     # delete client container
-    kubectl -n ${NAMESPACE} delete pod ${HELM_RELEASE}-client
+    kubectl -n "${NAMESPACE}" delete pod "${HELM_RELEASE}"-client
 
     # bring down helm install
-    helm delete ${HELM_RELEASE} --namespace ${NAMESPACE}
+    helm delete "${HELM_RELEASE}" --namespace "${NAMESPACE}"
 
     # delete the PVC associated
-    kubectl -n ${NAMESPACE} delete pvc --all
+    kubectl -n "${NAMESPACE}" delete pvc --all
 
     # update image in docker-compose yml
-    sed "s#@IMAGE#${IMAGE_REPOSITORY}:${TAG}#g" ${SCRIPTPATH}/docker-compose.yml.base > ${SCRIPTPATH}/docker-compose.yml
+    sed "s#@IMAGE#${IMAGE_REPOSITORY}:${TAG}#g" "${SCRIPTPATH}"/docker-compose.yml.base > "${SCRIPTPATH}"/docker-compose.yml
 
     # install redis container
-    docker-compose -f ${SCRIPTPATH}/docker-compose.yml -p ${NAMESPACE} up -d
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" up -d
 
     # sleep for 30 sec
     sleep 30
 
     # logs for tracking
-    docker-compose -f ${SCRIPTPATH}/docker-compose.yml -p ${NAMESPACE} logs
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" logs
 
     # kill docker-compose setup container
-    docker-compose -f ${SCRIPTPATH}/docker-compose.yml -p ${NAMESPACE} down
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" down
 
     # clean up docker file
-    rm -rf ${SCRIPTPATH}/docker-compose.yml
+    rm -rf "${SCRIPTPATH}"/docker-compose.yml
 }
 
-build_images ${INPUT_REGISTRY} ${INPUT_ACCOUNT} ${REPOSITORY} ${BASE_TAG} test ${PUBLISH_IMAGE}
+build_images "${INPUT_REGISTRY}" "${INPUT_ACCOUNT}" "${REPOSITORY}" "${BASE_TAG}" test "${PUBLISH_IMAGE}"

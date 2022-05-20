36e84a108ee3a9d604d8f40fb52bdd46b141a420
Vinod Gupta <vinod@rapidfort.com>
2022-05-19 22:02:32 -0700

    make fixes

--- community_images/mongodb/bitnami/perf_test/build_docker.sh
+++ community_images/mongodb/bitnami/perf_test/build_docker.sh
@@ -5,11 +5,11 @@ set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-TAG=`git ls-remote https://github.com/idealo/mongodb-performance-test.git HEAD | awk '{ print $1}'`
+TAG=$(git ls-remote https://github.com/idealo/mongodb-performance-test.git HEAD | awk '{ print $1}')
 
 IMAGE_TAG=rapidfort/mongodb-perfomance-test:"${TAG}"
 
-docker build -t ${IMAGE_TAG} ${SCRIPTPATH}
-docker push ${IMAGE_TAG}
-docker tag ${IMAGE_TAG} rapidfort/mongodb-perfomance-test:latest
+docker build -t "${IMAGE_TAG}" "${SCRIPTPATH}"
+docker push "${IMAGE_TAG}"
+docker tag "${IMAGE_TAG}" rapidfort/mongodb-perfomance-test:latest
 docker push rapidfort/mongodb-perfomance-test:latest

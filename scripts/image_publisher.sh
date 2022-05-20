dce859a7ce24dc601b7c65cdc34bfe502770b816
Vinod Gupta <vinod@rapidfort.com>
2022-05-20 09:37:09 -0700

    dont overhelm minikube

--- scripts/image_publisher.sh
+++ scripts/image_publisher.sh
@@ -8,7 +8,7 @@ set -e
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "Sending parallel request to create images"
-< "${SCRIPTPATH}"/../image.lst parallel -j10 --halt soon,fail=1 "${SCRIPTPATH}/../community_images/{.}/run.sh yes"
+< "${SCRIPTPATH}"/../image.lst parallel -j --halt soon,fail=1 "${SCRIPTPATH}/../community_images/{.}/run.sh yes"
 
 # prune containers
 docker image prune -a -f

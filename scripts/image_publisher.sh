127ea62f0a99751f948cece60bcaec37c6ae7aa8
Vinod Gupta <codervinod@gmail.com>
2022-05-19 22:52:16 -0700

    add shell check (#44)
    * add shell check
    
    Signed-off-by: Vinod Gupta <vinod@rapidfort.com>
    
    * try invoking shell check
    
    * fail shell
    
    * fix shell check issues
    
    * fix mongodb
    
    * make fixes
    
    * mysql changes
    
    Signed-off-by: Vinod Gupta <vinod@rapidfort.com>
    
    * fix postgresql
    
    Signed-off-by: Vinod Gupta <vinod@rapidfort.com>
    
    * fix redis
    
    Signed-off-by: Vinod Gupta <vinod@rapidfort.com>
    
    * fix redis cluster
    
    * fix script lints
    
    Signed-off-by: Vinod Gupta <vinod@rapidfort.com>
    
    Co-authored-by: Vinod Gupta <vinod@rapidfort.com>

--- scripts/image_publisher.sh
+++ scripts/image_publisher.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # This script runs all Pull Request tests in parallel
 
@@ -8,7 +8,7 @@ set -e
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "Sending parallel request to create images"
-cat ${SCRIPTPATH}/../image.lst | parallel -j10 --halt soon,fail=1 "${SCRIPTPATH}/../community_images/{.}/run.sh yes"
+< "${SCRIPTPATH}"/../image.lst parallel -j10 --halt soon,fail=1 "${SCRIPTPATH}/../community_images/{.}/run.sh yes"
 
 # prune containers
 docker image prune -a -f

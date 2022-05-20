5796ecad6941381bf1fa33250014d68988db2678
Vinod Gupta <vinod@rapidfort.com>
2022-05-19 22:16:58 -0700

    fix postgresql
    Signed-off-by: Vinod Gupta <vinod@rapidfort.com>

--- community_images/postgresql/bitnami/functional_test.sh
+++ community_images/postgresql/bitnami/functional_test.sh
@@ -4,76 +4,78 @@ set -x
 set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-. ${SCRIPTPATH}/../../common/helpers.sh
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/../../common/helpers.sh
 
 HELM_RELEASE=rf-postgresql
-NAMESPACE=$(get_namespace_string ${HELM_RELEASE})
+NAMESPACE=$(get_namespace_string "${HELM_RELEASE}")
 
 k8s_test()
 {
     # setup namespace
-    setup_namespace ${NAMESPACE}
+    setup_namespace "${NAMESPACE}"
 
     # install postgres
-    helm install ${HELM_RELEASE} bitnami/postgresql --set image.repository=rapidfort/postgresql --namespace ${NAMESPACE}
+    helm install "${HELM_RELEASE}" bitnami/postgresql --set image.repository=rapidfort/postgresql --namespace "${NAMESPACE}"
     
     # wait for cluster
-    kubectl wait pods ${HELM_RELEASE}-0 -n ${NAMESPACE} --for=condition=ready --timeout=10m
+    kubectl wait pods "${HELM_RELEASE}"-0 -n "${NAMESPACE}" --for=condition=ready --timeout=10m
     
     # dump pods for logging
-    kubectl -n ${NAMESPACE} get pods
+    kubectl -n "${NAMESPACE}" get pods
 
     # get password
-    POSTGRES_PASSWORD=$(kubectl get secret --namespace ${NAMESPACE} ${HELM_RELEASE} -o jsonpath="{.data.postgres-password}" | base64 --decode)
+    POSTGRES_PASSWORD=$(kubectl get secret --namespace "${NAMESPACE}" "${HELM_RELEASE}" -o jsonpath="{.data.postgres-password}" | base64 --decode)
     
     # run postgres benchmark
-    kubectl run ${HELM_RELEASE}-client --rm -i --restart='Never' --namespace ${NAMESPACE} --image rapidfort/postgresql --env="PGPASSWORD=$POSTGRES_PASSWORD" --command -- pgbench --host rf-postgresql -U postgres -d postgres -p 5432 -i -s 50
+    kubectl run "${HELM_RELEASE}"-client --rm -i --restart='Never' --namespace "${NAMESPACE}" --image rapidfort/postgresql --env="PGPASSWORD=$POSTGRES_PASSWORD" --command -- pgbench --host rf-postgresql -U postgres -d postgres -p 5432 -i -s 50
 
     # delte cluster
-    helm delete ${HELM_RELEASE} --namespace ${NAMESPACE}
+    helm delete "${HELM_RELEASE}" --namespace "${NAMESPACE}"
 
     # clean up PVC
-    kubectl -n ${NAMESPACE} delete pvc --all
+    kubectl -n "${NAMESPACE}" delete pvc --all
 
     # clean up namespace
-    cleanup_namespace ${NAMESPACE}
+    cleanup_namespace "${NAMESPACE}"
 }
 
 docker_test()
 {
     # create network
-    docker network create -d bridge ${NAMESPACE}
+    docker network create -d bridge "${NAMESPACE}"
 
     # create docker container
-    docker run --rm -d --network=${NAMESPACE} \
+    docker run --rm -d --network="${NAMESPACE}" \
         -e 'POSTGRES_PASSWORD=PgPwd' \
-        --name ${NAMESPACE} rapidfort/postgresql:latest
+        --name "${NAMESPACE}" rapidfort/postgresql:latest
 
     # sleep for few seconds
     sleep 30
 
     # get docker host ip
-    PG_HOST=`docker inspect ${NAMESPACE} | jq -r ".[].NetworkSettings.Networks[\"${NAMESPACE}\"].IPAddress"`
+    PG_HOST=$(docker inspect "${NAMESPACE}" | jq -r ".[].NetworkSettings.Networks[\"${NAMESPACE}\"].IPAddress")
 
     # run test on docker container
-    docker run --rm --network=${NAMESPACE} \
+    docker run --rm --network="${NAMESPACE}" \
         -i --env="PGPASSWORD=PgPwd" rapidfort/postgresql \
-        -- pgbench --host ${PG_HOST} -U postgres -d postgres -p 5432 -i -s 50
+        -- pgbench --host "${PG_HOST}" -U postgres -d postgres -p 5432 -i -s 50
 
     # clean up docker container
-    docker kill ${NAMESPACE}
+    docker kill "${NAMESPACE}"
 
     # delete network
-    docker network rm ${NAMESPACE}
+    docker network rm "${NAMESPACE}"
 }
 
 docker_compose_test()
 {
     # update image in docker-compose yml
-    sed "s#@IMAGE#rapidfort/postgresql#g" ${SCRIPTPATH}/docker-compose.yml.base > ${SCRIPTPATH}/docker-compose.yml
+    sed "s#@IMAGE#rapidfort/postgresql#g" "${SCRIPTPATH}"/docker-compose.yml.base > "${SCRIPTPATH}"/docker-compose.yml
 
     # install postgresql container
-    docker-compose -f ${SCRIPTPATH}/docker-compose.yml -p ${NAMESPACE} up -d
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" up -d
 
     # sleep for 30 sec
     sleep 30
@@ -82,7 +84,7 @@ docker_compose_test()
     POSTGRES_PASSWORD=my_password
 
     # logs for tracking
-    docker-compose -f ${SCRIPTPATH}/docker-compose.yml -p ${NAMESPACE} logs
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" logs
 
     # run pg benchmark container
     docker run --rm -i --network="${NAMESPACE}_default" \
@@ -90,10 +92,10 @@ docker_compose_test()
         -- pgbench --host postgresql-master -U postgres -d postgres -p 5432 -i -s 50
 
     # kill docker-compose setup container
-    docker-compose -f ${SCRIPTPATH}/docker-compose.yml -p ${NAMESPACE} down
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" down
 
     # clean up docker file
-    rm -rf ${SCRIPTPATH}/docker-compose.yml
+    rm -rf "${SCRIPTPATH}"/docker-compose.yml
 }
 
 main()
--- community_images/postgresql/bitnami/run.sh
+++ community_images/postgresql/bitnami/run.sh
@@ -4,7 +4,9 @@ set -x
 set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-. ${SCRIPTPATH}/../../common/helpers.sh
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/../../common/helpers.sh
 
 
 BASE_TAG=14.3.0-debian-10-r
@@ -31,56 +33,56 @@ test()
     helm repo update
 
     # Install postgresql
-    helm install ${HELM_RELEASE} ${INPUT_ACCOUNT}/${REPOSITORY} --namespace ${NAMESPACE} --set image.tag=${TAG} --set image.repository=${IMAGE_REPOSITORY} -f ${SCRIPTPATH}/overrides.yml
+    helm install "${HELM_RELEASE}" "${INPUT_ACCOUNT}"/"${REPOSITORY}" --namespace "${NAMESPACE}" --set image.tag="${TAG}" --set image.repository="${IMAGE_REPOSITORY}" -f "${SCRIPTPATH}"/overrides.yml
 
     # waiting for pod to be ready
     echo "waiting for pod to be ready"
-    kubectl wait pods ${HELM_RELEASE}-0 -n ${NAMESPACE} --for=condition=ready --timeout=10m
+    kubectl wait pods "${HELM_RELEASE}"-0 -n "${NAMESPACE}" --for=condition=ready --timeout=10m
 
     # get postgresql passwordk
-    POSTGRES_PASSWORD=$(kubectl get secret --namespace ${NAMESPACE} ${HELM_RELEASE} -o jsonpath="{.data.postgres-password}" | base64 --decode)
+    POSTGRES_PASSWORD=$(kubectl get secret --namespace "${NAMESPACE}" "${HELM_RELEASE}" -o jsonpath="{.data.postgres-password}" | base64 --decode)
 
     # copy test.psql into container
-    kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/../../common/tests/test.psql ${HELM_RELEASE}-0:/tmp/test.psql
+    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/../../common/tests/test.psql "${HELM_RELEASE}"-0:/tmp/test.psql
 
     # run script
-    kubectl -n ${NAMESPACE} exec -i ${HELM_RELEASE}-0 -- /bin/bash -c "PGPASSWORD=${POSTGRES_PASSWORD} psql --host ${HELM_RELEASE} -U postgres -d postgres -p 5432 -f /tmp/test.psql"
+    kubectl -n "${NAMESPACE}" exec -i "${HELM_RELEASE}"-0 -- /bin/bash -c "PGPASSWORD=${POSTGRES_PASSWORD} psql --host ${HELM_RELEASE} -U postgres -d postgres -p 5432 -f /tmp/test.psql"
 
     # copy common_commands.sh into container
-    kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/../../common/tests/common_commands.sh ${HELM_RELEASE}-0:/tmp/common_commands.sh
+    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/../../common/tests/common_commands.sh "${HELM_RELEASE}"-0:/tmp/common_commands.sh
 
     # run command on cluster
-    kubectl -n ${NAMESPACE} exec -i ${HELM_RELEASE}-0 -- /bin/bash -c "/tmp/common_commands.sh"
+    kubectl -n "${NAMESPACE}" exec -i "${HELM_RELEASE}"-0 -- /bin/bash -c "/tmp/common_commands.sh"
 
     # copy postgres_coverage.sh into container
-    kubectl -n ${NAMESPACE} cp ${SCRIPTPATH}/../../common/tests/postgres_coverage.sh ${HELM_RELEASE}-0:/tmp/postgres_coverage.sh
+    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/../../common/tests/postgres_coverage.sh "${HELM_RELEASE}"-0:/tmp/postgres_coverage.sh
 
     # run postgres_coverage on cluster
-    kubectl -n ${NAMESPACE} exec -i ${HELM_RELEASE}-0 -- /bin/bash -c "/tmp/postgres_coverage.sh"
+    kubectl -n "${NAMESPACE}" exec -i "${HELM_RELEASE}"-0 -- /bin/bash -c "/tmp/postgres_coverage.sh"
 
     # bring down helm install
-    helm delete ${HELM_RELEASE} --namespace ${NAMESPACE}
+    helm delete "${HELM_RELEASE}" --namespace "${NAMESPACE}"
 
     # delete the PVC associated
-    kubectl -n ${NAMESPACE} delete pvc --all
+    kubectl -n "${NAMESPACE}" delete pvc --all
 
     # update image in docker-compose yml
-    sed "s#@IMAGE#${IMAGE_REPOSITORY}:${TAG}#g" ${SCRIPTPATH}/docker-compose.yml.base > ${SCRIPTPATH}/docker-compose.yml
+    sed "s#@IMAGE#${IMAGE_REPOSITORY}:${TAG}#g" "${SCRIPTPATH}"/docker-compose.yml.base > "${SCRIPTPATH}"/docker-compose.yml
 
     # install redis container
-    docker-compose -f ${SCRIPTPATH}/docker-compose.yml -p ${NAMESPACE} up -d
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" up -d
 
     # sleep for 30 sec
     sleep 30
 
     # logs for tracking
-    docker-compose -f ${SCRIPTPATH}/docker-compose.yml -p ${NAMESPACE} logs
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" logs
 
     # kill docker-compose setup container
-    docker-compose -f ${SCRIPTPATH}/docker-compose.yml -p ${NAMESPACE} down
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" down
 
     # clean up docker file
-    rm -rf ${SCRIPTPATH}/docker-compose.yml
+    rm -rf "${SCRIPTPATH}"/docker-compose.yml
 }
 
-build_images ${INPUT_REGISTRY} ${INPUT_ACCOUNT} ${REPOSITORY} ${BASE_TAG} test ${PUBLISH_IMAGE}
+build_images "${INPUT_REGISTRY}" "${INPUT_ACCOUNT}" "${REPOSITORY}" "${BASE_TAG}" test "${PUBLISH_IMAGE}"

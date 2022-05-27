9be2fa3672fb0aa707520f2cdb95685456454598
Vinod Gupta <codervinod@gmail.com>
2022-05-26 18:01:20 -0700

    Add nginx image (#60)
    * add nginx
    
    * add nginx
    
    * fix pod name
    
    * clean up template
    
    * try running nginx coverage
    
    * dont change user
    
    * make script fix
    
    * use root user
    
    * allow errors
    
    * disable readiness and liveness probe
    
    * trap errors
    
    * add rfignore
    
    * port multi instance test
    
    * fix script path
    
    * fix hardened image
    
    * remove foo
    
    * add nginx opt directory
    
    * use port 8080
    
    * remove junk
    
    * development
    
    * fux run
    
    * test git clone function
    
    * comment out stubbing
    
    * add url and fetch it
    
    * add tls
    
    * fetch https url
    
    * create nginx image
    
    * try using namespace
    
    * add functional test
    
    * fix ft
    
    * fix ssl test
    
    * fix shell check
    
    * add tls cert test and lb test
    
    * create server config
    
    * add ssl and non-tls test
    
    * add load module
    
    * add rfignore
    
    * fix issues
    
    * run coverage script
    
    * fix shell check errors
    
    * add nginx
    
    * Add nginx ingress
    
    * try enabling ingress earlier
    
    * Create tls record
    
    * change to using service account
    
    * suppport more protocols
    
    * add nginx image
    
    Co-authored-by: Vinod Gupta <vinod@rapidfort.com>

---
+++ community_images/nginx/bitnami/functional_test.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+set -x
+set -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/../../common/helpers.sh
+
+REPOSITORY=nginx
+HELM_RELEASE=rf-"${REPOSITORY}"
+NAMESPACE=$(get_namespace_string "${HELM_RELEASE}")
+
+k8s_test()
+{
+    # setup namespace
+    setup_namespace "${NAMESPACE}"
+
+    # install helm
+    helm install "${HELM_RELEASE}" \
+        bitnami/"$REPOSITORY" \
+        --set image.repository=rapidfort/"$REPOSITORY" \
+        --set cloneStaticSiteFromGit.enabled=true \
+        --set cloneStaticSiteFromGit.repository="https://github.com/mdn/beginner-html-site-styled.git" \
+        --set cloneStaticSiteFromGit.branch=master \
+        --set ingress.enabled=true \
+        --set ingress.selfSigned=true \
+        --namespace "${NAMESPACE}"
+
+    # waiting for pod to be ready
+    echo "waiting for pod to be ready"
+    kubectl wait deployments "${HELM_RELEASE}" -n "${NAMESPACE}" --for=condition=Available=True --timeout=10m
+
+    # get pod name
+    # POD_NAME=$(kubectl -n "${NAMESPACE}" get pods -l app.kubernetes.io/name="$REPOSITORY" -o jsonpath="{.items[0].metadata.name}")
+
+    # fetch service url and curl to url
+    URL=$(minikube service "${HELM_RELEASE}" -n "${NAMESPACE}" --url)
+
+    # curl to http url
+    curl -k "${URL}"
+
+    # fetch minikube ip
+    MINIKUBE_IP=$(minikube ip)
+
+    # curl to https url
+    curl https://"${MINIKUBE_IP}":443 -k
+
+    # log pods
+    kubectl -n "${NAMESPACE}" get pods
+    kubectl -n "${NAMESPACE}" get svc
+
+    # delte cluster
+    helm delete "${HELM_RELEASE}" --namespace "${NAMESPACE}"
+
+    # delete pvc
+    kubectl -n "${NAMESPACE}" delete pvc --all
+
+    # clean up namespace
+    cleanup_namespace "${NAMESPACE}"
+}
+
+docker_test()
+{
+    # create network
+    docker network create -d bridge "${NAMESPACE}"
+
+    # create docker container
+    docker run --rm -d --network="${NAMESPACE}" \
+        --name "${NAMESPACE}" rapidfort/"$REPOSITORY":latest
+
+    # sleep for few seconds
+    sleep 30
+
+    # clean up docker container
+    docker kill "${NAMESPACE}"
+
+    # delete network
+    docker network rm "${NAMESPACE}"
+}
+
+docker_compose_test()
+{
+    # update image in docker-compose yml
+    sed "s#@IMAGE#rapidfort/$REPOSITORY#g" "${SCRIPTPATH}"/docker-compose.yml.base > "${SCRIPTPATH}"/docker-compose.yml
+
+    # install postgresql container
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" up -d
+
+    # sleep for 30 sec
+    sleep 30
+
+    # logs for tracking
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" logs
+
+    # kill docker-compose setup container
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" down
+
+    # clean up docker file
+    rm -rf "${SCRIPTPATH}"/docker-compose.yml
+}
+
+main()
+{
+    k8s_test
+    docker_test
+    docker_compose_test
+}
+
+main
---
+++ community_images/nginx/bitnami/run.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# shellcheck disable=SC1091
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/../../common/helpers.sh
+
+
+INPUT_REGISTRY=docker.io
+INPUT_ACCOUNT=bitnami
+REPOSITORY=nginx
+
+if [ "$#" -ne 1 ]; then
+    PUBLISH_IMAGE="no"
+else
+    PUBLISH_IMAGE=$1
+fi
+
+cleanup_certs()
+{
+    rm -rf "${SCRIPTPATH}"/certs
+    mkdir -p "${SCRIPTPATH}"/certs
+}
+
+create_certs()
+{
+    cleanup_certs
+
+    openssl req -newkey rsa:4096 \
+                -x509 \
+                -sha256 \
+                -days 3650 \
+                -nodes \
+                -out "${SCRIPTPATH}"/certs/server.crt \
+                -keyout "${SCRIPTPATH}"/certs/server.key \
+                -subj "/C=SI/ST=Ljubljana/L=Ljubljana/O=Security/OU=IT Department/CN=www.example.com"
+}
+
+test()
+{
+    local IMAGE_REPOSITORY=$1
+    local TAG=$2
+    local NAMESPACE=$3
+    local HELM_RELEASE="$REPOSITORY"-release
+    
+    echo "Testing $REPOSITORY"
+
+    # upgrade helm
+    helm repo update
+
+    # Install helm
+    helm install "${HELM_RELEASE}" "${INPUT_ACCOUNT}"/"${REPOSITORY}" \
+        --namespace "${NAMESPACE}" \
+        --set image.tag="${TAG}" \
+        --set image.repository="${IMAGE_REPOSITORY}" \
+        --set ingress.hostname="${NAMESPACE}" \
+        -f "${SCRIPTPATH}"/overrides.yml
+
+    # waiting for pod to be ready
+    echo "waiting for pod to be ready"
+    kubectl wait deployments "${HELM_RELEASE}" -n "${NAMESPACE}" --for=condition=Available=True --timeout=10m
+
+    # get pod name
+    POD_NAME=$(kubectl -n "${NAMESPACE}" get pods -l app.kubernetes.io/name="$REPOSITORY" -o jsonpath="{.items[0].metadata.name}")
+
+    # fetch service url and curl to url
+    URL=$(minikube service "${HELM_RELEASE}" -n "${NAMESPACE}" --url)
+
+    # curl to http url
+    curl -k "${URL}"
+
+    # fetch minikube ip
+    MINIKUBE_IP=$(minikube ip)
+
+    # curl to https url
+    curl https://"${MINIKUBE_IP}":443 -k
+
+    # copy common_commands.sh into container
+    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/../../common/tests/common_commands.sh "${POD_NAME}":/tmp/common_commands.sh
+
+    # run command on cluster
+    kubectl -n "${NAMESPACE}" exec -i "${POD_NAME}" -- /bin/bash -c "/tmp/common_commands.sh"
+
+    # bring down helm install
+    helm delete "${HELM_RELEASE}" --namespace "${NAMESPACE}"
+
+    # delete the PVC associated
+    kubectl -n "${NAMESPACE}" delete pvc --all
+
+    # create ssl certs
+    create_certs
+
+    # update image in docker-compose yml
+    sed "s#@IMAGE#${IMAGE_REPOSITORY}:${TAG}#g" "${SCRIPTPATH}"/docker-compose.yml.base > "${SCRIPTPATH}"/docker-compose.yml
+
+    # install docker container
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" up -d
+
+    # sleep for 30 sec
+    sleep 30
+
+    # exec into container and run coverage script
+    docker exec -i "${NAMESPACE}"_nginx_1 bash -c /opt/bitnami/scripts/coverage_script.sh
+
+    # find non-tls and tls port
+    NON_TLS_PORT=$(docker inspect "${NAMESPACE}"_nginx_1 | jq -r ".[].NetworkSettings.Ports.\"8080/tcp\"[0].HostPort")
+    TLS_PORT=$(docker inspect "${NAMESPACE}"_nginx_1 | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort")
+    
+    # run curl in loop for different endpoints
+    for i in {1..20};
+    do 
+        echo "$i"
+        curl http://localhost:"${NON_TLS_PORT}"/a
+        curl http://localhost:"${NON_TLS_PORT}"/b
+        curl https://localhost:"${TLS_PORT}"/a -k
+        curl https://localhost:"${TLS_PORT}"/b -k
+    done
+
+    # logs for tracking
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" logs
+
+    # kill docker-compose setup container
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" down
+
+    # clean up docker file
+    rm -rf "${SCRIPTPATH}"/docker-compose.yml
+
+    # clean up certs
+    cleanup_certs
+}
+
+declare -a BASE_TAG_ARRAY=("1.22.0-debian-10-r" "1.21.6-debian-10-r")
+
+build_images "${INPUT_REGISTRY}" "${INPUT_ACCOUNT}" "${REPOSITORY}" test "${PUBLISH_IMAGE}" "${BASE_TAG_ARRAY[@]}"

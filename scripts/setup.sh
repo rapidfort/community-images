f41a33e0d3b538d033a6cb0eabec8569d7cef6ee
Vinod Gupta <vinod@rapidfort.com>
2022-05-26 17:28:06 -0700

    change to using service account

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -21,7 +21,7 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 if [[ "${FUNCTIONAL_TEST_SETUP}" = "no" ]]; then
   # Install rf
   curl  https://frontrow.rapidfort.com/cli/ | bash
-  rflogin "${RF_USERNAME}" "${RF_PASSWORD}"
+  rflogin
 
   # do docker login
   docker login -u "${DOCKERHUB_USERNAME}" -p "${DOCKERHUB_PASSWORD}"

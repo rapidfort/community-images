6c38048117a4c725460d8755c4dcf269da2c64d8
Vinod Gupta <codervinod@gmail.com>
2022-05-31 09:33:24 -0700

    Add exponential backoff (#63)
    * Add exponential backoff
    
    * fix shellcheck

--- community_images/common/helpers.sh
+++ community_images/common/helpers.sh
@@ -195,6 +195,47 @@ function build_images()
     done
 }
 
+# Retries a command a with backoff.
+#
+# The retry count is given by ATTEMPTS (default 5), the
+# initial backoff timeout is given by TIMEOUT in seconds
+# (default 1.)
+#
+# Successive backoffs double the timeout.
+#
+# Beware of set -e killing your whole script!
+function with_backoff {
+  local max_attempts="${ATTEMPTS-5}"
+  local timeout="${TIMEOUT-1}"
+  local attempt=0
+  local exitCode=0
+
+  while [[ "$attempt" < "$max_attempts" ]]
+  do
+    set +e
+    "$@"
+    exitCode="$?"
+    set -e
+
+    if [[ "$exitCode" == 0 ]]
+    then
+      break
+    fi
+
+    echo "Failure! Retrying in $timeout.." 1>&2
+    sleep "$timeout"
+    attempt=$(( attempt + 1 ))
+    timeout=$(( timeout * 2 ))
+  done
+
+  if [[ "$exitCode" != 0 ]]
+  then
+    echo "You've failed me for the last time! ($*)" 1>&2
+  fi
+
+  return "$exitCode"
+}
+
 function finish {
     if [[ -z "$NAMESPACE_TO_CLEANUP" ]]; then
         kubectl get pods --all-namespaces

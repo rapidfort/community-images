f227268352b9c7bcd5168ae66dbd01302e9f43da
Vinod Gupta <vinod@rapidfort.com>
2022-06-01 16:56:00 -0700

    add common commands

--- community_images/envoy/bitnami/run.sh
+++ community_images/envoy/bitnami/run.sh
@@ -42,6 +42,9 @@ test()
     sleep 30
 
     # exec into container and run coverage script
+    docker exec -i "${NAMESPACE}"_envoy_1 bash -c /opt/bitnami/scripts/common_commands.sh
+
+    # exec into container and run coverage script
     docker exec -i "${NAMESPACE}"_envoy_1 bash -c /opt/bitnami/scripts/coverage_script.sh
 
     # find non-tls and tls port

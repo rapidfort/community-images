afc5a9dfbd1c4195246a0ca011a851d5d2b34819
Vinod Gupta <vinod@rapidfort.com>
2022-06-05 17:56:40 -0700

    fix screenshot code

--- report_shots/shots.js
+++ report_shots/shots.js
@@ -3,25 +3,26 @@ const puppeteer = require('puppeteer');
 async function main() {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.setViewport({width: 1200, height: 720});
-  await page.goto('http://frontrow.rapidfort.com/app/login', { waitUntil: 'networkidle0' }); // wait until page load
+  await page.setViewport({width: 3600, height: 2160});
+  await page.goto('https://frontrow.rapidfort.com/app/community/imageinfo/docker.io%2Fbitnami%2Fenvoy/vulns/original', { waitUntil: 'networkidle0' }); // wait until page load
 
-  await page.type('#email', "<enter_username>");
-  console.log("put email");
-  await Promise.all([
-    page.click('#App > div:nth-child(1) > div.auth-page.undefined > div > div > div > div > form > div:nth-child(3) > button'),
-    
-  ]);
-  console.log("typed password");
-  await page.type('#password', "<enter_password>");
-  await Promise.all([
-    page.click('#App > div:nth-child(1) > div.auth-page.undefined > div > div > div > div > form > div:nth-child(3) > button'),
-    page.waitForNavigation({ waitUntil: 'networkidle2' }),
-  ])
-  console.log("reached dashboard");
-  await page.screenshot({ path: 'img/frontrow.png' });
-  await browser.close();
-  console.log("screen shot taken");
+  await page.waitForSelector('#carousel__container');  
+  const metrics = await page.$('#carousel__container');
+  await metrics.screenshot({
+    path: 'img/metrics.png'
+    });
+
+    // #card-statistics-histogram
+  
+  await page.waitForSelector('#card-statistics-histogram');  
+  const cve_details = await page.$('#card-statistics-histogram'); 
+  await cve_details.screenshot({
+    path: 'img/cve.png'
+    });
+
+  await page.close();
+  await browser.close();    
+  console.log("screen shots taken");
 }
 
 main();

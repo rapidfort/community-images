882cde60dd3676df3c37f2bffdc411a515464e1e
Vinod Gupta <vinod@rapidfort.com>
2022-06-16 22:22:02 -0700

    fix shots code

--- report_shots/shots.js
+++ report_shots/shots.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const process = require('process');
+const util = require('util');
 
 async function takeShots(imageSavePath, imageUrl) {
   const browser = await puppeteer.launch({headless: true,

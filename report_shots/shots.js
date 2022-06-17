c51de42d995a98e057bd511d0d254776dfdd626e
Vinod Gupta <vinod@rapidfort.com>
2022-06-16 22:19:49 -0700

    create screen shots

--- report_shots/shots.js
+++ report_shots/shots.js
@@ -1,20 +1,21 @@
 const puppeteer = require('puppeteer');
+const process = require('process');
 
-async function main() {
+async function takeShots(imageSavePath, imageUrl) {
   const browser = await puppeteer.launch({headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
 
   await page.setViewport({width: 3600, height: 2160});
 
-  await page.goto('https://frontrow.rapidfort.com/app/community/imageinfo/docker.io%2Fbitnami%2Fenvoy/vulns/original', { waitUntil: 'networkidle0' }); // wait until page load
+  await page.goto(imageUrl, { waitUntil: 'networkidle0' }); // wait until page load
   await page.emulateMediaFeatures([{
     name: 'prefers-color-scheme', value: 'light' }]);
 
   await page.waitForSelector('#carousel__container');
   const metrics = await page.$('#carousel__container');
   await metrics.screenshot({
-    path: 'img/metrics.png'
+    path: util.format('%s/metrics.png', imageSavePath)
     });
 
     // #card-statistics-histogram
@@ -22,12 +23,19 @@ async function main() {
   await page.waitForSelector('#card-statistics-histogram');
   const cve_details = await page.$('#card-statistics-histogram');
   await cve_details.screenshot({
-    path: 'img/cve.png'
+    path: util.format('%s/cve.png', imageSavePath)
     });
 
   await page.close();
   await browser.close();
   console.log("screen shots taken");
+}
+
+async function main() {
+  imageSavePath = "img";
+  imageUrl = "https://frontrow.rapidfort.com/app/community/imageinfo/docker.io%2Fbitnami%2Fenvoy/vulns/original";
+  await takeShots(imageSavePath, imageUrl);
+  console.log(process.argv[1]);
 }
 
 main();
---
+++ scripts/generate_screenshots.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# this script keeps track of all things which need to be installed on github actions worker VM
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
+
+sudo apt-get install curl
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt-get install nodejs
+
+sudo apt-get install libatk1.0-0 \
+    libatk-bridge2.0-0 libcups2 libxkbcommon-x11-0 \
+    libxcomposite1 libxdamage1 libxfixes3 libxrandr2 \
+    libgbm1 libpango-1.0-0 libcairo2
+
+cd "$SCRIPTPATH"/../report_shots
+npm install
+node shots.js "${SCRIPTPATH}"/../image.lst

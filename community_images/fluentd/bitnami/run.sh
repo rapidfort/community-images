e3bb9b687acf2a8994d1b6bb95985532ea5270c3
Vinod Gupta <vinod@rapidfort.com>
2022-06-25 16:56:57 -0700

    add logs

--- community_images/fluentd/bitnami/run.sh
+++ community_images/fluentd/bitnami/run.sh
@@ -38,12 +38,14 @@ test()
 
     # waiting for pod to be ready
     echo "waiting for pod to be ready"
+    set +e
     kubectl wait pods "${HELM_RELEASE}"-0 -n "${NAMESPACE}" --for=condition=ready --timeout=10m
 
     # wait for daemonsets
     #kubectl rollout status daemonsets "${HELM_RELEASE}" -n "${NAMESPACE}" --timeout=20m
     kubectl -n "${NAMESPACE}" logs -l "app.kubernetes.io/component=aggregator"
     kubectl -n "${NAMESPACE}" logs -l "app.kubernetes.io/component=forwarder"
+    set -e
 
     # get pod name
     POD_NAME="${HELM_RELEASE}"-0

963c0815c650274be97748f7feab4de89bbea9ce
Vinod Gupta <vinod@rapidfort.com>
2022-06-26 12:53:26 -0700

    add scan image list

--- community_images/common/list_repos.sh
+++ community_images/common/list_repos.sh
@@ -2,13 +2,12 @@
 
 # This script deletes a image tag from dockerhub repo
 
-if [[ $# -ne 2 ]]; then
-    echo "Usage:$0 <image> <tag>"
+if [[ $# -ne 1 ]]; then
+    echo "Usage:$0 <org>"
     exit 1
 fi
 
-IMAGE=$1
-TAG=$2
+ORGANIZATION=$1
 
 if [[ -z "$DOCKERHUB_USERNAME" ]]; then
     echo "Must provide DOCKERHUB_USERNAME in env"
@@ -22,7 +21,6 @@ fi
 
 USERNAME=${DOCKERHUB_USERNAME}
 PASSWORD=${DOCKERHUB_PASSWORD}
-ORGANIZATION="rapidfort"
 
 
 login_data() {
@@ -37,10 +35,17 @@ EOF
 get_repos()
 {
     TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "$(login_data)" "https://hub.docker.com/v2/users/login/" | jq -r .token)
+    paginated_repo_list=
+    for p in {1..10} ; do
+        prepo_list=$(curl -s -H "Authorization: JWT ${TOKEN}" \
+        "https://hub.docker.com/v2/repositories/${ORGANIZATION}/?page_size=100&page=$p")
 
-    curl -s -H "Authorization: JWT ${TOKEN}" \
-        "https://hub.docker.com/v2/repositories/${ORGANIZATION}/?page_size=100" | jq -r '.results|.[]|.name'
-    
+        paginated_repo_list+=$(echo "$prepo_list" | jq -r '.results|.[]|.name')
+    done
+
+    echo "$paginated_repo_list" | while read object; do
+        echo "${ORGANIZATION}/$object"
+    done
 }
 
 get_repos

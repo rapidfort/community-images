d149f25e8c7c81652ddfabaf6916c1298968db4a
Vinod Gupta <codervinod@gmail.com>
2022-07-10 15:10:49 -0700

    Add influxdb (#83)
    * Add influxdb
    
    * add inch test
    
    * add influxdb
    
    * add write test
    
    * remove client
    
    * add example
    
    * add inch test
    
    * use token
    
    * run inch test
    
    * fix token
    
    * try bin bash
    
    * add tty
    
    * add v2
    
    * fix query
    
    * add influx functional test
    
    * remove tty
    
    * remove tty2
    
    * add tty back
    
    * add rapidfort account
    
    * fix shellcheck
    
    * add influxdb image
    
    * fix ft

---
+++ community_images/influxdb/bitnami/functional_test.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+set -x
+set -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/../../common/helpers.sh
+
+HELM_RELEASE=rf-influxdb
+NAMESPACE=$(get_namespace_string "${HELM_RELEASE}")
+REPOSITORY=influxdb
+IMAGE_REPOSITORY="$RAPIDFORT_ACCOUNT"/"$REPOSITORY"
+
+k8s_test()
+{
+    # setup namespace
+    setup_namespace "${NAMESPACE}"
+
+    # install helm
+    with_backoff helm install "${HELM_RELEASE}" bitnami/"$REPOSITORY" --set image.repository="$IMAGE_REPOSITORY" --namespace "${NAMESPACE}"
+    report_pulls "${IMAGE_REPOSITORY}"
+
+    # wait for pods
+    kubectl wait deployments "${HELM_RELEASE}" -n "${NAMESPACE}" --for=condition=Available=True --timeout=10m
+
+    # get pod name
+    POD_NAME=$(kubectl -n "${NAMESPACE}" get pods -l app.kubernetes.io/name="$REPOSITORY" -o jsonpath="{.items[0].metadata.name}")
+
+    # get influxdb token
+    INFLUXDB_TOKEN=$(kubectl get secret --namespace "${NAMESPACE}" "${HELM_RELEASE}" -o jsonpath="{.data.admin-user-token}" | base64 --decode)
+
+    # copy tests into container
+    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/tests/example.csv "${POD_NAME}":/tmp/example.csv
+    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/tests/query.flux "${POD_NAME}":/tmp/query.flux
+
+    # write data to db
+    kubectl -n "${NAMESPACE}" exec -it "${POD_NAME}" -- /bin/bash -c "influx write -t $INFLUXDB_TOKEN -b primary --org-id primary -f /tmp/example.csv"
+
+    # run query on db
+    kubectl -n "${NAMESPACE}" exec -i "${POD_NAME}" -- influx query -t "$INFLUXDB_TOKEN" --org primary -f /tmp/query.flux
+
+    # log pods
+    kubectl -n "${NAMESPACE}" get pods
+    kubectl -n "${NAMESPACE}" get svc
+
+    # delte cluster
+    helm delete "${HELM_RELEASE}" --namespace "${NAMESPACE}"
+
+    # delete pvc
+    kubectl -n "${NAMESPACE}" delete pvc --all
+
+    # clean up namespace
+    cleanup_namespace "${NAMESPACE}"
+}
+
+docker_test()
+{
+    # create network
+    docker network create -d bridge "${NAMESPACE}"
+
+    # create docker container
+    docker run --rm -d --network="${NAMESPACE}" \
+        -e INFLUXDB_ADMIN_USER_PASSWORD="bitnami123" \
+        -e INFLUXDB_ADMIN_USER_TOKEN="admintoken123" \
+        -e INFLUXDB_USER="my_user" \
+        -e INFLUXDB_USER_PASSWORD="my_password" \
+        -e INFLUXDB_DB="my_database" \
+        --name "${NAMESPACE}" "$IMAGE_REPOSITORY":latest
+    report_pulls "${IMAGE_REPOSITORY}"
+
+    # sleep for few seconds
+    sleep 30
+
+    # clean up docker container
+    docker kill "${NAMESPACE}"
+
+    # delete network
+    docker network rm "${NAMESPACE}"
+}
+
+docker_compose_test()
+{
+    # update image in docker-compose yml
+    sed "s#@IMAGE#$IMAGE_REPOSITORY#g" "${SCRIPTPATH}"/docker-compose.yml.base > "${SCRIPTPATH}"/docker-compose.yml
+
+    # install postgresql container
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" up -d
+    report_pulls "${IMAGE_REPOSITORY}"
+
+    # sleep for 30 sec
+    sleep 30
+
+    # logs for tracking
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" logs
+
+    # kill docker-compose setup container
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" down
+
+    # clean up docker file
+    rm -rf "${SCRIPTPATH}"/docker-compose.yml
+}
+
+main()
+{
+    k8s_test
+    docker_test
+    docker_compose_test
+}
+
+main
--- community_images/mongodb/bitnami/perf_test/build_docker.sh
+++ community_images/mongodb/bitnami/perf_test/build_docker.sh
@@ -4,12 +4,13 @@ set -x
 set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+RAPIDFORT_ACCOUNT="${RAPIDFORT_ACCOUNT:-rapidfort}"
 
 TAG=$(git ls-remote https://github.com/idealo/mongodb-performance-test.git HEAD | awk '{ print $1}')
 
-IMAGE_TAG=rapidfort/mongodb-perfomance-test:"${TAG}"
+IMAGE_TAG="$RAPIDFORT_ACCOUNT"/mongodb-perfomance-test:"${TAG}"
 
 docker build -t "${IMAGE_TAG}" "${SCRIPTPATH}"
 docker push "${IMAGE_TAG}"
-docker tag "${IMAGE_TAG}" rapidfort/mongodb-perfomance-test:latest
-docker push rapidfort/mongodb-perfomance-test:latest
+docker tag "${IMAGE_TAG}" "$RAPIDFORT_ACCOUNT"/mongodb-perfomance-test:latest
+docker push "$RAPIDFORT_ACCOUNT"/mongodb-perfomance-test:latest

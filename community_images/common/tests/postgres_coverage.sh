71d13f6bd1735335b41a08576d80969780c97814
Vinod Gupta <vinod@rapidfort.com>
2022-07-13 13:12:04 -0700

    make some commands optional

--- community_images/common/tests/postgres_coverage.sh
+++ community_images/common/tests/postgres_coverage.sh
@@ -10,8 +10,15 @@ createdb --version
 createuser --version
 dropdb --version
 dropuser --version
+if command -v ecpg --version &> /dev/null
+then
+    ecpg --version
+fi
 ecpg --version
-# pg_amcheck --version
+if command -v pg_amcheck --version &> /dev/null
+then
+    pg_amcheck --version
+fi
 pg_basebackup --version
 pgbench --version
 pg_config --version
@@ -21,7 +28,10 @@ pg_isready --version
 pg_receivewal --version
 pg_recvlogical --version
 pg_restore --version
-# pg_verifybackup --version
+if command -v pg_verifybackup --version &> /dev/null
+then
+    pg_verifybackup --version
+fi
 psql --version
 reindexdb --version
 vacuumdb --version

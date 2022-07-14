2eccaab319c33889911889cb34422ca3f241d82a
Ankit Kumar <ankit@rapidfort.com>
2022-07-13 22:37:01 -0700

    remove commands.sh

--- community_images/nats/bitnami/commands.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-    GO111MODULE=off go get github.com/nats-io/nats.go
-    cd "$GOPATH"/src/github.com/nats-io/nats.go/examples/nats-pub && go install && cd || exit
-    cd "$GOPATH"/src/github.com/nats-io/nats.go/examples/nats-echo && go install && cd || exit
-    nats-echo -s nats://nats_client:uzlbJ8FN5p@rf-nats.rf-nats-2adfc4b399.svc.cluster.local:4222 SomeSubject &
-    nats-pub -s nats://nats_client:uzlbJ8FN5p@rf-nats.rf-nats-2adfc4b399.svc.cluster.local:4222 -reply Hi SomeSubject 'Hi everyone'

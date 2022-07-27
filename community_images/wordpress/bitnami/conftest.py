bf58a80d73e8b6d0d5bf43c1454ede4bea1c23a5
Ankit Kumar <ankit@rapidfort.com>
2022-07-27 23:37:07 +0530

    fix pylint issues

--- community_images/wordpress/bitnami/conftest.py
+++ community_images/wordpress/bitnami/conftest.py
@@ -1,15 +1,18 @@
+"""The conftest file for running selenium test."""
 # conftest.py
-import pytest
+import pytest #pylint: disable=import-error
 
 def pytest_addoption(parser):
+    """The function to add options"""
     parser.addoption("--ip_address", action="store", help="ip address of wordpress")
     parser.addoption("--port", action="store", help="port for wordpress container")
 
 @pytest.fixture
 def params(request):
-    params = {}
-    params['ip_address'] = request.config.getoption('--ip_address')
-    params['port'] = request.config.getoption('--port')
-    if params['ip_address'] is None or params['port'] is None:
+    """the params"""
+    config_params = {}
+    config_params['ip_address'] = request.config.getoption('--ip_address')
+    config_params['port'] = request.config.getoption('--port')
+    if config_params['ip_address'] is None or config_params['port'] is None:
         pytest.skip()
-    return params
+    return config_params

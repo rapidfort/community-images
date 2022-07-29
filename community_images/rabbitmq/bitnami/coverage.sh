1da1385d5b23b14041016ce13e51d68339a1546f
Ankit Kumar <ankit@rapidfort.com>
2022-07-29 20:01:55 +0530

    update coverage run rabbitmq

--- community_images/rabbitmq/bitnami/coverage.sh
+++ community_images/rabbitmq/bitnami/coverage.sh
@@ -17,24 +17,24 @@ function test_rabbitmq() {
     pip install pika
     python3 /tmp/publish.py --rabbitmq-server=$RABBITMQ_SERVER" > "$SCRIPTPATH"/publish_commands.sh
 
-    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/publish.py "${CHROME_POD}":/tmp/publish.py
+    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/publish.py "${PUBLISHER_POD_NAME}":/tmp/publish.py
     chmod +x "$SCRIPTPATH"/publish_commands.sh
     kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/publish_commands.sh "${PUBLISHER_POD_NAME}":/tmp/publish_commands.sh
 
     kubectl -n "${NAMESPACE}" exec -i "${POD_NAME}" -- bash -c "/tmp/publish_commands.sh"
 
     # consumer specific
-    PUBLISHER_POD_NAME="publisher"
-    kubectl run "${PUBLISHER_POD_NAME}" --restart='Never' --image bitnami/python --namespace "${NAMESPACE}" --command -- sleep infinity
+    CONSUMER_POD_NAME="consumer"
+    kubectl run "${CONSUMER_POD_NAME}" --restart='Never' --image bitnami/python --namespace "${NAMESPACE}" --command -- sleep infinity
     # wait for nats client to come up
-    kubectl wait pods "${PUBLISHER_POD_NAME}" -n "${NAMESPACE}" --for=condition=ready --timeout=10m
+    kubectl wait pods "${CONSUMER_POD_NAME}" -n "${NAMESPACE}" --for=condition=ready --timeout=10m
     echo "#!/bin/bash
     pip install pika
     python3 /tmp/consume.py --rabbitmq-server=$RABBITMQ_SERVER" > "$SCRIPTPATH"/consume_commands.sh
 
-    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/consume.py "${CHROME_POD}":/tmp/consume.py
+    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/consume.py "${CONSUMER_POD_NAME}":/tmp/consume.py
     chmod +x "$SCRIPTPATH"/consume_commands.sh
-    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/consume_commands.sh "${PUBLISHER_POD_NAME}":/tmp/consume_commands.sh
+    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/consume_commands.sh "${CONSUMER_POD_NAME}":/tmp/consume_commands.sh
 
     kubectl -n "${NAMESPACE}" exec -i "${POD_NAME}" -- bash -c "/tmp/consume_commands.sh"
 

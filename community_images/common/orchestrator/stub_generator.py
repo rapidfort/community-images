c1b9b0c5dfdb05f8d59188089f49429b659387d3
Vinod Gupta <vinod@rapidfort.com>
2022-08-04 10:45:02 -0700

    Framework rewrite 9: Add rapidfort banner and report pulls

--- community_images/common/orchestrator/stub_generator.py
+++ community_images/common/orchestrator/stub_generator.py
@@ -1,8 +1,11 @@
 """ Generates stub """
 
 import logging
+import os
+import shutil
 import subprocess
-
+import tempfile
+from consts import Consts
 
 class StubGenerator:
     """ Stub generation command handler """
@@ -23,11 +26,16 @@ class StubGenerator:
             input_tag_details = tag_mapping.input_tag_details
             output_tag_details = tag_mapping.output_tag_details
 
-            # pull docker image
-            self.docker_client.images.pull(
-                repository=input_tag_details.full_repo_path,
-                tag=input_tag_details.tag
-            )
+            if tag_mapping.input_tag_details.account == Consts.BITNAMI:
+                # create image with RapidFort banner
+                self._add_rf_banner(tag_mapping.input_tag_details)
+            else:
+                # pull docker image
+                self.docker_client.images.pull(
+                    repository=input_tag_details.full_repo_path,
+                    tag=input_tag_details.tag
+                )
+
             subprocess.check_output(["rfstub", input_tag_details.full_tag])
 
             # tag input stubbed image to output stubbed image
@@ -40,3 +48,26 @@ class StubGenerator:
                 output_tag_details.full_repo_path,
                 output_tag_details.stub_tag)
             logging.info(f"docker client push result: {result}")
+
+    def _add_rf_banner(self, tag_details):
+        """ Add RapidFort banner in bitnami images """
+        logging.info(f"Adding rapidfort banner for image: {tag_details}")
+
+        with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as tmpdirname:
+            script_dir = os.path.abspath(os.path.dirname( __file__ ))
+
+            # copy over libbitnami.sh file
+            src = os.path.join(script_dir, "../libbitnami.sh")
+            dst = os.path.join(tmpdirname, "libbitnami.sh")
+            shutil.copyfile(src, dst)
+
+            # create docker file
+            file_path = os.path.join(tmpdirname, "Dockerfile")
+            with open(file_path, 'w', encoding="UTF-8") as dckr_fp:
+                dckr_fp.write(f"FROM {tag_details.repo_path}:{tag_details.tag}\n")
+                dckr_fp.write("ADD libbitnami.sh /opt/bitnami/scripts/libbitnami.sh\n")
+
+            # run docker build
+            image = self.docker_client.build(path=tmpdirname)
+            result = image.tag(tag_details.full_tag)
+            logging.info(f"rf banner image tag:[{tag_details.full_stub_tag}] success={result}")

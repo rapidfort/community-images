e21af98aa6367402f1be8c9d3b03c4bac68b766a
Ankit Kumar <ankit@rapidfort.com>
2022-08-05 21:02:13 +0530

    update

--- community_images/wordpress/bitnami/wordpress_selenium_test.py
+++ community_images/wordpress/bitnami/wordpress_selenium_test.py
@@ -26,6 +26,7 @@ class TestWordpresstest1():
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         self.driver = webdriver.Chrome(options=chrome_options)  # pylint: disable=attribute-defined-outside-init
+        self.driver.implicitly_wait(10)
 
     def teardown_method(self, method):  # pylint: disable=unused-argument
         """teardown method."""

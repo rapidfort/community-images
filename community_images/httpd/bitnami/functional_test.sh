ebf504e530ed38df618ff315cdda3e8f360717b2
Anmol Virdi <67299745+AnmolVirdi@users.noreply.github.com>
2022-08-11 00:45:53 +0530

    Bitnami Httpd image (#103)
    * Check1
    
    * check2
    
    * added base tag array
    
    * added base tag array
    
    * Made changes in latest_tags.py
    
    * Tag issue resolved
    
    * Made changes in functional_test.sh
    
    * Setting up coverage_script.sh
    
    * Adding ab test to functional_test.sh
    
    * Adding ab test to functional_test.sh
    
    * Adding ab test to functional_test.sh
    
    * Adding ab test to run.sh
    
    * Adding changes to run.sh
    
    * script errors fixed
    
    * Adding changes to run.sh
    
    * check run.sh
    
    * configuring run.sh
    
    * some changes in run.sh
    
    * port mapping issue sorted in run.sh
    
    * setting up coverage_script.sh
    
    * checkpoint till coverage scripts
    
    * Coverage scripts and hardening process done
    
    * Updated helpers.sh, run.sh
    
    * Updated helpers.sh, run.sh
    
    * Adding changes to setup.sh
    
    * Adding changes to latest_tag.py
    
    * Added CI
    
    * Modified functional_test.sh
    
    Co-authored-by: Vinod Gupta <codervinod@gmail.com>

---
+++ community_images/httpd/bitnami/functional_test.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+set -x
+set -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/../../common/helpers.sh
+
+HELM_RELEASE=apache-release
+NAMESPACE=$(get_namespace_string "${HELM_RELEASE}")
+REPOSITORY=apache
+IMAGE_REPOSITORY=rapidfort/"$REPOSITORY"
+
+k8s_test()
+{
+    # setup namespace
+    setup_namespace "${NAMESPACE}"
+
+    # install helm
+    with_backoff helm install "${HELM_RELEASE}" bitnami/"$REPOSITORY" --set image.repository="$IMAGE_REPOSITORY" --namespace "${NAMESPACE}"
+    report_pulls "${IMAGE_REPOSITORY}"
+
+    # wait for pods
+    kubectl wait pods "${HELM_RELEASE}"-0 -n "${NAMESPACE}" --for=condition=ready --timeout=10m
+
+    # log pods
+    kubectl -n "${NAMESPACE}" get pods
+    kubectl -n "${NAMESPACE}" get svc
+
+    # delte cluster
+    helm delete "${HELM_RELEASE}" --namespace "${NAMESPACE}"
+
+    # delete pvc
+    kubectl -n "${NAMESPACE}" delete pvc --all
+
+    # clean up namespace
+    cleanup_namespace "${NAMESPACE}"
+}
+
+docker_test()
+{
+    # create network
+    docker network create -d bridge "${NAMESPACE}"
+
+    # create docker container
+    docker run --rm -d --network="${NAMESPACE}" \
+        --name "${NAMESPACE}" "$IMAGE_REPOSITORY":latest
+    report_pulls "${IMAGE_REPOSITORY}"
+
+    # sleep for few seconds
+    sleep 30
+
+    # get docker host ip
+    HTTPD_HOST=$(docker inspect "${NAMESPACE}" | jq -r ".[].NetworkSettings.Networks[\"${NAMESPACE}\"].IPAddress")
+
+    # Install Apache benchmark testing tool
+    sudo apt-get install apache2-utils
+    sudo apt-get install apache2
+    
+    # testing using apache benchmark tool
+    ab -t 100 -n 10000 -c 10 HTTPD_HOST
+
+    # clean up docker container
+    docker kill "${NAMESPACE}"
+
+    # delete network
+    docker network rm "${NAMESPACE}"
+}
+
+docker_compose_test()
+{
+    # update image in docker-compose yml
+    sed "s#@IMAGE#$IMAGE_REPOSITORY#g" "${SCRIPTPATH}"/docker-compose.yml.base > "${SCRIPTPATH}"/docker-compose.yml
+
+    # install postgresql container
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" up -d
+    report_pulls "${IMAGE_REPOSITORY}"
+
+    # sleep for 30 sec
+    sleep 30
+
+    # logs for tracking
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" logs
+
+    # kill docker-compose setup container
+    docker-compose -f "${SCRIPTPATH}"/docker-compose.yml -p "${NAMESPACE}" down
+
+    # clean up docker file
+    rm -rf "${SCRIPTPATH}"/docker-compose.yml
+}
+
+main()
+{
+    k8s_test
+    docker_test
+    docker_compose_test
+}
+
+main

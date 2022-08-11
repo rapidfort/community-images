195e526d535f8bfa4dd77f684eda38522e67373d
AnmolVirdi <anmolvirdi70@gmail.com>
2022-08-12 00:44:49 +0530

    ab test fixed for apache

--- community_images/apache/bitnami/functional_test.sh
+++ community_images/apache/bitnami/functional_test.sh
@@ -61,7 +61,7 @@ docker_test()
     sudo apt-get install apache2 -y
 
     # testing using apache benchmark tool
-    ab -t 100 -n 10000 -c 10 http://${APACHE_HOST}:8080/
+    ab -t 100 -n 10000 -c 10 http://"${APACHE_HOST}":8080/
 
     # clean up docker container
     docker kill "${NAMESPACE}"

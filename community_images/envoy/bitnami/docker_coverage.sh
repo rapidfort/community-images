b99f1e9761d635c516e1373b4dcef281bd7dc3b2
Vinod Gupta <codervinod@gmail.com>
2022-08-10 21:41:32 -0700

    Move envoy v2 (#109)
    * move envoy to v2
    
    * temp checkin
    
    * add tls certs for envoy
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix command runner
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * remove obsolete versions

---
+++ community_images/envoy/bitnami/docker_coverage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+set -e
+
+JSON_PARAMS="$1"
+
+JSON=$(cat "$JSON_PARAMS")
+
+echo "Json params for docker coverage = $JSON"
+
+NETWORK_NAME=$(jq -r '.network_name' < "$JSON_PARAMS")
+ENVOY_HOST=$(jq -r '.container_details.envoy.ip_address' < "$JSON_PARAMS")
+
+# run test on docker container
+docker run --rm --network="${NETWORK_NAME}" \
+    -i alpine \
+    apk add curl;curl http://"${ENVOY_HOST}":8081/ip;curl http://"${ENVOY_HOST}":9001/ready

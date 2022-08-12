30bf00b881dbefc1a437f9a2a336e1224729340a
Vinod Gupta <vinod@rapidfort.com>
2022-08-12 13:10:21 -0700

    fix runtime

--- community_images/redis/ironbank/dc_coverage.sh
+++ community_images/redis/ironbank/dc_coverage.sh
@@ -11,7 +11,6 @@ echo "Json params for docker compose coverage = $JSON"
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 
 CONTAINER_NAME="${NAMESPACE}"-redis-primary-1
-REDIS_PASSWORD=my_password
 
 # run redis tests
 docker exec -i "${CONTAINER_NAME}" bash -c "cat /tmp/test.redis | redis-cli"

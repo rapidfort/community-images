73279d4a3df349395aafc7d6c5d5556215fdbe41
Ankit Kumar <ankit@rapidfort.com>
2022-08-15 21:34:36 +0530

    update

--- community_images/prometheus/bitnami/flask_app.py
+++ community_images/prometheus/bitnami/flask_app.py
@@ -1,8 +1,9 @@
+"""The flask application"""
 #!/usr/bin/env python3
 
-from flask import Flask, Response
+from flask import Flask, Response #pylint: disable=import-error
+import prometheus_client #pylint: disable=import-error
 from helpers.middleware import setup_metrics
-import prometheus_client
 
 CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
 
@@ -12,19 +13,23 @@ setup_metrics(app)
 
 @app.route('/test/')
 def test():
+    """The test endpoint"""
     return 'rest'
 
 @app.route('/test1/')
 def test1():
-    1/0
+    """The test1 endpoint"""
+    1/0 #pylint: disable=pointless-statement
     return 'rest'
 
 @app.errorhandler(500)
 def handle_500(error):
+    """The error handler"""
     return str(error), 500
 
 @app.route('/metrics')
 def metrics():
+    """The metrics endpoint"""
     return Response(prometheus_client.generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
--- community_images/prometheus/bitnami/prometheus_selenium_test.py
+++ community_images/prometheus/bitnami/prometheus_selenium_test.py
@@ -14,48 +14,48 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities  
 
 
 class TestPrometheus():
-  """The test word press class for testing wordpress image."""
-  def setup_method(self, method):
-    """setup method."""
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument("disable-infobars")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    self.driver = webdriver.Chrome(options=chrome_options)  # pylint: disable=attribute-defined-outside-init
-    self.driver.implicitly_wait(10)
-  
-  def teardown_method(self, method): # pylint: disable=unused-argument
-    """teardown method."""
-    self.driver.quit()
-  
-  def test_s1(self, params):
-    # Test name: s1
-    # Step # | name | target | value
-    # 1 | open | /graph?g0.expr=&g0.tab=1&g0.stacked=0&g0.show_exemplars=0&g0.range_input=1h | 
-    self.driver.get("http://{}:{}/".format(params["prom_server"], params["port"]))  # pylint: disable=consider-using-f-string
-    # self.driver.get("http://localhost:9090/graph?g0.expr=&g0.tab=1&g0.stacked=0&g0.show_exemplars=0&g0.range_input=1h")
-    # 2 | setWindowSize | 1200x859 | 
-    self.driver.set_window_size(1200, 859)
-    # 3 | click | css=.cm-line | 
-    self.driver.find_element(By.CSS_SELECTOR, ".cm-line").click()
-    # 4 | click | css=.execute-btn | 
-    self.driver.find_element(By.CSS_SELECTOR, ".execute-btn").click()
-    # 5 | click | css=.cm-line | 
-    self.driver.find_element(By.CSS_SELECTOR, ".cm-line").click()
-    # 6 | editContent | css=.cm-content | <div class="cm-line">request_count_total</div>
-    element = self.driver.find_element(By.CSS_SELECTOR, ".cm-content")
-    self.driver.execute_script("if(arguments[0].contentEditable === 'true') {arguments[0].innerText = '<div class=\"cm-line\">request_count_total</div>'}", element)
-    # 7 | click | css=.execute-btn | 
-    self.driver.find_element(By.CSS_SELECTOR, ".execute-btn").click()
-    # search for the text on the page now
-    assert "request_count_total" in self.driver.page_source
-    # 8 | editContent | css=.cm-content | <div class="cm-line">request_latency_seconds_bucket</div>
-    element = self.driver.find_element(By.CSS_SELECTOR, ".cm-content")
-    self.driver.execute_script("if(arguments[0].contentEditable === 'true') {arguments[0].innerText = '<div class=\"cm-line\">request_latency_seconds_bucket</div>'}", element)
-    # 9 | click | css=.execute-btn | 
-    self.driver.find_element(By.CSS_SELECTOR, ".execute-btn").click()
-    # search for the text on the page
-    assert "request_latency_seconds_bucket" in self.driver.page_source
+    """The test word press class for testing wordpress image."""
+    def setup_method(self):
+        """setup method."""
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument("disable-infobars")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        self.driver = webdriver.Chrome(options=chrome_options)  # pylint: disable=attribute-defined-outside-init
+        self.driver.implicitly_wait(10)
+
+    def teardown_method(self, method): # pylint: disable=unused-argument
+        """teardown method."""
+        self.driver.quit()
+
+    def test_prometheus(self, params):
+        """The test method"""
+        # Test name: s1
+        # Step # | name | target | value
+        # 1 | open | /graph?g0.expr=&g0.tab=1&g0.stacked=0&g0.show_exemplars=0&g0.range_input=1h |
+        self.driver.get("http://{}:{}/".format(params["prom_server"], params["port"]))  # pylint: disable=consider-using-f-string
+        # 2 | setWindowSize | 1200x859 |
+        self.driver.set_window_size(1200, 859)
+        # 3 | click | css=.cm-line |
+        self.driver.find_element(By.CSS_SELECTOR, ".cm-line").click()
+        # 4 | click | css=.execute-btn |
+        self.driver.find_element(By.CSS_SELECTOR, ".execute-btn").click()
+        # 5 | click | css=.cm-line |
+        self.driver.find_element(By.CSS_SELECTOR, ".cm-line").click()
+        # 6 | editContent | css=.cm-content | <div class="cm-line">request_count_total</div>
+        element = self.driver.find_element(By.CSS_SELECTOR, ".cm-content")
+        self.driver.execute_script("if(arguments[0].contentEditable === 'true') {arguments[0].innerText = '<div class=\"cm-line\">request_count_total</div>'}", element) #pylint: disable=line-too-long
+        # 7 | click | css=.execute-btn |
+        self.driver.find_element(By.CSS_SELECTOR, ".execute-btn").click()
+        # search for the text on the page now
+        assert "request_count_total" in self.driver.page_source
+        # 8 | editContent | css=.cm-content | <div class="cm-line">request_latency_seconds_bucket</div> # pylint: disable=line-too-long
+        element = self.driver.find_element(By.CSS_SELECTOR, ".cm-content")
+        self.driver.execute_script("if(arguments[0].contentEditable === 'true') {arguments[0].innerText = '<div class=\"cm-line\">request_latency_seconds_bucket</div>'}", element)
+        # 9 | click | css=.execute-btn |
+        self.driver.find_element(By.CSS_SELECTOR, ".execute-btn").click()
+        # search for the text on the page
+        assert "request_latency_seconds_bucket" in self.driver.page_source

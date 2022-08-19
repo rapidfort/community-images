af9e2b324686b85278162cbdddcd4449ac59db04
Vinod Gupta <codervinod@gmail.com>
2022-08-18 19:59:13 -0700

    try enabling sys bench test (#116)
    * try enabling sys bench test
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime

--- community_images/mariadb/bitnami/k8s_coverage.sh
+++ community_images/mariadb/bitnami/k8s_coverage.sh
@@ -26,46 +26,46 @@ kubectl -n "${NAMESPACE}" exec -i "${RELEASE_NAME}"-0 -- /bin/bash -c "mysql -h 
 # copy mysql_coverage.sh into container
 kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/../../common/tests/mysql_coverage.sh "${RELEASE_NAME}"-0:/tmp/mysql_coverage.sh
 
-# # run mysql_coverage on cluster
-# kubectl -n "${NAMESPACE}" exec -i "${RELEASE_NAME}"-0 -- /bin/bash -c "/tmp/mysql_coverage.sh"
+# run mysql_coverage on cluster
+kubectl -n "${NAMESPACE}" exec -i "${RELEASE_NAME}"-0 -- /bin/bash -c "/tmp/mysql_coverage.sh"
 
-# # create sbtest schema
-# kubectl -n "${NAMESPACE}" exec -i "${RELEASE_NAME}"-0 \
-#     -- /bin/bash -c \
-#     "mysql -h localhost -uroot -p\"$MARIADB_ROOT_PASSWORD\" -e \"CREATE SCHEMA sbtest;\""
+# create sbtest schema
+kubectl -n "${NAMESPACE}" exec -i "${RELEASE_NAME}"-0 \
+    -- /bin/bash -c \
+    "mysql -h localhost -uroot -p\"$MARIADB_ROOT_PASSWORD\" -e \"CREATE SCHEMA sbtest;\""
 
-# # prepare benchmark
-# kubectl run -n "${NAMESPACE}" sb-prepare \
-#     --rm -i --restart='Never' \
-#     --image severalnines/sysbench \
-#     --command -- sysbench \
-#     --db-driver=mysql \
-#     --oltp-table-size=100000 \
-#     --oltp-tables-count=24 \
-#     --threads=1 \
-#     --mysql-host="${RELEASE_NAME}" \
-#     --mysql-port=3306 \
-#     --mysql-user=root \
-#     --mysql-password="${MARIADB_ROOT_PASSWORD}" \
-#     --mysql-debug=on \
-#     /usr/share/sysbench/tests/include/oltp_legacy/parallel_prepare.lua \
-#     run
+# prepare benchmark
+kubectl run -n "${NAMESPACE}" sb-prepare \
+    --rm -i --restart='Never' \
+    --image severalnines/sysbench \
+    --command -- sysbench \
+    --db-driver=mysql \
+    --oltp-table-size=100000 \
+    --oltp-tables-count=24 \
+    --threads=1 \
+    --mysql-host="${RELEASE_NAME}" \
+    --mysql-port=3306 \
+    --mysql-user=root \
+    --mysql-password="${MARIADB_ROOT_PASSWORD}" \
+    --mysql-debug=on \
+    /usr/share/sysbench/tests/include/oltp_legacy/parallel_prepare.lua \
+    run
 
-# # execute test
-# kubectl run -n "${NAMESPACE}" sb-run \
-#     --rm -i --restart='Never' \
-#     --image severalnines/sysbench \
-#     --command -- sysbench \
-#     --db-driver=mysql \
-#     --report-interval=2 \
-#     --mysql-table-engine=innodb \
-#     --oltp-table-size=100000 \
-#     --oltp-tables-count=24 \
-#     --threads=4 \
-#     --time=30 \
-#     --mysql-host="${RELEASE_NAME}" \
-#     --mysql-port=3306 \
-#     --mysql-user=root \
-#     --mysql-password="${MARIADB_ROOT_PASSWORD}" \
-#     /usr/share/sysbench/tests/include/oltp_legacy/oltp.lua \
-#     run
+# execute test
+kubectl run -n "${NAMESPACE}" sb-run \
+    --rm -i --restart='Never' \
+    --image severalnines/sysbench \
+    --command -- sysbench \
+    --db-driver=mysql \
+    --report-interval=2 \
+    --mysql-table-engine=innodb \
+    --oltp-table-size=100000 \
+    --oltp-tables-count=24 \
+    --threads=4 \
+    --time=30 \
+    --mysql-host="${RELEASE_NAME}" \
+    --mysql-port=3306 \
+    --mysql-user=root \
+    --mysql-password="${MARIADB_ROOT_PASSWORD}" \
+    /usr/share/sysbench/tests/include/oltp_legacy/oltp.lua \
+    run

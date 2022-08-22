722d240455d0d9d368e3f6d053bed09f58b08a24
Vinod Gupta <vinod@rapidfort.com>
2022-08-22 15:26:29 -0700

    move prometheus selenium tests to common

--- community_images/prometheus/bitnami/coverage.sh
+++ community_images/prometheus/bitnami/coverage.sh
@@ -52,39 +52,43 @@ function test_prometheus() {
     # wait for 10 secs for the metrics to be scraped and published
     sleep 10
 
-    CHROME_POD="python-chromedriver"
-    # delete the directory if present already
-    rm -rf "${SCRIPTPATH}"/docker-python-chromedriver
-    # delete the pod if present already, this sometimes happens if the cleanup didn't happen properly and is transient
-    kubectl delete pod "${CHROME_POD}" --namespace "${NAMESPACE}" --ignore-not-found=true
-    # clone the docker python chromedriver repo
-    git clone https://github.com/joyzoursky/docker-python-chromedriver.git "${SCRIPTPATH}"/docker-python-chromedriver
-    # cd docker-python-chromedriver
-    kubectl run "${CHROME_POD}" --restart='Never' --image joyzoursky/"${CHROME_POD}":latest --namespace "${NAMESPACE}" --command -- sleep infinity
+    # run selenium tests
+    "${SCRIPTPATH}"/../../common/selenium_tests/runner.sh "${PROMETHEUS_SERVER}" "${PROMETHEUS_PORT}" "${SCRIPTPATH}"/selenium_tests "${NAMESPACE}"
 
-    kubectl wait pods "${CHROME_POD}" -n "${NAMESPACE}" --for=condition=ready --timeout=10m
-    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/docker-python-chromedriver "${CHROME_POD}":/usr/workspace
 
-    echo "#!/bin/bash
-    cd /usr/workspace
-    . /tmp/helpers.sh
-    pip install pytest
-    pip install selenium
-    with_backoff pytest -s /tmp/prometheus_selenium_test.py --prom_server $PROMETHEUS_SERVER --port $PROMETHEUS_PORT" > "$SCRIPTPATH"/commands.sh
-    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/conftest.py "${CHROME_POD}":/tmp/conftest.py
-    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/prometheus_selenium_test.py "${CHROME_POD}":/tmp/prometheus_selenium_test.py
-    chmod +x "$SCRIPTPATH"/commands.sh
-    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/commands.sh "${CHROME_POD}":/tmp/common_commands.sh
-    kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/../../common/retry_helper.sh "${CHROME_POD}":/tmp/helpers.sh
+    # CHROME_POD="python-chromedriver"
+    # # delete the directory if present already
+    # rm -rf "${SCRIPTPATH}"/docker-python-chromedriver
+    # # delete the pod if present already, this sometimes happens if the cleanup didn't happen properly and is transient
+    # kubectl delete pod "${CHROME_POD}" --namespace "${NAMESPACE}" --ignore-not-found=true
+    # # clone the docker python chromedriver repo
+    # git clone https://github.com/joyzoursky/docker-python-chromedriver.git "${SCRIPTPATH}"/docker-python-chromedriver
+    # # cd docker-python-chromedriver
+    # kubectl run "${CHROME_POD}" --restart='Never' --image joyzoursky/"${CHROME_POD}":latest --namespace "${NAMESPACE}" --command -- sleep infinity
 
-    kubectl -n "${NAMESPACE}" exec -i "${CHROME_POD}" -- bash -c "/tmp/common_commands.sh"
-    # delete the generated commands.sh
-    rm "$SCRIPTPATH"/commands.sh
+    # kubectl wait pods "${CHROME_POD}" -n "${NAMESPACE}" --for=condition=ready --timeout=10m
+    # kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/docker-python-chromedriver "${CHROME_POD}":/usr/workspace
 
-    # delete the cloned directory
-    rm -rf "${SCRIPTPATH}"/docker-python-chromedriver
+    # echo "#!/bin/bash
+    # cd /usr/workspace
+    # . /tmp/helpers.sh
+    # pip install pytest
+    # pip install selenium
+    # with_backoff pytest -s /tmp/prometheus_selenium_test.py --prom_server $PROMETHEUS_SERVER --port $PROMETHEUS_PORT" > "$SCRIPTPATH"/commands.sh
+    # kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/conftest.py "${CHROME_POD}":/tmp/conftest.py
+    # kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/prometheus_selenium_test.py "${CHROME_POD}":/tmp/prometheus_selenium_test.py
+    # chmod +x "$SCRIPTPATH"/commands.sh
+    # kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/commands.sh "${CHROME_POD}":/tmp/common_commands.sh
+    # kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/../../common/retry_helper.sh "${CHROME_POD}":/tmp/helpers.sh
 
-    # delete the pods
-    kubectl delete pod "${FLASK_POD_NAME}" --namespace "${NAMESPACE}"
-    kubectl delete pod "${CHROME_POD}" --namespace "${NAMESPACE}"
+    # kubectl -n "${NAMESPACE}" exec -i "${CHROME_POD}" -- bash -c "/tmp/common_commands.sh"
+    # # delete the generated commands.sh
+    # rm "$SCRIPTPATH"/commands.sh
+
+    # # delete the cloned directory
+    # rm -rf "${SCRIPTPATH}"/docker-python-chromedriver
+
+    # # delete the pods
+    # kubectl delete pod "${FLASK_POD_NAME}" --namespace "${NAMESPACE}"
+    # kubectl delete pod "${CHROME_POD}" --namespace "${NAMESPACE}"
 }

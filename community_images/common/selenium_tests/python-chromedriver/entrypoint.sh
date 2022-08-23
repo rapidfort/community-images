eab19f5852a317c49b02716d8d466529e0e7039e
Vinod Gupta <vinod@rapidfort.com>
2022-08-23 12:06:20 -0700

    fix runtime

--- community_images/common/selenium_tests/python-chromedriver/entrypoint.sh
+++ community_images/common/selenium_tests/python-chromedriver/entrypoint.sh
@@ -17,5 +17,8 @@ fi
 # shellcheck source=community_images/common/scripts/bash_helper.sh
 source /usr/workspace/bash_helper.sh
 
+# show folders
+ls -lR /usr/workspace/
+
 # run pytest
 with_backoff pytest -s /usr/workspace/selenium_tests/ --server "$SERVER" --port "$PORT"

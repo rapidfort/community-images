6d92f1f518ad4e4892b01ec873ee8acf553934f6
Vinod Gupta <vinod@rapidfort.com>
2022-09-06 13:21:47 -0700

    fix view browse

--- community_images/airflow/bitnami/selenium_tests/test_viewbrowse.py
+++ community_images/airflow/bitnami/selenium_tests/test_viewbrowse.py
@@ -51,7 +51,6 @@ class TestViewbrowse():
         actions.move_to_element(element).perform()
         self.driver.find_element(By.LINK_TEXT, "Jobs").click()
         self.driver.find_element(By.LINK_TEXT, "2").click()
-        self.driver.find_element(By.LINK_TEXT, "3").click()
         element = self.driver.find_element(By.LINK_TEXT, "Browse")
         actions = ActionChains(self.driver)
         actions.move_to_element(element).perform()

48cff5b7e4e21c3d84ed666c85be7928fd0fc725
AnmolVirdi <anmolvirdi70@gmail.com>
2022-09-06 23:13:50 +0530

    modifying docker coverage for apache v2

--- community_images/apache/bitnami/docker_coverage.sh
+++ community_images/apache/bitnami/docker_coverage.sh
@@ -10,20 +10,11 @@ JSON=$(cat "$JSON_PARAMS")
 echo "Json params for docker coverage = $JSON"
 
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
+NETWORK_NAME=$(jq -r '.network_name' < "$JSON_PARAMS")
 REPOSITORY=apache
 
-# create network
-docker network create -d bridge "${NAMESPACE}"
-
-# create docker container
-docker run --rm -d --network="${NAMESPACE}" \
-    --name "${NAMESPACE}" "$REPOSITORY":latest
-
-# sleep for few seconds
-sleep 30
-
 # get docker host ip
-APACHE_HOST=$(docker inspect "${NAMESPACE}" | jq -r ".[].NetworkSettings.Networks[\"${NAMESPACE}\"].IPAddress")
+APACHE_HOST=$(docker inspect "${NETWORK_NAME}" | jq -r ".[].NetworkSettings.Networks[\"${NETWORK_NAME}\"].IPAddress")
 
 # Install Apache benchmark testing tool
 sudo apt-get install apache2-utils -y
@@ -31,9 +22,3 @@ sudo apt-get install apache2 -y
 
 # testing using apache benchmark tool
 ab -t 100 -n 10000 -c 10 http://"${APACHE_HOST}":8080/
-
-# clean up docker container
-docker kill "${NAMESPACE}"
-
-# delete network
-docker network rm "${NAMESPACE}"

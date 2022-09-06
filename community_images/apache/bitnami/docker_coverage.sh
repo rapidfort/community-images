72fbfd027f1101d852534f68431370b72dbb548c
AnmolVirdi <anmolvirdi70@gmail.com>
2022-09-06 23:30:02 +0530

    finalizing docker coverage for apache v2

--- community_images/apache/bitnami/docker_coverage.sh
+++ community_images/apache/bitnami/docker_coverage.sh
@@ -9,12 +9,8 @@ JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker coverage = $JSON"
 
-NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 NETWORK_NAME=$(jq -r '.network_name' < "$JSON_PARAMS")
-REPOSITORY=apache
-
-# get docker host ip
-APACHE_HOST=$(docker inspect "${NETWORK_NAME}" | jq -r ".[].NetworkSettings.Networks[\"${NETWORK_NAME}\"].IPAddress")
+APACHE_HOST=$(jq -r '.container_details.apache.ip_address' < "$JSON_PARAMS")
 
 # Install Apache benchmark testing tool
 sudo apt-get install apache2-utils -y

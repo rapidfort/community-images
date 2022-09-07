7c7b8776c32dc194792cdad8689d0560e4a52f05
AnmolVirdi <anmolvirdi70@gmail.com>
2022-09-07 23:10:50 +0530

    Removed unnecessary code elements for apache v2

--- community_images/apache/bitnami/k8s_coverage.sh
+++ community_images/apache/bitnami/k8s_coverage.sh
@@ -5,13 +5,8 @@ set -e
 
 JSON_PARAMS="$1"
 
-SCRIPTPATH=$(jq -r '.image_script_dir' < "$JSON_PARAMS")
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
-REPOSITORY=apache
-
-# get pod name
-POD_NAME=$(kubectl -n "${NAMESPACE}" get pods -l app.kubernetes.io/name="$REPOSITORY" -o jsonpath="{.items[0].metadata.name}")
 
 # fetch service url and store the urls in URLS file
 rm -f URLS

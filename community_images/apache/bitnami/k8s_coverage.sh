9db3640d3c16dfdad594a39bfc8e5e2b48c5ce10
AnmolVirdi <anmolvirdi70@gmail.com>
2022-09-07 10:17:53 +0530

    Removed unneecessary code elements for apache v2

--- community_images/apache/bitnami/k8s_coverage.sh
+++ community_images/apache/bitnami/k8s_coverage.sh
@@ -3,8 +3,6 @@
 set -x
 set -e
 
-# shellcheck disable=SC1091
-#SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 JSON_PARAMS="$1"
 
 SCRIPTPATH=$(jq -r '.image_script_dir' < "$JSON_PARAMS")
@@ -37,9 +35,3 @@ MINIKUBE_IP=$(minikube ip)
 
 # curl to https url
 curl http://"${MINIKUBE_IP}" -k
-
-# copy common_commands.sh into container
-kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/../../common/tests/common_commands.sh "${POD_NAME}":/tmp/common_commands.sh
-
-# run command on cluster
-kubectl -n "${NAMESPACE}" exec -i "${POD_NAME}" -c apache -- /bin/bash -c "/tmp/common_commands.sh"

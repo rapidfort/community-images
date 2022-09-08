ee44f570d5039c17579d07ecd63ce63208202003
AnmolVirdi <anmolvirdi70@gmail.com>
2022-09-09 01:32:11 +0530

    final touchups for nginx/bitnami v2

--- community_images/nginx/bitnami/k8s_coverage.sh
+++ community_images/nginx/bitnami/k8s_coverage.sh
@@ -12,7 +12,7 @@ RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
 rm -f URLS
 URL=$(minikube service "${RELEASE_NAME}" -n "${NAMESPACE}" --url)
 
-# sleep 5 after minikube service
+# sleep 5 after minikube service (Required)
 sleep 5
 
 # curl to http url

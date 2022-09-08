162513266de89cd4b01ce30f3a9a0f83d5f00edd
AnmolVirdi <anmolvirdi70@gmail.com>
2022-09-08 10:25:21 +0530

    Added runtimes scripts for Nginx v2

---
+++ community_images/nginx/bitnami/k8s_coverage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+set -e
+
+JSON_PARAMS="$1"
+
+NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
+RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
+
+# fetch service url and store the urls in URLS file
+rm -f URLS
+URL = $(minikube service "${RELEASE_NAME}" -n "${NAMESPACE}" --url)
+
+# curl to http url
+curl "${URL}"
+
+# fetch minikube ip
+MINIKUBE_IP=$(minikube ip)
+
+# curl to https url
+curl http://"${MINIKUBE_IP}" -k

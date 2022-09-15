c1c4e17cfc54b6eb50cb5820a8dd7def2e157bd2
AnmolVirdi <anmolvirdi70@gmail.com>
2022-09-15 06:11:19 +0530

    Modified coverage script

--- community_images/consul/bitnami/coverage_script.sh
+++ community_images/consul/bitnami/coverage_script.sh
@@ -18,6 +18,9 @@ consul services register /consul.d/sample_service.json
 consul reload
 sleep 10
 
+# #Query our service using DNS api
+# dig @127.0.0.1 -p 8600 rails.web.service.consul SRV
+
 #Query our service using HTTP Api
 curl http://localhost:8500/vi/catalog/service/web
 
@@ -25,5 +28,5 @@ curl http://localhost:8500/vi/catalog/service/web
 curl 'http://localhost:8500/v1/health/service/web?passing'
 
 #Removing service
-consul services deregister web.json
+consul services deregister /consul.d/sample_service.json
 consul reload

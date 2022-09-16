30e20ad0640bf209de49f1bd96edddc68e6a7f9c
Vinod Gupta <vinod@rapidfort.com>
2022-09-16 16:21:46 -0700

    fix runtime

--- community_images/curl/curlimages/docker_coverage.sh
+++ community_images/curl/curlimages/docker_coverage.sh
@@ -13,6 +13,6 @@ NETWORK_NAME=$(jq -r '.network_name' < "$JSON_PARAMS")
 CONTAINER_NAME=$(jq -r '.container_details.curl' < "$JSON_PARAMS")
 
 # run test on docker container
-docker run --rm --network="${NETWORK_NAME}" \
-    -i alpine \
-    apk add curl;curl http://"${ENVOY_HOST}":8081/ip;curl http://"${ENVOY_HOST}":9001/ready
+docker exec \
+    -i "$CONTAINER_NAME" \
+    curl --version

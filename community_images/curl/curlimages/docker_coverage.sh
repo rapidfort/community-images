0e94f43ef724164e6ad52f3b4da5c0e640d12a97
Vinod Gupta <vinod@rapidfort.com>
2022-09-18 11:56:11 -0700

    fix runtime

--- community_images/curl/curlimages/docker_coverage.sh
+++ community_images/curl/curlimages/docker_coverage.sh
@@ -9,7 +9,6 @@ JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker compose coverage = $JSON"
 
-NETWORK_NAME=$(jq -r '.network_name' < "$JSON_PARAMS")
 CONTAINER_NAME=$(jq -r '.container_details.curl.name' < "$JSON_PARAMS")
 
 # run version

b45e745b5152efb95b25f0756c6c8e1a425892eb
AnmolVirdi <anmolvirdi70@gmail.com>
2022-09-18 23:44:19 +0530

    Check for client container

--- community_images/consul/bitnami/scripts/client_coverage_script.sh
+++ community_images/consul/bitnami/scripts/client_coverage_script.sh
@@ -7,13 +7,9 @@ ls
 
 # Available Scripts
 ls /opt/bitnami/scripts
-consul members
 
 # Registering a test service via client
 consul reload
-
-# #Query our service using DNS API
-# dig @127.0.0.1 -p 8600 rails.web.service.consul SRV
 
 # Query our service using HTTP Api
 curl http://localhost:8500/vi/catalog/service/web
--- community_images/consul/bitnami/scripts/server_coverage_script.sh
+++ community_images/consul/bitnami/scripts/server_coverage_script.sh
@@ -8,20 +8,18 @@ ls
 # Available Scripts
 ls /opt/bitnami/scripts
 
-# Display the congiguration of the server
-# cat /opt/bitnami/consul/consul.json
-
-# consul aclt token list
+# consul acl token list
 # consul acl boostrap
 consul members
+consul info
 
 # Registering a test service
 consul services register /consul.d/sample_service.json
 consul reload
 sleep 10
 
-# #Query our service using DNS API
-# dig @127.0.0.1 -p 8600 rails.web.service.consul SRV
+# Display the congiguration of the server
+cat /opt/bitnami/consul/consul.json
 
 # Query our service using HTTP Api
 curl http://localhost:8500/vi/catalog/service/web

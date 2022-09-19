27be41aa749dd1e6b675225eb29e139d25f36da2
Vinod Gupta <vinod@rapidfort.com>
2022-09-19 09:52:12 -0700

    Add curlimages (#160)
    * add curl images
    
    * add sleep script
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * add date based tags
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * add curl image
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * fix slack CTA2
    
    * Fix common entrypoint.
    
    * fix slack CTA2
    
    * Fix entrypoint
    
    * fix python lint
    
    * fix issues with shellcheck

--- scripts/cleanup_workflows.sh
+++ scripts/cleanup_workflows.sh
@@ -1,16 +1,21 @@
+#!/bin/bash
+
 org=rapidfort
 repo=community-images
 
 # Get workflow IDs with status "disabled_manually"
+# shellcheck disable=SC2207
 workflow_ids=($(gh api -H "Accept: application/vnd.github+json" /repos/$org/$repo/actions/workflows | jq '.workflows[] | select(.["state"] | contains("disabled_manually")) | .id'))
 
 for workflow_id in "${workflow_ids[@]}"
 do
   echo "Listing runs for the workflow ID $workflow_id"
-  run_ids=( $(gh api -H "Accept: application/vnd.github+json" repos/$org/$repo/actions/workflows/$workflow_id/runs --paginate | jq '.workflow_runs[].id') )
+
+  # shellcheck disable=SC2207
+  run_ids=( $(gh api -H "Accept: application/vnd.github+json" repos/$org/$repo/actions/workflows/"$workflow_id"/runs --paginate | jq '.workflow_runs[].id') )
   for run_id in "${run_ids[@]}"
   do
     echo "Deleting Run ID $run_id"
-    gh api -H "Accept: application/vnd.github+json" repos/$org/$repo/actions/runs/$run_id -X DELETE >/dev/null
+    gh api -H "Accept: application/vnd.github+json" repos/$org/$repo/actions/runs/"$run_id" -X DELETE >/dev/null
   done
 done

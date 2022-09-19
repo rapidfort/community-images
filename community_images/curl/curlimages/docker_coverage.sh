c59aea0ce7ab8845dc99849b4a429d8e4c2a8dad
Vinod Gupta <vinod@rapidfort.com>
2022-09-19 14:45:40 -0700

    Improve curl (#161)
    * improve curl image
    
    * fix runtime
    
    * fix runtime

--- community_images/curl/curlimages/docker_coverage.sh
+++ community_images/curl/curlimages/docker_coverage.sh
@@ -16,6 +16,11 @@ docker exec \
     -i "$CONTAINER_NAME" \
     curl --version
 
+# use entrypoint
+docker exec \
+    -i "$CONTAINER_NAME" \
+    /entrypoint.sh --version
+
 # run curl
 docker exec \
     -i "$CONTAINER_NAME" \
@@ -25,3 +30,13 @@ docker exec \
 docker exec \
     -i "$CONTAINER_NAME" \
     curl -d@/work/test.txt https://httpbin.org/post
+
+# run http2
+docker exec \
+    -i "$CONTAINER_NAME" \
+    curl -sI https://curl.se -o/dev/null -w '%{http_version}\n'
+
+# test brotli compression
+docker exec \
+    -i "$CONTAINER_NAME" \
+    curl --compressed https://httpbin.org/brotli

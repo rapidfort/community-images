2fff221a4cb2b12e1efcff96c6adc23dbf501a21
Vinod Gupta <vinod@rapidfort.com>
2022-09-22 13:45:25 -0700

    use tls in k8s only

--- community_images/redis-cluster/bitnami/dc_coverage.sh
+++ community_images/redis-cluster/bitnami/dc_coverage.sh
@@ -20,7 +20,13 @@ RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
 CONTAINER_NAME="${PROJECT_NAME}"-redis-node-0-1
 REDIS_PASSWORD=bitnami
 
+# run all redis commands in test.redis
 docker exec -i "$CONTAINER_NAME" \
     bash -c "/tmp/redis_cluster_runner.sh ${REDIS_PASSWORD} localhost -p 9379 /tmp/test.redis --tls --cert /certs/server.crt --key /certs/server.key"
 
+# run redis coverage
+docker exec -i "$CONTAINER_NAME" \
+    bash -c "/tmp/redis_coverage.sh"
+
+# run redis benchmark
 docker exec -i "$CONTAINER_NAME"  bash -c "redis-benchmark -h localhost -p 6379 -c 2 -n 100 -a ${REDIS_PASSWORD} --cluster"
--- community_images/redis-cluster/bitnami/k8s_tls_coverage.sh
+++ community_images/redis-cluster/bitnami/k8s_tls_coverage.sh
@@ -12,28 +12,8 @@ RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
 # get Redis passwordk
 REDIS_PASSWORD=$(kubectl get secret --namespace "${NAMESPACE}" "${RELEASE_NAME}" -o jsonpath="{.data.redis-password}" | base64 --decode)
 
-# copy test.redis into container
-kubectl -n "${NAMESPACE}" cp \
-    "${SCRIPTPATH}"/../../common/tests/test.redis \
-    "${RELEASE_NAME}"-0:/tmp/test.redis
-
-# copy redis_cluster_runner.sh into container
-kubectl -n "${NAMESPACE}" cp \
-    "${SCRIPTPATH}"/redis_cluster_runner.sh \
-    "${RELEASE_NAME}"-0:/tmp/redis_cluster_runner.sh
-
 # run redis_cluster_runner on cluster
 kubectl -n "${NAMESPACE}" exec \
     -i "${RELEASE_NAME}"-0 \
     -- /bin/bash -c \
-    "/tmp/redis_cluster_runner.sh ${REDIS_PASSWORD} ${RELEASE_NAME} /tmp/test.redis --tls --cert /opt/bitnami/redis/certs/tls.crt --key /opt/bitnami/redis/certs/tls.key --cacert /opt/bitnami/redis/certs/ca.crt"
-
-# copy redis_coverage.sh into container
-kubectl -n "${NAMESPACE}" cp \
-    "${SCRIPTPATH}"/../../common/tests/redis_coverage.sh \
-    "${RELEASE_NAME}"-0:/tmp/redis_coverage.sh
-
-# run redis_coverage command on cluster
-kubectl -n "${NAMESPACE}" exec \
-    -i "${RELEASE_NAME}"-0 -- /bin/bash -c \
-    "/tmp/redis_coverage.sh"
+    "REDISCLI_AUTH=${REDIS_PASSWORD} redis-cli -h ${RELEASE_NAME} -p 6379 --tls --cert /opt/bitnami/redis/certs/tls.crt --key /opt/bitnami/redis/certs/tls.key --cacert /opt/bitnami/redis/certs/ca.crt -c $line"

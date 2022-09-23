56b90d2add7de9729c31c04e865f888239df9432
Vinod Gupta <vinod@rapidfort.com>
2022-09-23 16:53:29 -0700

    Add total pulls (#168)
    * add total pull count
    
    * add total pull counts
    
    * format pull counts
    
    * add total pull count
    
    * fix runtime

--- scripts/gen_image_list.py
+++ scripts/gen_image_list.py
@@ -4,11 +4,34 @@ This is consumed by jinja template for generating main readme
 """
 
 import os
+import requests
 import sys
 import yaml
 
+def get_image_dh_data(image_dict):
+    """
+    Get dockerhub data and push it to yaml
+    input: image_dict
+    output: image_dict
+    """
+    rf_docker_link = image_dict["rf_docker_link"]
+    url = f"https://hub.docker.com/v2/repositories/{rf_docker_link}/"
+    resp = requests.get(url)
+    if 199 <= resp.status_code < 400:
+        resp_json = resp.json()
+        image_dict["pull_count"] = resp_json.get("pull_count")
+        image_dict["last_updated"] = resp_json.get("last_updated")
+        image_dict["star_count"] = resp_json.get("star_count")
+        image_dict["pull_count_formatted"] = format(image_dict["pull_count"], ",")
+        print(f"Got image data for {rf_docker_link}")
+        print(f"p:{image_dict['pull_count']}, lu:{image_dict['last_updated']}, s:{image_dict['star_count']}")
+    else:
+        print("failed to get dh data [%d]: %s ", resp.status_code, resp.texts)
+        sys.exit(1)
+    return image_dict
 
-def merge_yaml_files(image_list_file):
+
+def merge_yaml_files(image_list_file, needs_dh_data=False):
     """
     Reads image.lst file
     Opens image.yml for each image
@@ -29,10 +52,17 @@ def merge_yaml_files(image_list_file):
             try:
                 with open(image_yml_path, "r", encoding="utf8") as image_yml_stream:
                     image_dict = yaml.safe_load(image_yml_stream)
+                    if needs_dh_data:
+                        image_dict = get_image_dh_data(image_dict)
                     image_list.append(image_dict)
             except yaml.YAMLError as exc:
                 print(exc)
-    image_list_dict = dict(image_list=image_list)
+    image_list=sorted(image_list, key=lambda i: i["name"])
+    if needs_dh_data:
+        image_list_sorted = sorted(image_list, key=lambda i: i["pull_count"],reverse=True)
+        image_list_dict = dict(image_list=image_list,image_list_sorted=image_list_sorted)
+    else:
+        image_list_dict = dict(image_list=image_list)
 
     out_yml_path = os.path.join(script_path, "..", "image_list.yml")
     with open(out_yml_path, "w", encoding="utf8") as outfile:
@@ -42,11 +72,12 @@ def merge_yaml_files(image_list_file):
 def main():
     """ main function """
     image_list_file = "image.lst"
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 3:
         image_list_file = sys.argv[1]
+        needs_dh_data = True if sys.argv[2]=="yes" else False
 
     print(f"Using image list={image_list_file}")
-    merge_yaml_files(image_list_file)
+    merge_yaml_files(image_list_file, needs_dh_data)
 
 
 if __name__ == "__main__":

3d6a22dbafdbac54f07e25a0c8ffc449f1662fe4
AnmolVirdi <anmolvirdi70@gmail.com>
2022-09-30 18:02:34 +0530

    added roundrobin check in dc_coverage

--- community_images/haproxy/bitnami/dc_coverage.sh
+++ community_images/haproxy/bitnami/dc_coverage.sh
@@ -16,7 +16,7 @@ echo "Json params for docker compose coverage = $JSON"
 
 PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
 
-CONTAINER_NAME="${PROJECT_NAME}"-apache-1
+CONTAINER_NAME="${PROJECT_NAME}"-haproxy-1
 
 # exec into container and run coverage script
 docker exec -i "${CONTAINER_NAME}" bash -c /opt/bitnami/scripts/coverage_script.sh
@@ -24,17 +24,14 @@ docker exec -i "${CONTAINER_NAME}" bash -c /opt/bitnami/scripts/coverage_script.
 # log for debugging
 docker inspect "${CONTAINER_NAME}"
 
-# find non-tls and tls port
-docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8080/tcp\"[0].HostPort"
-docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort"
-NON_TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8080/tcp\"[0].HostPort")
-TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort")
+# finding port
+docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort"
+PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort")
 
 # run curl in loop for different endpoints
-for i in {1..20};
+for i in {1..10};
 do 
     echo "Attempt $i"
-        curl http://localhost:"${NON_TLS_PORT}"
-        with_backoff curl https://localhost:"${TLS_PORT}" -k -v
+    curl http://localhost:"${PORT}"
 done
 

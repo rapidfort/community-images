f9359598811ee813e5d7fa5f5687bf435fe03c5e
AnmolVirdi <anmolvirdi70@gmail.com>
2022-09-30 22:08:38 +0530

    added routes in php sample server

--- community_images/haproxy/bitnami/configs/sample-server/src/index.php
+++ community_images/haproxy/bitnami/configs/sample-server/src/index.php
@@ -1,3 +1,43 @@
 <?php
-echo "My container IP is ", $_SERVER['SERVER_ADDR'];
-?>
+
+$routes = [];
+
+route('/', function () {
+    echo "My container IP is ", $_SERVER['SERVER_ADDR'];
+});
+
+route('/app1', function () {
+    echo "My container IP is ", $_SERVER['SERVER_ADDR'];
+});
+
+route('/app2', function () {
+    echo "My container IP is ", $_SERVER['SERVER_ADDR'];
+});
+
+route('/404', function () {
+  echo "Page not found";
+});
+
+function route(string $path, callable $callback) {
+  global $routes;
+  $routes[$path] = $callback;
+}
+
+run();
+
+function run() {
+  global $routes;
+  $uri = $_SERVER['REQUEST_URI'];
+  $found = false;
+  foreach ($routes as $path => $callback) {
+    if ($path !== $uri) continue;
+
+    $found = true;
+    $callback();
+  }
+
+  if (!$found) {
+    $notFoundCallback = $routes['/404'];
+    $notFoundCallback();
+  }
+}

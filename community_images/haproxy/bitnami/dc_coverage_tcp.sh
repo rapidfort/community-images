b7fda1d37a5c6162acfe48f2328d111a44012391
AnmolVirdi <anmolvirdi70@gmail.com>
2022-10-01 09:22:39 +0530

    Adding more functionality

--- community_images/haproxy/bitnami/dc_coverage_tcp.sh
+++ community_images/haproxy/bitnami/dc_coverage_tcp.sh
@@ -26,6 +26,36 @@ docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"
 PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort")
 
 # run curl in loop (roundrobin)
+for i in {1..5};
+do 
+    echo "Attempt $i"
+    curl http://localhost:"${PORT}"
+done
+
+# Changing compression algo from identity to gzip
+docker exec -i "${CONTAINER_NAME}" cp /bitnami/haproxy/conf/haproxy.cfg /bitnami/haproxy/haproxy.cfg
+sed -i 's/identity/gzip/g' /bitnami/haproxy/haproxy.cfg
+docker exec -i "${CONTAINER_NAME}" cp /bitnami/haproxy/haproxy.cfg /bitnami/haproxy/conf/haproxy.cfg
+# reloading
+docker kill -s HUP haproxy
+sleep 5
+# Checking leastconn
+# run curl in loop
+for i in {1..4};
+do 
+    echo "Attempt $i"
+    curl http://localhost:"${PORT}"
+done
+
+# Changing compression algo from gzip to deflate
+docker exec -i "${CONTAINER_NAME}" cp /bitnami/haproxy/conf/haproxy.cfg /bitnami/haproxy/haproxy.cfg
+sed -i 's/gzip/deflate/g' /bitnami/haproxy/conf/haproxy.cfg
+docker exec -i "${CONTAINER_NAME}" cp /bitnami/haproxy/haproxy.cfg /bitnami/haproxy/conf/haproxy.cfg
+# reloading
+docker kill -s HUP haproxy
+sleep 5
+# Checking source mode
+# run curl in loop
 for i in {1..10};
 do 
     echo "Attempt $i"

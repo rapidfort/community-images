0705e6844d3dcf96614b33e09ca6aab8f2827cf3
AnmolVirdi <anmolvirdi70@gmail.com>
2022-10-01 22:40:52 +0530

    modified k8s runtime script

--- community_images/haproxy/bitnami/k8s_coverage.sh
+++ community_images/haproxy/bitnami/k8s_coverage.sh
@@ -11,20 +11,5 @@ RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
 # fetch service url and store the urls in URLS file
 rm -f URLS
 minikube service "${RELEASE_NAME}" -n "${NAMESPACE}" --url | tee -a URLS
-
 cat URLS
-
-# curl to url
-while read -r p;
-do
-    curl -k "${p}"
-done <URLS
-
-#Removing urls file
 rm URLS
-
-# fetch minikube ip
-MINIKUBE_IP=$(minikube ip)
-
-# curl to https url
-curl http://"${MINIKUBE_IP}" -k

57547d03ea447110c7d4cc031dbea1a55619c3dd
Vinod Gupta <vinod@rapidfort.com>
2022-10-04 09:27:47 -0700

    Use separate output registry (#179)
    * Use RapidFort account from environment
    
    * add RAPIDFORT_ACCOUNT secret
    
    * dont run latest in PR
    
    * trigger build.
    
    * fix errors
    
    * fix runtime
    
    * use remove orphans
    
    * handle docker compose down errors

--- community_images/prometheus/bitnami/build_docker.sh
+++ community_images/prometheus/bitnami/build_docker.sh
@@ -4,11 +4,10 @@ set -x
 set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-RAPIDFORT_ACCOUNT="${RAPIDFORT_ACCOUNT:-rapidfort}"
 
 TAG="latest"
 
-IMAGE_TAG="$RAPIDFORT_ACCOUNT"/flaskapp:"${TAG}"
+IMAGE_TAG=rapidfort/flaskapp:"${TAG}"
 
 docker build -t "${IMAGE_TAG}" "${SCRIPTPATH}"
 docker push "${IMAGE_TAG}"

9fd7394f3fab0401d1a72b52d3e2e993f25d04e4
David Dooley <davidd@rapidfort.com>
2022-10-06 19:17:51 -0400

    adding temp logging

--- community_images/common/orchestrator/stub_generator.py
+++ community_images/common/orchestrator/stub_generator.py
@@ -64,13 +64,14 @@ class StubGenerator:
 
             self._run_stub_command(input_tag_details.full_tag)
 
+            logging.warning(f"ddooley output_tag_details: [{output_tag_details}]")
+
             # tag input stubbed image to output stubbed image
             stub_image = self.docker_client.images.get(
                 input_tag_details.full_stub_tag)
             result = stub_image.tag(output_tag_details.full_stub_tag)
 
             # push stubbed image to output repo
-            logging.warning("output_tag_details: [{output_tag_details}]")
             result = self.docker_client.api.push(
                 output_tag_details.full_repo_path,
                 output_tag_details.stub_tag)

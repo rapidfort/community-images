d1cafd54b4ad7ef0e63edc2fddd3a0a9c72e510b
David Dooley <davidd@rapidfort.com>
2022-10-10 19:55:57 -0400

    trying first test

--- community_images/kong/official/dc_coverage.sh
+++ community_images/kong/official/dc_coverage.sh
@@ -10,3 +10,10 @@ JSON=$(cat "$JSON_PARAMS")
 echo "Json params for docker compose coverage = $JSON"
 
 # PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
+
+## SERVICES & ROUTES
+# add a new service
+curl -i -s -X POST http://localhost:8001/services \
+  --data name=example_service \
+  --data url='http://mockbin.org'
+  

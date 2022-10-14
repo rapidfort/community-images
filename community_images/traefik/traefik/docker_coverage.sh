08822416105784915d13a7bb1d3cb699c2b70ee5
Ashish Srivastava <ashish@rapidfort.com>
2022-10-15 00:22:52 +0530

    fixing exposed port and curl command

--- community_images/traefik/traefik/docker_coverage.sh
+++ community_images/traefik/traefik/docker_coverage.sh
@@ -11,15 +11,11 @@ echo "Json params for docker coverage = $JSON"
 
 PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
 
-NON_TLS_PORT="82"
-TLS_PORT="445"
-
 # Testing load balencer 
 for i in {1..3};
 do
     echo "Attempt $i"
-    curl -H Host:whoami.docker.localhost http://127.0.0.1:"${NON_TLS_PORT}"
-    curl http://localhost:"${NON_TLS_PORT}"       
-    with_backoff curl -H Host:whoami.docker.localhost http://127.0.0.1:"${TLS_PORT}" -k -v
-    with_backoff curl -H Host:whoami.docker.localhost http://127.0.0.1:"${TLS_PORT}" -k -v
+    docker run -d -p "{$PROJECT_NAME}" rapidfort/curl "-H Host:whoami.docker.localhost http://127.0.0.1" 
+    docker run -d -p "{$PROJECT_NAME}" rapidfort/curl "-H Host:whoami.docker.localhost http://127.0.0.1" 
+    docker run -d -p "{$PROJECT_NAME}" rapidfort/curl "-H Host:whoami.docker.localhost http://127.0.0.1" 
 done

ab31e0d7ebd777b98ff7cd2bcbf5c863c57aec65
Ashish Srivastava <ashish@rapidfort.com>
2022-10-17 03:01:29 +0530

    updated coverage

--- community_images/traefik/traefik/dc_coverage.sh
+++ community_images/traefik/traefik/dc_coverage.sh
@@ -9,4 +9,14 @@ JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker compose coverage = $JSON"
 
-# PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
+PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
+
+NETWORK_NAME="${PROJECT_NAME}"_default
+# Testing load balencer 
+for i in {1..3};
+do
+    echo "Attempt $i"
+    docker run -d --net "$NETWORK_NAME" rapidfort/curl "-H Host:whoami.docker.localhost http://127.0.0.1" 
+    docker run -d --net "$NETWORK_NAME" rapidfort/curl "-H Host:whoami.docker.localhost http://127.0.0.1" 
+    docker run -d --net "$NETWORK_NAME" rapidfort/curl "-H Host:whoami.docker.localhost http://127.0.0.1" 
+done
--- community_images/traefik/traefik/docker_coverage.sh
+++ community_images/traefik/traefik/docker_coverage.sh
@@ -8,14 +8,3 @@ JSON_PARAMS="$1"
 JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker coverage = $JSON"
-
-PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
-
-# Testing load balencer 
-for i in {1..3};
-do
-    echo "Attempt $i"
-    docker run -d -p "{$PROJECT_NAME}" rapidfort/curl "-H Host:whoami.docker.localhost http://127.0.0.1" 
-    docker run -d -p "{$PROJECT_NAME}" rapidfort/curl "-H Host:whoami.docker.localhost http://127.0.0.1" 
-    docker run -d -p "{$PROJECT_NAME}" rapidfort/curl "-H Host:whoami.docker.localhost http://127.0.0.1" 
-done

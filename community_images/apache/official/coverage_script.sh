ac709c4f1936319f84ffb79e402eeaf82cafc9ab
vinod <anmolvirdi70@gmail.com>
2022-10-31 15:11:58 -0700

    configuring httpd

--- community_images/apache/official/coverage_script.sh
+++ community_images/apache/official/coverage_script.sh
@@ -2,13 +2,13 @@
 
 set -x
 set -e
-ls modules/
-httpd -M
+#ls modules/
+#httpd -M
 ls
-sed -i '/LoadModule /d' conf/httpd.conf
-cat modules_list >> conf/httpd.conf
+#sed -i '/LoadModule /d' conf/httpd.conf
+#cat modules_list >> conf/httpd.conf
 #Modules excluded: ["mpm_worker_module" "mpm_event_module"]
-apachectl configtest
-apachectl -k graceful
-httpd -M
+#apachectl configtest
+#apachectl -k graceful
+#httpd -M
 
--- community_images/apache/official/dc_coverage.sh
+++ community_images/apache/official/dc_coverage.sh
@@ -19,21 +19,27 @@ PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
 CONTAINER_NAME="${PROJECT_NAME}"-apache-1
 
 # exec into container and run coverage script
-docker exec -i "${CONTAINER_NAME}" bash -c /opt/bitnami/scripts/coverage_script.sh
+docker exec -i "${CONTAINER_NAME}" ls
+docker exec -i "${CONTAINER_NAME}" httpd -M
+docker exec -i "${CONTAINER_NAME}" sed -i '/LoadModule /d' conf/httpd.conf
+docker exec -i "${CONTAINER_NAME}" tee < /usr/local/apache2/modules_list -a conf/httpd.conf
+docker exec -i "${CONTAINER_NAME}" apachectl configtest
+docker exec -i "${CONTAINER_NAME}" apachectl -k graceful
+docker exec -i "${CONTAINER_NAME}" httpd -M
 
 # log for debugging
 docker inspect "${CONTAINER_NAME}"
 
 # find non-tls and tls port
-docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8080/tcp\"[0].HostPort"
-docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort"
-NON_TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8080/tcp\"[0].HostPort")
-TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort")
+docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort"
+docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"443/tcp\"[0].HostPort"
+NON_TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort")
+TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"443/tcp\"[0].HostPort")
 
 # run curl in loop for different endpoints
 for i in {1..20};
-do 
+do
     echo "Attempt $i"
         curl http://localhost:"${NON_TLS_PORT}"
-        with_backoff curl https://localhost:"${TLS_PORT}" -k -v
+        #with_backoff curl https://localhost:"${TLS_PORT}" -k -v
 done

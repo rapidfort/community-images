d4f33d79d88d44c06ffe1269a459a60f244121d6
Anmol Virdi <67299745+AnmolVirdi@users.noreply.github.com>
2022-11-04 05:10:36 +0530

    Apache official adder (#190)
    * module list check1
    
    * module list check2
    
    * module list check3
    
    * module list check4
    
    * module list check4
    
    * module list check5
    
    * module list check6
    
    * module list check7
    
    * module list check7
    
    * module list check8
    
    * module list check9
    
    * configuring httpd
    
    * adding mpms
    
    * adding mpms
    
    * adding ssl certs
    
    * adding ssl config
    
    * adding ssl config
    
    * adding ssl config
    
    * adding ssl config
    
    * adding ssl config
    
    * adding ssl config
    
    * coverage scripts in check
    
    * coverage scripts in check2
    
    * coverage scripts in check3
    
    * configs complete
    
    * trail
    
    * configured latest_tag_gen
    
    * configured image.yml
    
    * some final config changes
    
    * configured image.yml
    
    * Revert "configured image.yml"
    
    This reverts commit cf9213655c9a012ab793bc4dda80e18048a9e833.
    
    * Added workflows
    
    * added readme
    
    * finalized image.yml
    
    * finalized image.yml
    
    * removed comments from registry helper

--- community_images/apache/official/docker_coverage.sh
+++ community_images/apache/official/docker_coverage.sh
@@ -9,11 +9,11 @@ JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker coverage = $JSON"
 
-APACHE_HOST=$(jq -r '.container_details.apache.ip_address' < "$JSON_PARAMS")
+APACHE_HOST=$(jq -r '.container_details."apache-official".ip_address' < "$JSON_PARAMS")
 
 # Install Apache benchmark testing tool
 sudo apt-get install apache2-utils -y
 sudo apt-get install apache2 -y
 
 # testing using apache benchmark tool
-ab -t 100 -n 10000 -c 10 http://"${APACHE_HOST}":8080/
+ab -t 100 -n 10000 -c 10 http://"${APACHE_HOST}":80/
--- community_images/common/tests/common_commands.sh
+++ community_images/common/tests/common_commands.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 set -x
 set -e
 
 # Add common commands here which should be present in all hardened images
 
-function run_commands()
+run_commands()
 {
-    local cmd="$1"
-    if command -v "$cmd" --help &> /dev/null
+    cmd="$1"
+    if command -v "$cmd" --help > /dev/null
     then
         "$cmd" --help
     fi
@@ -16,17 +16,15 @@ function run_commands()
 
 sleep 1
 
-declare -a command_array=(
-    cp mkdir chmod ls mv rm ln rmdir chgrp chown touch cat grep sed tar sort head date
-)
+set -- cp mkdir chmod ls mv rm ln rmdir chgrp chown touch cat grep sed tar sort head date
 
-for cmd in "${command_array[@]}"
+for cmd in "$@"
 do
     run_commands "$cmd"
 done
 
 # add clear command
-if command -v clear -V &> /dev/null
+if command -v clear -V > /dev/null
 then
     clear -V
 fi

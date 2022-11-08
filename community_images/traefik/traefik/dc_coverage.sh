1d760b1c11128651ed145650a45f2baffb4ba411
David Dooley <davidd@rapidfort.com>
2022-11-07 19:53:42 -0500

    Adding TLS to Traefik
    Signed-off-by: David Dooley <davidd@rapidfort.com>

--- community_images/traefik/traefik/dc_coverage.sh
+++ community_images/traefik/traefik/dc_coverage.sh
@@ -13,17 +13,9 @@ PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
 
 NETWORK_NAME="${PROJECT_NAME}"_default
 
-
-# find non-tls and tls port
-docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8080/tcp\"[0].HostPort"
-docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort"
-NON_TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8080/tcp\"[0].HostPort")
-TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort")
-
 # run curl in loop for different endpoints
 for i in {1..3};
 do
     echo "Attempt $i"
-    docker run -d --net "$NETWORK_NAME" rapidfort/curl "-H Host:whoami.docker.localhost http://localhost:${NON_TLS_PORT}"
-    docker run -d --net "$NETWORK_NAME" rapidfort/curl with_backoff "-H Host:whoami.docker.localhost https://localhost:${TLS_PORT}" -k
+    curl -k -H Host:whoami.docker.localhost https://localhost:9443
 done

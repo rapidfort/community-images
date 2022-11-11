c2aeeab56efa942cf2649564c3b72a7f24098674
Vinod Gupta <vinod@rapidfort.com>
2022-11-10 17:01:46 -0800

    Add yugabytedb in new framework (#201)
    * add yugabyte db
    
    * fix runtime
    
    * Add term =xterm
    
    Signed-off-by: vinod <vinod@rapidfort.com>
    
    * add docker coverage
    
    * add sql script
    
    * fix runtime
    
    * fix runtime
    
    * fix runtime
    
    * add yugabyte db container wait
    
    * add httrack option
    
    * add yugabyte assets
    
    * generate files
    
    * update httrack
    
    * fix shellscript issues
    
    Signed-off-by: vinod <vinod@rapidfort.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -60,7 +60,7 @@ helm install \
 kubectl apply -f "${SCRIPTPATH}"/cert_manager.yml
 
 # install some helpers
-sudo apt-get install jq parallel expect -y
+sudo apt-get install jq parallel expect httrack -y
 
 # install docker-compose latest
 DC_VERSION="$(with_backoff curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)"
@@ -69,7 +69,7 @@ echo "Downloding  https://github.com/docker/compose/releases/download/${DC_VERSI
 with_backoff sudo curl -L https://github.com/docker/compose/releases/download/"${DC_VERSION}"/docker-compose-"$(uname -s)"-"$(uname -m)" -o "$DC_DESTINATION"
 sudo chmod 755 $DC_DESTINATION
 
-# upgrade bash, curl, openssl
+# upgrade bash, curl, openssl,
 sudo apt-get install --only-upgrade bash openssl curl -y
 bash --version
 

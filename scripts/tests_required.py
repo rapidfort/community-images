fd8538ef6bf7e3b09167e9f5d0b84e129454c988
Vinod Gupta <vinod@rapidfort.com>
2022-11-14 12:09:38 -0800

    fix pylint and structure code

--- scripts/tests_required.py
+++ scripts/tests_required.py
@@ -1,15 +1,34 @@
-# Script to check if github actions test is required
+""" Script to check if github actions test is required """
 import sys
+import logging
+
+
+def check_if_tests_required(image_name):
+    """ Check if tests required """
+    logging.info(image_name)
+    return False
+
+
+def output_test_required(output_file, tests_required):
+    """ Create output file for tests required """
+    test_req_str = "yes" if tests_required else "no"
+    with open(output_file, "w", encoding="utf8") as out_fp:
+        out_fp.write(f"TEST_REQUIRED={test_req_str}")
 
 
 def main():
-    print(sys.argv)
+    """ Main function """
+    logging.basicConfig(level=logging.DEBUG)
+
+    logging.info(sys.argv)
     if len(sys.argv) != 3:
         sys.exit(1)
 
+    image_name = sys.argv[1]
     output_file = sys.argv[2]
-    with open(output_file, "w", encoding="utf8") as fp:
-        fp.write("TEST_REQUIRED=yes")
+    tests_required = check_if_tests_required(image_name)
+    output_test_required(output_file, tests_required)
+
 
 if __name__ == "__main__":
     main()

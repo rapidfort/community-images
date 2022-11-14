075589d0ae56bcc66f19fc130d1046604d15be94
Vinod Gupta <vinod@rapidfort.com>
2022-11-14 11:53:46 -0800

    check if tests required

---
+++ scripts/tests_required.py
@@ -0,0 +1,15 @@
+# Script to check if github actions test is required
+import sys
+
+
+def main():
+    print(sys.argv)
+    if len(sys.argv) != 3:
+        sys.exit(1)
+
+    output_file = sys.argv(2)
+    with open(output_file, "w", encoding="utf8") as fp:
+        fp.write("TEST_REQUIRED=yes")
+
+if __name__ == "__main__":
+    main()

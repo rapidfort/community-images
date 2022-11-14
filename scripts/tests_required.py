a75f0e2f226f899df0443cd0d8fa1a4c061fb837
Vinod Gupta <vinod@rapidfort.com>
2022-11-14 11:56:00 -0800

    Fix python script

--- scripts/tests_required.py
+++ scripts/tests_required.py
@@ -7,7 +7,7 @@ def main():
     if len(sys.argv) != 3:
         sys.exit(1)
 
-    output_file = sys.argv(2)
+    output_file = sys.argv[2]
     with open(output_file, "w", encoding="utf8") as fp:
         fp.write("TEST_REQUIRED=no")
 

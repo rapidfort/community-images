fe79835982d788b9f4979f154364826c6ea9f62c
AnmolVirdi <anmolvirdi70@gmail.com>
2022-11-16 23:50:27 +0530

    test3

--- community_images/haproxy/official/dc_coverage_tcp.sh
+++ community_images/haproxy/official/dc_coverage_tcp.sh
@@ -14,48 +14,42 @@ JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker compose coverage = $JSON"
 
-CONTAINER_NAME=haproxy
+# identity mode
+CONTAINER1_NAME=haproxy-1
+# gzip mode
+CONTAINER2_NAME=haproxy-2
+# deflate mode
+CONTAINER3_NAME=haproxy-3
+
 
 # log for debugging
 docker inspect "${CONTAINER_NAME}"
 
-# finding port
-docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort"
-PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort")
+# finding ports
+docker inspect "${CONTAINER1_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort"
+PORT1=$(docker inspect "${CONTAINER1_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort")
+docker inspect "${CONTAINER2_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort"
+PORT2=$(docker inspect "${CONTAINER2_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort")
+docker inspect "${CONTAINER3_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort"
+PORT3=$(docker inspect "${CONTAINER3_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort")
 
-# run curl in loop (roundrobin)
+# run curl in loop (identity)
 for i in {1..5};
 do 
     echo "Attempt $i"
-    curl http://localhost:"${PORT}"
+    curl http://localhost:"${PORT1}"
 done
 
-# Changing compression algo from identity to gzip
-docker exec -i "${CONTAINER_NAME}" cp /usr/local/etc/haproxy/haproxy.cfg /usr/local/etc/haproxy.cfg
-docker exec -i "${CONTAINER_NAME}" sed -i 's/identity/gzip/g' /usr/local/etc/haproxy.cfg
-docker exec -i "${CONTAINER_NAME}" cp /usr/local/etc/haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
-# reloading
-docker kill -s HUP haproxy
-sleep 5
-# Checking leastconn
-# run curl in loop
-for i in {1..4};
+# run curl in loop (gzip)
+for i in {1..5};
 do 
     echo "Attempt $i"
-    curl http://localhost:"${PORT}"
+    curl http://localhost:"${PORT2}"
 done
 
-# Changing compression algo from gzip to deflate
-docker exec -i "${CONTAINER_NAME}" cp /usr/local/etc/haproxy/haproxy.cfg /usr/local/etc/haproxy.cfg
-docker exec -i "${CONTAINER_NAME}" sed -i 's/gzip/deflate/g' /usr/local/etc/haproxy.cfg
-docker exec -i "${CONTAINER_NAME}" cp /usr/local/etc/haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
-# reloading
-docker kill -s HUP haproxy
-sleep 5
-# Checking source mode
-# run curl in loop
-for i in {1..10};
+# run curl in loop (deflate)
+for i in {1..5};
 do 
     echo "Attempt $i"
-    curl http://localhost:"${PORT}"
+    curl http://localhost:"${PORT3}"
 done

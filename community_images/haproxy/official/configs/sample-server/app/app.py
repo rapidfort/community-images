5913517a9a3578e5352f4174dc730671855292db
Anmol Virdi <67299745+AnmolVirdi@users.noreply.github.com>
2022-11-17 03:25:17 +0530

    Haproxy adder (#209)
    * added haproxy official
    
    * bug fix
    
    * image.yml fix
    
    * minute check
    
    * check1
    
    * check2
    
    * added to image list
    
    * changed config
    
    * changed tcp config
    
    * test
    
    * test2
    
    * test3
    
    * test4
    
    * test5
    
    * test6
    
    * test7
    
    * rebuild scripts
    
    * remove unnecessary overrides.yml file
    
    * fix runtime
    
    * fix docker links
    
    * updated readme
    
    Co-authored-by: davidd <davidd@rapidfort.com>
    Co-authored-by: Vinod Gupta <vinod@rapidfort.com>

---
+++ community_images/haproxy/official/configs/sample-server/app/app.py
@@ -0,0 +1,48 @@
+""" Sample flask app for testing """
+import socket
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """ add index route """
+    try:
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        return render_template('index.html', hostname=host_name, ip=host_ip, message = "Home page")
+    except Exception as _: # pylint: disable=broad-except
+        return render_template('error.html')
+
+@app.route("/app1")
+def app1():
+    """ app1 route """
+    try:
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        return render_template('index.html', hostname=host_name, ip=host_ip, message = "App1 page")
+    except Exception as _: # pylint: disable=broad-except
+        return render_template('error.html')
+
+@app.route("/app2")
+def app2():
+    """ app2 route """
+    try:
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        return render_template('index.html', hostname=host_name, ip=host_ip, message = "App2 page")
+    except Exception as _: # pylint: disable=broad-except
+        return render_template('error.html')
+
+@app.route("/admin")
+def admin():
+    """ admin route """
+    try:
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        return render_template('index.html', hostname=host_name, ip=host_ip, message = "Admin page (privileged)")
+    except Exception as _: # pylint: disable=broad-except
+        return render_template('error.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)

b6f5b30a32a4837f3e75f299964fa711bcf3323f
Reuben Santhan O <reuben@rapidfort.com>
2022-11-24 10:47:30 -0800

    updated docker coverage scripts and created cql tests
    Signed-off-by: Reuben Santhan O <reuben@rapidfort.com>

--- community_images/cassandra/official/docker_coverage.sh
+++ community_images/cassandra/official/docker_coverage.sh
@@ -9,5 +9,13 @@ JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker coverage = $JSON"
 
-# NETWORK_NAME=$(jq -r '.network_name' < "$JSON_PARAMS")
-# ENVOY_HOST=$(jq -r '.container_details.envoy.ip_address' < "$JSON_PARAMS")
+RAPIDFORT_ACCOUNT="${RAPIDFORT_ACCOUNT:-rapidfort}"
+NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
+
+# get docker host ip
+CASSANDRA_HOST=$(jq -r '.container_details."cassandra-official".ip_address' < "$JSON_PARAMS")
+REPO_PATH=$(jq -r '.image_tag_details."cassandra-official".repo_path' < "$JSON_PARAMS")
+TAG=$(jq -r '.image_tag_details."cassandra-official".tag' < "$JSON_PARAMS")
+
+# run docker
+docker run --rm -i --cap-add=SYS_PTRACE --name="${NAMESPACE}"$(date +%s) -d "${REPO_PATH}:${TAG}"

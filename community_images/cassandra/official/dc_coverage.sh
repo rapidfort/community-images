34390acb488b5200229e68d22337336f94d886e6
Reuben Santhan O <reuben@rapidfort.com>
2022-11-28 15:06:20 -0800

    updated coverage scripts to fix shellcheck warnings
    Signed-off-by: Reuben Santhan O <reuben@rapidfort.com>

--- community_images/cassandra/official/dc_coverage.sh
+++ community_images/cassandra/official/dc_coverage.sh
@@ -15,9 +15,9 @@ cp -v ./test.cql /tmp/
 
 sleep 60
 
-sudo docker exec -i $(docker ps | grep $NAMESPACE-cassandra-1 | cut -f1 -d' ') bash -c 'cqlsh' < "/tmp/test.cql"
+docker exec -i $(docker ps | grep "$NAMESPACE"-cassandra-1 | cut -f1 -d' ') bash -c 'cqlsh' < "/tmp/test.cql"
 
 sleep 10
 
-sudo docker rm --force $(sudo docker ps -a | grep $NAMESPACE |cut -f1 -d' ')
+docker rm --force $(sudo docker ps -a | grep "$NAMESPACE" |cut -f1 -d' ')
 
--- community_images/cassandra/official/docker_coverage.sh
+++ community_images/cassandra/official/docker_coverage.sh
@@ -13,9 +13,9 @@ RAPIDFORT_ACCOUNT="${RAPIDFORT_ACCOUNT:-rapidfort}"
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 
 # get docker host ip
-CASSANDRA_HOST=$(jq -r '.container_details."cassandra-official".ip_address' < "$JSON_PARAMS")
+#CASSANDRA_HOST=$(jq -r '.container_details."cassandra-official".ip_address' < "$JSON_PARAMS")
 REPO_PATH=$(jq -r '.image_tag_details."cassandra-official".repo_path' < "$JSON_PARAMS")
 TAG=$(jq -r '.image_tag_details."cassandra-official".tag' < "$JSON_PARAMS")
 
 # run docker
-docker run --rm -i --cap-add=SYS_PTRACE --name="${NAMESPACE}"$(date +%s) -d "${REPO_PATH}:${TAG}"
+docker run --rm -i --cap-add=SYS_PTRACE --name="${NAMESPACE}"-"$(date +%s)" -d "${REPO_PATH}:${TAG}"

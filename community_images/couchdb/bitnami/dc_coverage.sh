39a64f32d0eab329d93362b8f50535a771cb2422
Ankit Kumar <ankit.kumar@rubrik.com>
2022-11-29 23:45:39 +0530

    update

--- community_images/couchdb/bitnami/dc_coverage.sh
+++ community_images/couchdb/bitnami/dc_coverage.sh
@@ -3,6 +3,12 @@
 set -x
 set -e
 
+# shellcheck disable=SC1091
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/coverage.sh
+
 JSON_PARAMS="$1"
 
 JSON=$(cat "$JSON_PARAMS")
@@ -10,3 +16,4 @@ JSON=$(cat "$JSON_PARAMS")
 echo "Json params for docker compose coverage = $JSON"
 
 # PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
+test_couchdb
--- community_images/couchdb/bitnami/docker_coverage.sh
+++ community_images/couchdb/bitnami/docker_coverage.sh
@@ -3,6 +3,12 @@
 set -x
 set -e
 
+# shellcheck disable=SC1091
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/coverage.sh
+
 JSON_PARAMS="$1"
 
 JSON=$(cat "$JSON_PARAMS")

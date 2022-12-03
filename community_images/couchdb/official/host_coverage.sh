ecec70e5c9fa3559d4bec6864c4213bdc918a7ce
Vinod Gupta <vinod@rapidfort.com>
2022-12-03 14:34:29 -0800

    Add couchDB official image

---
+++ community_images/couchdb/official/host_coverage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -x
+
+couchjs -V || echo "couchjs didn't run properly"
+
+remsh -h || echo "couldn't run remsh"
+
+couchdb --version || echo "couldn't get couchdb version"

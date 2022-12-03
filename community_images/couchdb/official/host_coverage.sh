fc44204a2e529cf0d508fe36e985d8f5716ec72c
Vinod Gupta <vinod@rapidfort.com>
2022-12-03 14:57:54 -0800

    fix runtime

--- community_images/couchdb/official/host_coverage.sh
+++ community_images/couchdb/official/host_coverage.sh
@@ -3,8 +3,8 @@
 set -e
 set -x
 
-couchjs -V || echo "couchjs didn't run properly"
+/opt/couchdb/bin/couchjs -V || echo "couchjs didn't run properly"
 
-remsh -h || echo "couldn't run remsh"
+/opt/couchdb/bin/remsh -h || echo "couldn't run remsh"
 
-couchdb --version || echo "couldn't get couchdb version"
+/opt/couchdb/bin/couchdb --version || echo "couldn't get couchdb version"

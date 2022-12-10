86671557ef047b931bf742e1bc90c1005155d437
Ankit Kumar <ankitbansal.nitj@gmail.com>
2022-12-10 15:02:43 +0530

    update

--- community_images/elastic/bitnami/docker_coverage.sh
+++ community_images/elastic/bitnami/docker_coverage.sh
@@ -15,9 +15,10 @@ SCRIPTPATH="$(
 JSON_PARAMS="$1"
 
 JSON=$(cat "$JSON_PARAMS")
+NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 
 echo "Json params for docker coverage = $JSON"
 
 ES_SERVER="127.0.0.1"
 # run coverage script
-test_elasticsearch "${ES_SERVER}" "yes"
+test_elasticsearch "${ES_SERVER}" "yes" "${NAMESPACE}"

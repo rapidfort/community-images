662b4a9c4783fff0f9cd088bd28943eceffa75e2
Ankit Kumar <ankitbansal.nitj@gmail.com>
2022-12-10 14:46:21 +0530

    update

--- community_images/elastic/bitnami/docker_coverage.sh
+++ community_images/elastic/bitnami/docker_coverage.sh
@@ -3,11 +3,21 @@
 set -x
 set -e
 
+# shellcheck disable=SC1091
+SCRIPTPATH="$(
+    cd -- "$(dirname "$0")" >/dev/null 2>&1
+    pwd -P
+)"
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/coverage.sh
+
 JSON_PARAMS="$1"
 
 JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker coverage = $JSON"
 
-# NETWORK_NAME=$(jq -r '.network_name' < "$JSON_PARAMS")
-# ENVOY_HOST=$(jq -r '.container_details.envoy.ip_address' < "$JSON_PARAMS")
+ES_SERVER="127.0.0.1"
+# run coverage script
+test_elasticsearch "${ES_SERVER}"

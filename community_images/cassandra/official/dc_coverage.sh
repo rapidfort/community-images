b9cfa10ea7787fadc6ed3012736cb5b0b4487804
Reuben Santhan <reuben@rapidfort.com>
2022-12-12 13:01:51 -0800

    updated input image tag for latest image
    Signed-off-by: Reuben Santhan <reuben@rapidfort.com>

--- community_images/cassandra/official/dc_coverage.sh
+++ community_images/cassandra/official/dc_coverage.sh
@@ -10,9 +10,6 @@ JSON=$(cat "$JSON_PARAMS")
 echo "Json params for docker compose coverage = $JSON"
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 
-
-#cp -v ./test.cql /tmp/
-
 sleep 60
 
 docker exec -i "$(docker ps | grep "$NAMESPACE"-cassandra-1 | cut -f1 -d' ')" bash -c 'cqlsh -u cassandra -p cassandra < /opt/test.cql'

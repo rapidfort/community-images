994547007d48170aae7aae000e505365b2891aa1
AnmolVirdi <anmolvirdi70@gmail.com>
2023-01-07 16:35:45 +0530

    modified dc_coverage for postgresl bitnami
    Signed-off-by: AnmolVirdi <anmolvirdi70@gmail.com>

--- community_images/postgresql/bitnami/dc_coverage.sh
+++ community_images/postgresql/bitnami/dc_coverage.sh
@@ -11,14 +11,8 @@ echo "Json params for docker compose coverage = $JSON"
 
 PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
 
-# password
-POSTGRESQL_PASSWORD=my_password
-
 # pg container
 PG_CONTAINER="${PROJECT_NAME}"-postgresql-master-1
-
-# password
-POSTGRESQL_PASSWORD=my_password
 
 # Get Port
 #PG_PORT=$(docker inspect "${PG_CONTAINER}" | jq -r ".[].NetworkSettings.Ports.\"5432/tcp\"[0].HostPort")

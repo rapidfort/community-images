646a499979f48916a16a806cb19c68b6cd907f1b
AnmolVirdi <anmolvirdi70@gmail.com>
2023-01-10 17:54:34 +0530

    Added ghost image's initial files
    Signed-off-by: AnmolVirdi <anmolvirdi70@gmail.com>

---
+++ community_images/ghost/bitnami/dc_coverage.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -x
+set -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/../../common/scripts/bash_helper.sh
+
+JSON_PARAMS="$1"
+
+JSON=$(cat "$JSON_PARAMS")
+
+echo "Json params for docker compose coverage = $JSON"
+
+PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
+
+CONTAINER_NAME="${PROJECT_NAME}"-apache-1
+
+# exec into container and run coverage script
+docker exec -i "${CONTAINER_NAME}" bash -c /opt/bitnami/scripts/coverage_script.sh
+
+# log for debugging
+docker inspect "${CONTAINER_NAME}"
+
+# find non-tls and tls port
+docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8080/tcp\"[0].HostPort"
+docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort"
+NON_TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8080/tcp\"[0].HostPort")
+TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort")
+
+# run curl in loop for different endpoints
+for i in {1..20};
+do 
+    echo "Attempt $i"
+        curl http://localhost:"${NON_TLS_PORT}"
+        with_backoff curl https://localhost:"${TLS_PORT}" -k -v
+done
+

8aeae7cc26d6f5d42116c673662e6a6da7a03bba
AnmolVirdi <anmolvirdi70@gmail.com>
2023-02-02 04:56:16 +0530

    Added selenium tests, docker runtime support
    Signed-off-by: AnmolVirdi <anmolvirdi70@gmail.com>

--- community_images/ghost/bitnami/dc_coverage.sh
+++ community_images/ghost/bitnami/dc_coverage.sh
@@ -15,26 +15,14 @@ JSON=$(cat "$JSON_PARAMS")
 echo "Json params for docker compose coverage = $JSON"
 
 PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
-
-CONTAINER_NAME="${PROJECT_NAME}"-apache-1
+PORT=8080
+CONTAINER_NAME="${PROJECT_NAME}"-ghost-1
 
 # exec into container and run coverage script
-docker exec -i "${CONTAINER_NAME}" bash -c /opt/bitnami/scripts/coverage_script.sh
+docker exec -i "${CONTAINER_NAME}" cat /opt/bitnami/ghost/config.production.json
 
 # log for debugging
 docker inspect "${CONTAINER_NAME}"
 
-# find non-tls and tls port
-docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8080/tcp\"[0].HostPort"
-docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort"
-NON_TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8080/tcp\"[0].HostPort")
-TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort")
-
-# run curl in loop for different endpoints
-for i in {1..20};
-do 
-    echo "Attempt $i"
-        curl http://localhost:"${NON_TLS_PORT}"
-        with_backoff curl https://localhost:"${TLS_PORT}" -k -v
-done
-
+# Running selenium tests
+"${SCRIPTPATH}"/../../common/selenium_tests/runner-dc.sh "${PROJECT_NAME}" "${PORT}" "${SCRIPTPATH}"/selenium_tests 2>&1
--- community_images/ghost/bitnami/selenium_tests/conftest.py
+++ community_images/ghost/bitnami/selenium_tests/conftest.py
@@ -7,9 +7,9 @@ import pytest  # pylint: disable=import-error
 
 def pytest_addoption(parser):
     """The function to add options"""
-    parser.addoption("--server", action="store", help="wordpress server")
+    parser.addoption("--server", action="store", help="Ghost server")
     parser.addoption("--port", action="store",
-                     help="port on host linked to yourls container")
+                     help="port on host linked to Ghost container")
 
 
 @pytest.fixture

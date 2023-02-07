0ac15ddcb162be778532100c542ed5c9899bb97f
Ankit Kumar <ankitbansal.nitj@gmail.com>
2023-02-07 22:48:19 +0530

    update

--- community_images/azure-sql-edge/mcr.microsoft.com/test_azure_sql.py
+++ community_images/azure-sql-edge/mcr.microsoft.com/test_azure_sql.py
@@ -3,6 +3,14 @@
 import pyodbc
 
 def main():
+    def _execute_and_print(cursor, query, fetch_rows=False):
+        cursor.execute(query)
+        if fetch_rows:
+            row = cursor.fetchone()
+            while row:
+                print('row is ' + str(row))
+                row = cursor.fetchone()
+
     server = 'tcp:localhost,57000'
     database = 'master'
     username = 'SA'
@@ -14,7 +22,7 @@ def main():
 
     #Sample select query
     version_query = "SELECT @@version;"
-    _execute_and_print(cursor, version_query)
+    _execute_and_print(cursor, version_query, fetch_rows=True)
 
     show_tables_query = '''
     select schema_name(t.schema_id) as schema_name,
@@ -25,12 +33,11 @@ def main():
     order by schema_name,
             table_name;
     '''
-    _execute_and_print(cursor, show_tables_query)
+    _execute_and_print(cursor, show_tables_query, fetch_rows=True)
 
     create_table_query = """
     IF EXISTS ( SELECT [name] FROM sys.tables WHERE [name] = 'HotelMaster' )   
-    DROP TABLE HotelMaster   
-    GO   
+    DROP TABLE HotelMaster
         
     CREATE TABLE HotelMaster   
     (   
@@ -55,10 +62,9 @@ def main():
     select_query = """
     select * from dbo.HotelMaster
     """
-    _execute_and_print(cursor, select_query)
+    _execute_and_print(cursor, select_query, fetch_rows=True)
 
     stored_procedure_query = """
-    GO  
     CREATE PROCEDURE dbo.uspGetEmployeesTest2
         @RoomType nvarchar(50),
         @Prize nvarchar(50)
@@ -68,27 +74,19 @@ def main():
     SELECT RoomNo, RoomType, Prize
     FROM dbo.HotelMaster
     WHERE RoomType = @RoomType AND Prize = @Prize
-    GO  
 
     EXECUTE dbo.uspGetEmployeesTest2 N'Single', N'100$';
     """
     _execute_and_print(cursor, stored_procedure_query)
     
     get_row_count_query = "SELECT COUNT(*) FROM dbo.HotelMaster"
-    _execute_and_print(cursor, get_row_count_query)
+    _execute_and_print(cursor, get_row_count_query, fetch_rows=True)
 
     get_single_room_count_query = "SELECT COUNT(*) FROM dbo.HotelMaster WHERE RoomType = 'Single'"
-    _execute_and_print(cursor, get_single_room_count_query)
+    _execute_and_print(cursor, get_single_room_count_query, fetch_rows=True)
 
     drop_table_query = "DROP TABLE dbo.HotelMaster"
     _execute_and_print(cursor, drop_table_query)
-
-def _execute_and_print(cursor, query):
-    cursor.execute(query)
-    row = cursor.fetchone()
-    while row:
-        print('row is ' + str(row))
-        row = cursor.fetchone()
 
 if __name__ == "__main__":
     main()

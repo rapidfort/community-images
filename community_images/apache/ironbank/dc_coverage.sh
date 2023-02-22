653c9e4e8387a61aaa535ef495ad0afde50b46c6
AnmolVirdi <anmolvirdi70@gmail.com>
2023-02-22 15:50:46 +0530

    adding test
    Signed-off-by: AnmolVirdi <anmolvirdi70@gmail.com>

--- community_images/apache/ironbank/dc_coverage.sh
+++ community_images/apache/ironbank/dc_coverage.sh
@@ -59,3 +59,5 @@ sudo apt-get install apache2-utils -y
 sudo apt-get install apache2 -y
 
 APACHE_HOST=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Networks.\""${PROJECT_NAME}"_default\".IPAddress")
+# testing using apache benchmark tool
+ab -t 100 -n 10000 -c 10 http://"${APACHE_HOST}":80/

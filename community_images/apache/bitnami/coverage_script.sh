586ef98db6809ca6a66ef64ef5b71bc8fadf56e2
Rajat <91206945+Rajat0002@users.noreply.github.com>
2023-03-01 16:25:17 +0530

    Update coverage_script.sh

--- community_images/apache/bitnami/coverage_script.sh
+++ community_images/apache/bitnami/coverage_script.sh
@@ -10,7 +10,5 @@ cat /opt/bitnami/scripts/modules_list >> /opt/bitnami/apache2/conf/httpd.conf
 /opt/bitnami/scripts/apache/reload.sh
 /opt/bitnami/scripts/apache/status.sh
 
-/lib/init/vars.sh
-
 httpd -M
 #Modules excluded: ["unixd_module" "pagespeed_module" "pagespeed_ap24_module" "mpm_worker_module" "mpm_event_module"]

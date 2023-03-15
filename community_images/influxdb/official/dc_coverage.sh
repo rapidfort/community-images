4c613dcd92c21df0a7d15f6a1fb26078d28aac64
ashishujjain <ashishsingh2000@yahoo.com>
2023-03-15 16:23:45 +0000

    fixing the review comments
    Signed-off-by: ashishujjain <ashishsingh2000@yahoo.com>

--- community_images/influxdb/official/dc_coverage.sh
+++ community_images/influxdb/official/dc_coverage.sh
@@ -12,6 +12,7 @@ echo "$SCRIPTPATH"
 
 # Source Env Variable for Variable replacement
 . "${SCRIPTPATH}"/docker.env.temp
+#. "${SCRIPTPATH}"/docker.env.ashish
 
 INFLUXDB_TOKEN=${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN}
 echo "INFLUXDB_TOKEN=${INFLUXDB_TOKEN}"
@@ -30,27 +31,33 @@ echo "Json params for docker compose coverage = $JSON"
 
 PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
 CONTAINER_NAME="${PROJECT_NAME}"-influxdb-1
+#CONTAINER_NAME=influxdb-official-ashish-influxdb-1
 
 # Wait for all influxdb server to set up
 sleep 20
 
 # log for debugging
 docker inspect "${CONTAINER_NAME}"
-#echo $?
+
 # find non-tls and tls port
 PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8086/tcp\"[0].HostPort")
 echo "influxdb port = $PORT"
 
+# Running the influxdb setup
+docker exec -it "${CONTAINER_NAME}" /bin/bash -c "influx setup --skip-verify --bucket ${DOCKER_INFLUXDB_INIT_BUCKET} --token ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN} --org ${DOCKER_INFLUXDB_INIT_ORG} --username ${DOCKER_INFLUXDB_INIT_USERNAME} --password ${DOCKER_INFLUXDB_INIT_PASSWORD} --force"
+
+echo "======================================================================"
+
 # copy tests into container
 docker cp ${SCRIPTPATH}/tests/example.csv "${CONTAINER_NAME}":/tmp/
-#echo $?
 docker cp ${SCRIPTPATH}/tests/query.flux "${CONTAINER_NAME}":/tmp/
-#echo $?
+
 # executing tests in the container, write data to influxdb
 docker exec -it "${CONTAINER_NAME}" /bin/bash -c "influx write -t $INFLUXDB_TOKEN -b $INFLUXDB_INIT_BUCKET --org-id $INFLUXDB_INIT_ORG -f /tmp/example.csv"
-#echo $?
+
 # run query on db
 docker exec -it "${CONTAINER_NAME}" /bin/bash -c "influx query -t $INFLUXDB_TOKEN --org $INFLUXDB_INIT_ORG -f /tmp/query.flux"
-#echo $?
+
 echo "Done testing the scripts, waiting for 10 seconds"
+
 sleep 10

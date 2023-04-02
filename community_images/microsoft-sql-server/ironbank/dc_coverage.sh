32b1be13f85eee66ddc84f04accf0e7cbe79b86a
AnmolVirdi <anmolvirdi70@gmail.com>
2023-04-02 17:14:30 +0530

    test
    Signed-off-by: AnmolVirdi <anmolvirdi70@gmail.com>

---
+++ community_images/microsoft-sql-server/ironbank/dc_coverage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+set -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/../../common/scripts/bash_helper.sh
+
+JSON_PARAMS="$1"
+
+JSON=$(cat "$JSON_PARAMS")
+
+echo "Json params for docker compose coverage = $JSON"
+
+PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
+
+CONTAINER_NAME="${PROJECT_NAME}"-microsoft-sql-server-1
+
+# checking all modules and config test
+docker exec -i "${CONTAINER_NAME}" bash -c sqlcmd -S localhost -U SA -P 'rapidfortpassword' -f /tmp/test.ms_sql
+# /opt/mssql-tools/bin/sqlcmd

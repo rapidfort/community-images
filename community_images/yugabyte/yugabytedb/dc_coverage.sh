3c4aedd1dd372f590edefc4ead6f6a5333080940
Anmol Virdi <anmol@rapidfort.com>
2023-04-18 21:44:57 +0530

    Added fixes to yugabyte
    Signed-off-by: Anmol Virdi <anmol@rapidfort.com>

---
+++ community_images/yugabyte/yugabytedb/dc_coverage.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -x
+set -e
+
+JSON_PARAMS="$1"
+
+JSON=$(cat "$JSON_PARAMS")
+
+echo "Json params for docker compose coverage = $JSON"
+
+PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
+CONTAINER_NAME="${PROJECT_NAME}"-yugabyte-1
+
+# log for debugging
+docker inspect "${CONTAINER_NAME}"
+
+# wait for container to be up
+with_backoff docker exec -i "${CONTAINER_NAME}" /bin/bash -c \
+    "./bin/yugabyted status"
+
+# copy test.psql into container
+docker cp "${SCRIPTPATH}"/../../common/tests/test.psql "${CONTAINER_NAME}":/tmp/test.psql
+
+# run script
+docker exec -i "${CONTAINER_NAME}" /bin/bash -c ysqlsh -h ${YB_HOST} -p 5433 -U yugabyte -d yugabyte -f /tmp/test.psql
+
+# ysqlsh and ycqlsh
+docker exec -i "${CONTAINER_NAME}" /bin/bash -c ysqlsh --version
+docker exec -i "${CONTAINER_NAME}" /bin/bash -c ycqlsh --version
+
+# exercise all webpages
+UI_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"15433/tcp\"[0].HostPort")
+YU_HOST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${CONTAINER_NAME}")
+HTML_DIR="${SCRIPTPATH}"/html_output
+mkdir -p "${HTML_DIR}"
+httrack http://"${YB_HOST}":"${UI_PORT}" -O "${HTML_DIR}"
+rm -rf "${HTML_DIR}"

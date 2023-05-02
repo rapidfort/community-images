a7d6ef2bc06e93bbe77e714a70321a0ceed9fc59
Anmol Virdi <anmol@rapidfort.com>
2023-05-03 00:25:43 +0530

    Fixed docker compose runtime
    Signed-off-by: Anmol Virdi <anmol@rapidfort.com>

--- community_images/rabbitmq/official/dc_coverage.sh
+++ community_images/rabbitmq/official/dc_coverage.sh
@@ -8,3 +8,21 @@ JSON_PARAMS="$1"
 JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker compose coverage = $JSON"
+
+# shellcheck disable=SC1091
+SCRIPTPATH="$(
+    cd -- "$(dirname "$0")" >/dev/null 2>&1
+    pwd -P
+)"
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/coverage.sh
+
+JSON_PARAMS="$1"
+
+NAMESPACE=$(jq -r '.namespace_name' <"$JSON_PARAMS")
+RELEASE_NAME=$(jq -r '.release_name' <"$JSON_PARAMS")
+
+RABBITMQ_PASS=password
+# run coverage script
+test_rabbitmq "${NAMESPACE}" rabbitmq_server "${RABBITMQ_PASS}"
--- community_images/rabbitmq/official/docker_coverage.sh
+++ community_images/rabbitmq/official/docker_coverage.sh
@@ -8,21 +8,3 @@ JSON_PARAMS="$1"
 JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker coverage = $JSON"
-
-# shellcheck disable=SC1091
-SCRIPTPATH="$(
-    cd -- "$(dirname "$0")" >/dev/null 2>&1
-    pwd -P
-)"
-
-# shellcheck disable=SC1091
-. "${SCRIPTPATH}"/coverage.sh
-
-JSON_PARAMS="$1"
-
-NAMESPACE=$(jq -r '.namespace_name' <"$JSON_PARAMS")
-RELEASE_NAME=$(jq -r '.release_name' <"$JSON_PARAMS")
-
-RABBITMQ_PASS=password
-# run coverage script
-test_rabbitmq "${NAMESPACE}" rabbitmq_server "${RABBITMQ_PASS}"

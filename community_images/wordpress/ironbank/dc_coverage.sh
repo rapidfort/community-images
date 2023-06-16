4c42329c80fa4ff07cc6b1d7cebaf5e48f1b6aba
anmol <anmol@rapidfort.com>
2023-06-16 06:53:50 -0700

    Added wordpress/ironbank image

---
+++ community_images/wordpress/ironbank/dc_coverage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+set -e
+
+JSON_PARAMS="$1"
+
+JSON=$(cat "$JSON_PARAMS")
+
+echo "Json params for docker compose coverage = $JSON"
+
+PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
+
+# shellcheck disable=SC1091
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+WORDPRESS_PORT='8080'
+"${SCRIPTPATH}"/../../common/selenium_tests/runner-dc.sh "${PROJECT_NAME}" "${WORDPRESS_PORT}" "${SCRIPTPATH}"/selenium_tests 2>&1

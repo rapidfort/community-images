91c36d9f3d19d85ae88c830bf4a8b66c00e41699
anmol <anmol@rapidfort.com>
2023-06-27 03:13:35 -0700

    Pylint fix
    Signed-off-by: anmol <anmol@rapidfort.com>

--- community_images/common/orchestrator/registry_helper.py
+++ community_images/common/orchestrator/registry_helper.py
@@ -249,25 +249,26 @@ class IronBankHelper(RegistryHelper):
             url = f"{self.BASE_URL}/api/v2.0/projects/{account}/repositories/{url_safe_repo}/artifacts?page_size=100"
             auth = HTTPBasicAuth(self.username, self.password)
 
-            for page in range(0,100):
-                page_url = f"{url}&page={page}"
-                resp = requests.get(page_url, auth=auth, timeout=120)
-                logging.debug(f"page_url : {page_url}, {resp.status_code}, {resp.text}")
-                if 200 <= resp.status_code < 300:
-                    artifacts = resp.json()
-                    if len(artifacts) == 0:
-                        break
-                    for artifact in artifacts:
-                        local_tag_list = artifact.get("tags")
-                        logging.debug(f"artifact={artifact}")
-                        if local_tag_list:
-                            # ingest the digest into all the tags
-                            sha_digest = artifact.get("digest")
-                            for local_tag in local_tag_list:
-                                local_tag["digest"] = sha_digest
-                            tags += local_tag_list
-                else:
-                    break
+            for page in range(0, 100):
+            page_url = f"{url}&page={page}"
+            resp = requests.get(page_url, auth=auth, timeout=120)
+            logging.debug(f"page_url: {page_url}, {resp.status_code}, {resp.text}")
+
+            if not (200 <= resp.status_code < 300):
+                break
+            artifacts = resp.json()
+            if len(artifacts) == 0:
+                break
+
+            for artifact in artifacts:
+                local_tag_list = artifact.get("tags")
+                logging.debug(f"artifact={artifact}")
+                if local_tag_list:
+                    sha_digest = artifact.get("digest")
+                    for local_tag in local_tag_list:
+                        local_tag["digest"] = sha_digest
+                    tags += local_tag_list
+
         return tags
 
     def get_auth_header(self):

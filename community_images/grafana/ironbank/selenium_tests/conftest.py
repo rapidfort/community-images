bc1ea40313d66adb836167fd549ff50c245840f0
Kirandeep-Singh-Khehra <107160937+Kirandeep-Singh-Khehra@users.noreply.github.com>
2023-07-04 11:27:39 +0530

    Grafana ironbank adder (#322)
    * Added selenium tests
    
    Signed-off-by: Kirandeep Singh <kirandeep@rapidfort.com>
    
    * Added grafana/ironbank image directory. and tested stub coverage.
    
    Signed-off-by: Kirandeep Singh <kirandeep@rapidfort.com>
    
    * Code clean up after successful run of harden_coverage.
    
    Signed-off-by: Kirandeep Singh <kirandeep@rapidfort.com>
    
    * Updated scripts to setup plugins that need restart of grafana container.
    
    Signed-off-by: Kirandeep Singh <kirandeep@rapidfort.com>
    
    * Added workflows for grafana/ironbank
    
    Signed-off-by: Kirandeep Singh <kirandeep@rapidfort.com>
    
    * Removed interactive and tty flag from docker exec commands
    
    Signed-off-by: Kirandeep Singh <kirandeep@rapidfort.com>
    
    * Cleanup
    
    Signed-off-by: Kirandeep Singh <kirandeep@rapidfort.com>
    
    * Used rapidfort/prometheus instead of prom/prometheus.
    
    Signed-off-by: Kirandeep Singh <kirandeep@rapidfort.com>
    
    * Updated dc_coverage as per suggested by review-dog suggestions.
    
    Signed-off-by: Kirandeep Singh <kirandeep@rapidfort.com>
    
    ---------
    
    Signed-off-by: Kirandeep Singh <kirandeep@rapidfort.com>
    Co-authored-by: Kirandeep Singh <kirandeep@rapidfort.com>
    Co-authored-by: Anmol Virdi <67299745+AnmolVirdi@users.noreply.github.com>

---
+++ community_images/grafana/ironbank/selenium_tests/conftest.py
@@ -0,0 +1,23 @@
+"""The conftest file for running selenium test."""
+# pylint: skip-file
+
+# conftest.py
+import pytest  # pylint: disable=import-error
+
+
+def pytest_addoption(parser):
+    """The function to add options"""
+    parser.addoption("--server", action="store", help="grafana server")
+    parser.addoption("--port", action="store",
+                     help="port for grafana container")
+
+
+@pytest.fixture
+def params(request):
+    """the params"""
+    config_params = {}
+    config_params['server'] = request.config.getoption('--server')
+    config_params['port'] = request.config.getoption('--port')
+    if config_params['server'] is None or config_params['port'] is None:
+        pytest.skip()
+    return config_params

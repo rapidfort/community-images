3adfcc4d84a2f7ebefaf4698f6934d43d27ba892
Anmol Virdi <anmolvirdi70@gmail.com>
2023-09-20 06:16:31 -0700

    Added init files
    Signed-off-by: Anmol Virdi <anmolvirdi70@gmail.com>

---
+++ community_images/keycloak/bitnami/coverage_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+set -e
+ls /opt/bitnami/apache2/modules/
+httpd -M
+sed -i '/LoadModule /d' /opt/bitnami/apache2/conf/httpd.conf
+cat /opt/bitnami/scripts/modules_list >> /opt/bitnami/apache2/conf/httpd.conf
+#cat /opt/bitnami/apache2/conf/httpd.conf
+/opt/bitnami/scripts/apache/reload.sh
+/opt/bitnami/scripts/apache/status.sh
+
+httpd -M
+#Modules excluded: ["unixd_module" "pagespeed_module" "pagespeed_ap24_module" "mpm_worker_module" "mpm_event_module"]

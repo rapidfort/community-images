2751c2448f41178732484e204e410a4b6b7715a2
Anmol Virdi <67299745+AnmolVirdi@users.noreply.github.com>
2023-09-22 14:40:31 +0530

    Keycloak Official adder (#351)
    * Added init files
    
    Signed-off-by: Anmol Virdi <anmolvirdi70@gmail.com>
    
    * Added themes
    
    Signed-off-by: Anmol Virdi <anmolvirdi@Anmols-MacBook-Pro.local>
    
    * Keycloak official files complete
    
    Signed-off-by: Anmol Virdi <anmolvirdi70@gmail.com>
    
    * Keycloak official files complete
    
    Signed-off-by: Anmol Virdi <anmolvirdi70@gmail.com>
    
    * Fixed keycloak official and updated scripts
    
    Signed-off-by: Anmol Virdi <anmolvirdi70@gmail.com>
    
    * Updated readme
    
    Signed-off-by: Anmol Virdi <anmolvirdi70@gmail.com>
    
    ---------
    
    Signed-off-by: Anmol Virdi <anmolvirdi70@gmail.com>
    Signed-off-by: Anmol Virdi <anmolvirdi@Anmols-MacBook-Pro.local>
    Co-authored-by: Anmol Virdi <anmolvirdi@Anmols-MacBook-Pro.local>

---
+++ community_images/keycloak/official/dc_coverage_tls.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+set -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/../../common/scripts/bash_helper.sh
+
+JSON_PARAMS="$1"
+
+JSON=$(cat "$JSON_PARAMS")
+
+echo "Json params for docker compose coverage = $JSON"
+
+sleep 15
+docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort"
+TLS_PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8443/tcp\"[0].HostPort")
+
+# run curl in loop for different endpoints
+for i in {1..5};
+do 
+    echo "Attempt $i"
+        with_backoff curl https://localhost:"${TLS_PORT}" -k -v
+done

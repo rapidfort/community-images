2f2d29673e80c5632d83df9099300af29456f711
Rajat <rajat@rapidfort.com>
2023-12-14 08:23:21 -0800

    correction in fluent-bit/bitnami
    Signed-off-by: Rajat <rajat@rapidfort.com>

--- community_images/fluent-bit/bitnami/dc_coverage.sh
+++ community_images/fluent-bit/bitnami/dc_coverage.sh
@@ -9,13 +9,20 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 . "${SCRIPTPATH}"/../../common/scripts/bash_helper.sh
 
 JSON_PARAMS="$1"
-
-
 PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
 CONTAINER_NAME="${PROJECT_NAME}"-fluent-bit-1
-
 # Wait
 sleep 10
-docker cp "${SCRIPTPATH}"/config/plugins.config "${CONTAINER_NAME}":/opt/bitnami/fluent-bit/conf/plugins.conf
 # log for debugging
 docker inspect "${CONTAINER_NAME}"
+docker exec -d "${CONTAINER_NAME}" /bin/bash -c "nohup /opt/bitnami/fluent-bit/bin/fluent-bit -c /tmp/fluent-bit.conf > /opt/bitnami/fluent-bit/logs/fluent-bit.log 2>&1 " &
+
+sleep 10
+# Get the PID of the last background process
+DOCKER_PID=$(docker exec "${CONTAINER_NAME}" /bin/bash -c "pgrep -o fluent-bit")
+# Check if the process is still running and terminate it if needed
+if [ -n "${DOCKER_PID}" ]; then
+    echo "Fluent Bit process is still running. Terminating..."
+    docker exec "${CONTAINER_NAME}" kill "${DOCKER_PID}"
+fi
+
--- community_images/fluent-bit/bitnami/k8s_coverage.sh
+++ community_images/fluent-bit/bitnami/k8s_coverage.sh
@@ -10,16 +10,15 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 . "${SCRIPTPATH}"/../../common/scripts/bash_helper.sh
 
 JSON_PARAMS="$1"
-
 #JSON=$(cat "$JSON_PARAMS")
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
-
-sleep 10
 CONTAINER_NAME=$(kubectl get pods -n "$NAMESPACE" -l "app.kubernetes.io/instance=$RELEASE_NAME" -o jsonpath='{.items[0].metadata.name}')
-
-kubectl cp "${SCRIPTPATH}"/config/fluent-bit.config "${CONTAINER_NAME}":/tmp/fluent-bit.config -n "${NAMESPACE}"
-sleep 10
 # copy over the script to the pod
-kubectl cp "${SCRIPTPATH}"/scripts/fluent-bit_coverage_script.sh "${CONTAINER_NAME}":/tmp/cvoverage_script.sh -n "${NAMESPACE}"
-
+kubectl exec "${CONTAINER_NAME}" -n "${NAMESPACE}" -- /bin/bash -c "nohup /opt/bitnami/fluent-bit/bin/fluent-bit -c /opt/bitnami/fluent-bit/conf/fluent-bit.conf" &
+sleep 10
+# Check if the process is still running and terminate it if needed
+if ps -p $! > /dev/null; then
+    echo "Fluent Bit process is still running. Terminating..."
+    kill $!
+fi

dbd3f2cf1c464395937884341b1516c6fffc5b0d
Kirandeep-Singh-Khehra <107160937+Kirandeep-Singh-Khehra@users.noreply.github.com>
2023-12-18 23:35:24 +0530

    Nats ib error removal[Do not merge] (#363)
    * Removed Nats Ironbank's Error
    
    Signed-off-by: Kirandeep-Singh-Khehra <kirandeepsinghkhehra@gmail.com>
    
    * Updated workflows
    
    Signed-off-by: Kirandeep-Singh-Khehra <kirandeepsinghkhehra@gmail.com>
    
    * Fixed coverages
    
    Signed-off-by: anmol <anmol@rapidfort.com>
    
    * Fixed coverages
    
    Signed-off-by: anmol <anmol@rapidfort.com>
    
    * Fixed curl test for nginx bitnami k8s coverage
    
    Signed-off-by: anmol <anmol@rapidfort.com>
    
    * Test1
    
    Signed-off-by: anmol <anmol@rapidfort.com>
    
    ---------
    
    Signed-off-by: Kirandeep-Singh-Khehra <kirandeepsinghkhehra@gmail.com>
    Signed-off-by: anmol <anmol@rapidfort.com>
    Signed-off-by: kirandeep <kirandeep@rapidfort.com>
    Co-authored-by: anmol <anmol@rapidfort.com>
    Co-authored-by: Anmol Virdi <67299745+AnmolVirdi@users.noreply.github.com>
    Co-authored-by: kirandeep <kirandeep@rapidfort.com>

--- community_images/nats/ironbank/coverage.sh
+++ community_images/nats/ironbank/coverage.sh
@@ -27,6 +27,9 @@ function test_nats() {
    set -x
    set -e
    GO111MODULE=off go get github.com/nats-io/nats.go
+   go env -w GOPROXY=http://${NATS_SERVER}:8222,direct
+   go install golang.org/x/crypto/blake2b@v0.14.0
+   go mod tidy
    cd \"\$GOPATH\"/src/github.com/nats-io/nats.go/examples/nats-pub && go install && cd || exit
    cd \"\$GOPATH\"/src/github.com/nats-io/nats.go/examples/nats-echo && go install && cd || exit
    nats-echo -s nats://$NATS_USER:$NATS_PASS@${NATS_SERVER}:4222 SomeSubject &

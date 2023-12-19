5a976d8c60cacd70b0e70ae9eb34d51345a92f90
Rajat <rajat@rapidfort.com>
2023-12-19 12:23:00 -0800

    fixing the fluent-bit
    Signed-off-by: Rajat <rajat@rapidfort.com>

--- community_images/fluent-bit/ironbank/k8s_coverage.sh
+++ community_images/fluent-bit/ironbank/k8s_coverage.sh
@@ -12,17 +12,18 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 JSON_PARAMS="$1"
 
-#JSON=$(cat "$JSON_PARAMS")
+JSON=$(cat "$JSON_PARAMS")
+echo "Json params for k8s coverage = $JSON"
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
 echo "NAMESPACE: $NAMESPACE"
 echo "RELEASE_NAME: $RELEASE_NAME"
 
 sleep 10
-CONTAINER_NAME=$(kubectl get pods -n "$NAMESPACE" -l "app.kubernetes.io/instance=$RELEASE_NAME" -o jsonpath='{.items[0].metadata.name}')
-# copy over the script to the pod
-kubectl cp "${SCRIPTPATH}"/config/fluent-bit.config "${CONTAINER_NAME}":/tmp/fluent-bit.config -n "${NAMESPACE}"
-kubectl exec "${CONTAINER_NAME}" -n "${NAMESPACE}" -- /bin/bash -c "nohup /fluent-bit/bin/fluent-bit -c /tmp/fluent-bit.config" &
+
+CONTAINER_NAME="$RELEASE_NAME"
+
+kubectl exec -n "${NAMESPACE}" "${CONTAINER_NAME}" -- /bin/bash -c "/fluent-bit/bin/fluent-bit -c /etc/fluent-bit.conf" &
 sleep 10
 # Check if the process is still running and terminate it if needed
 if ps -p $! > /dev/null; then

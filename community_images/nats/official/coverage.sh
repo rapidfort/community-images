28b7b5011904ac3f6b0c981816f63794655ddfd4
nrupen <nrupen@rapidfort.com>
2023-12-27 09:14:01 -0800

    Fixed official
    Signed-off-by: nrupen <nrupen@rapidfort.com>

--- community_images/nats/official/coverage.sh
+++ community_images/nats/official/coverage.sh
@@ -21,11 +21,12 @@ function test_nats() {
     # wait for nats client to come up
    kubectl wait pods nats-release-client -n "${NAMESPACE}" --for=condition=ready --timeout=10m
    echo "#!/bin/bash
+   set -x
+   set -e
    GO111MODULE=off go get github.com/nats-io/nats.go
    go env -w GOPROXY=direct
-   go install golang.org/x/crypto/blake2b@v0.14.0
-   go mod tidy
-   cd \"\$GOPATH\"/src/github.com/nats-io/nats.go/examples/nats-pub && go install && cd || exit
+   go env -w GOSUMDB=off
+   cd \"\$GOPATH\"/src/github.com/nats-io/nats.go/examples/nats-pub && go get golang.org/x/crypto/blake2b@v0.14.0 && go install && cd || exit
    cd \"\$GOPATH\"/src/github.com/nats-io/nats.go/examples/nats-echo && go install && cd || exit
    nats-echo -s nats://$NATS_USER:$NATS_PASS@${HELM_RELEASE}.${NAMESPACE}.svc.cluster.local:4222 SomeSubject &
    nats-pub -s nats://$NATS_USER:$NATS_PASS@${HELM_RELEASE}.${NAMESPACE}.svc.cluster.local:4222 -reply Hi SomeSubject 'Hi everyone'" > "$SCRIPTPATH"/commands.sh

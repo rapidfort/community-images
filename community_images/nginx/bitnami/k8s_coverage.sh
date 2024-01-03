1405b8605d068a93f7e97ee25d2722d666dde101
Rajat <rajat@rapidfort.com>
2024-01-03 09:54:11 -0800

    changing port
    Signed-off-by: Rajat <rajat@rapidfort.com>

--- community_images/nginx/bitnami/k8s_coverage.sh
+++ community_images/nginx/bitnami/k8s_coverage.sh
@@ -8,18 +8,8 @@ JSON_PARAMS="$1"
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
 
-# fetch service url and store the urls in URLS file
-rm -f URLS
-URL=$(minikube service "${RELEASE_NAME}" -n "${NAMESPACE}" --url)
+kubectl port-forward service/"${RELEASE_NAME}" -n "${NAMESPACE}" 8090:80 &
 
-# sleep 5 after minikube service (Required)
-sleep 20
+sleep 10
 
-# curl to http url
-curl "${URL}"
-
-# fetch minikube ip
-MINIKUBE_IP=$(minikube ip)
-
-# curl to https url
-curl https://"${MINIKUBE_IP}" -k
+curl http://localhost:8090

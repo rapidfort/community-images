3943a10b053eb41d34f81cf9e4865a434fab2020
Rajat <rajat@rapidfort.com>
2024-01-04 08:50:41 -0800

    change in script
    Signed-off-by: Rajat <rajat@rapidfort.com>

--- community_images/nginx/bitnami/k8s_coverage.sh
+++ community_images/nginx/bitnami/k8s_coverage.sh
@@ -12,11 +12,16 @@ JSON_PARAMS="$1"
 
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
-echo "NAMESPACE: $NAMESPACE"
-echo "RELEASE_NAME: $RELEASE_NAME"
 # Create ConfigMap for server block
 kubectl -n "${NAMESPACE}" create configmap server-block-map --from-file=my_server_block.conf="${SCRIPTPATH}"/configs/nginx.conf
 # Get the ConfigMap details
 kubectl -n "${NAMESPACE}" get configmap server-block-map -o yaml
 # Describe the ConfigMap
 kubectl -n "${NAMESPACE}" describe configmap server-block-map
+ # Describe the service
+kubectl describe svc "${RELEASE_NAME}" -n "${NAMESPACE}"
+#export external IP
+export CLUSTER_IP=$(minikube ip) 
+PORT=$(kubectl get svc "${RELEASE_NAME}" -n "${NAMESPACE}" -o jsonpath='{.spec.ports[0].Port}')
+# Checking HTTP connection
+curl http://${CLUSTER_IP}:"${PORT}" || true

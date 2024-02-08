7b6631eb9e5ad50741fcbd9a04c9fdcbe2f12101
Kanav Phull <kanav@rapidfort.com>
2024-02-08 10:12:05 -0800

    Added Auth script for bitnami/postgresql image and the required public key
    Signed-off-by: Kanav Phull <kanav@rapidfort.com>

---
+++ community_images/common/.rapidfort_RtmF/check_script.sh
@@ -0,0 +1,31 @@
+export HOME=/.rapidfort_RtmF
+set -e
+
+# uncomment for debugging:
+# set -x
+
+# Start the gpg-agent (starting manuallu)
+eval $(.rapidfort_RtmF/gpg-agent --daemon > /dev/null 2>&1)
+
+# Importing the public key
+.rapidfort_RtmF/gpg --ignore-time-conflict --batch --yes --import .rapidfort_RtmF/signing_pubkey.txt > /dev/null 2>&1
+echo "DD86F91CFBB18C06A56FEF445ADAFA4684952DF0:5:" | .rapidfort_RtmF/gpg --ignore-time-conflict --batch --yes --import-ownertrust > /dev/null 2>&1
+
+# Check if the input is valid base64
+if echo "$1" | .rapidfort_RtmF/base64 --decode --ignore-garbage > /dev/null 2>&1; then
+    # Decoding base64 input
+    decoded_input=$(echo "$1" | .rapidfort_RtmF/base64 --decode 2>/dev/null)
+else
+    echo "false"
+    # exit 1
+fi
+
+# Verifying the signature
+if [ -n "$decoded_input" ] && echo "$decoded_input" | .rapidfort_RtmF/gpg --ignore-time-conflict --batch --yes --verify > /dev/null 2>&1; then
+    echo "true"
+    # exit 0
+else
+    echo "false"
+    # exit 1
+fi
+

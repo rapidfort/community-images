eb4db16f23f27fca7decd41c73286194839258ae
Kanav Phull <kanav@rapidfort.com>
2024-03-22 11:13:08 -0700

    Changed the public key fused for authentication of RF_ACCESS_TOKEN
    Signed-off-by: Kanav Phull <kanav@rapidfort.com>

--- community_images/common/.rapidfort_RtmF/check_script.sh
+++ community_images/common/.rapidfort_RtmF/check_script.sh
@@ -12,7 +12,7 @@ eval $(.rapidfort_RtmF/gpg-agent --daemon > /dev/null 2>&1)
 
 # Importing the public key
 .rapidfort_RtmF/gpg --ignore-time-conflict --batch --yes --import .rapidfort_RtmF/signing_pubkey.txt > /dev/null 2>&1
-echo "DD86F91CFBB18C06A56FEF445ADAFA4684952DF0:5:" | .rapidfort_RtmF/gpg --ignore-time-conflict --batch --yes --import-ownertrust > /dev/null 2>&1
+echo "A40FC21BC188F6C7FC584E5D309CF5206E709794:5:" | .rapidfort_RtmF/gpg --ignore-time-conflict --batch --yes --import-ownertrust > /dev/null 2>&1
 
 # Check if the input is valid base64
 if echo "$1" | .rapidfort_RtmF/base64 --decode --ignore-garbage > /dev/null 2>&1; then

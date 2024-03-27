cb0539cf34cb650cdfd8812c754c123a42c070a5
Kirandeep Singh <kirandeep@rapidfort.com>
2024-03-28 01:20:14 +0530

    Satisfying shellcheck
    Signed-off-by: Kirandeep Singh <kirandeep@rapidfort.com>

--- community_images/argocd/quay/k8s_coverage.sh
+++ community_images/argocd/quay/k8s_coverage.sh
@@ -14,9 +14,9 @@ echo "Json params for k8s coverage = $JSON"
 
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
-POD_NAME=$(kubectl get pod -n ${NAMESPACE} | grep "${RELEASE_NAME}-server[a-z0-9-]*"  --color=auto -o)
+POD_NAME=$(kubectl get pod -n "${NAMESPACE}" | grep "${RELEASE_NAME}-server[a-z0-9-]*"  --color=auto -o)
 
-ARGOCD_SERVER=$(kubectl get svc -n ${NAMESPACE} ${RELEASE_NAME}-server -o json | jq '.spec.clusterIP')
+ARGOCD_SERVER=$(kubectl get svc -n "${NAMESPACE}" "${RELEASE_NAME}-server" -o json | jq '.spec.clusterIP')
 ARGOCD_PORT='443'
 
 # Create testing namespace
@@ -26,6 +26,6 @@ sleep 20
 
 kubectl cp "${SCRIPTPATH}"/coverage.sh "${POD_NAME}":/home/argocd/coverage.sh -n "${NAMESPACE}"
 
-kubectl exec -i ${POD_NAME} -n ${NAMESPACE} -- bash -c "./coverage.sh pass_123" 
+kubectl exec -i "${POD_NAME}" -n "${NAMESPACE}" -- bash -c "./coverage.sh pass_123" 
 
 "${SCRIPTPATH}"/../../common/selenium_tests/runner.sh "${ARGOCD_SERVER}" "${ARGOCD_PORT}" "${SCRIPTPATH}"/selenium_tests "${NAMESPACE}" 2>&1

4b1c02233222931fa45b0510b59a11e113b93e54
SaarthakMarkandey <78125526+SaarthakMarkandey@users.noreply.github.com>
2024-04-04 21:05:15 +0530

    [MYSQL FIX]: Added conditional support for executing certain binaries… (#505)
    * [MYSQL FIX]: Addedd conditonal support for executing certain binaries only for debian based images
    
    Signed-off-by: saarthak <saarthak@rapidfort.com>
    
    * Minor fix
    
    Signed-off-by: saarthak <saarthak@rapidfort.com>
    
    * Shellcheck issue fixed
    
    Signed-off-by: saarthak <saarthak@rapidfort.com>
    
    ---------
    
    Signed-off-by: saarthak <saarthak@rapidfort.com>

--- community_images/common/tests/mysql_coverage.sh
+++ community_images/common/tests/mysql_coverage.sh
@@ -5,14 +5,15 @@ set -e
 
 # add common commands here which should be present in all hardened images
 # ref: https://dev.mysql.com/doc/refman/8.0/en/programs-client.html
-
 mysql --version
 mysqladmin --version
-mysqlcheck --version
 mysqldump --version
-mysqlimport --version
+if [[ "${MYSQL_TAG}" != *"oracle"* ]]; then
+	mysqlimport --version
+	mysqlcheck --version
+	mysqlshow --version
+fi	
 # mysqlpump --version # not present on mariadb
-mysqlshow --version
 # mysqlslap --version # doesnt work on mariadb due to char set issue
 mysqld_safe --help || mysqld --version
 # mysqld_safe is deisgned such that it'll always give exit code status as 1

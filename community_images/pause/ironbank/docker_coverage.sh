8319d5670fcd4302477c2950b788e018bc90d426
saarthak <saarthak@rapidfort.com>
2024-04-06 11:02:16 -0700

    Added network namespace check in coverage
    Signed-off-by: saarthak <saarthak@rapidfort.com>

--- community_images/pause/ironbank/docker_coverage.sh
+++ community_images/pause/ironbank/docker_coverage.sh
@@ -9,10 +9,34 @@ JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker compose coverage = $JSON"
 
-CONTAINER_NAME=$(jq -r '.container_details."pause-ib".name' < "$JSON_PARAMS")
+CONTAINER1_NAME=$(jq -r '.container_details."pause-ib".name' < "$JSON_PARAMS")
+CONTAINER2_NAME="nginx-ironbank"
+docker run -d --name "$CONTAINER2_NAME" --net=container:"$CONTAINER1_NAME" --pid=container:"$CONTAINER1_NAME" registry1.dso.mil/ironbank/opensource/nginx/nginx
 
-docker run -d --name nginx --net=container:"$CONTAINER_NAME" --pid=container:"$CONTAINER_NAME" nginx:latest
+#checking if both pause and nginx share the same namespace or not 
 
-docker stop nginx
-docker remove nginx
+function get_container_interfaces() {
+  container_pid=$(docker inspect --format '{{ .State.Pid }}' "$1")
+  if [[ $? -ne 0 ]]; then
+    echo "Error: Failed to inspect container '$1'"
+    exit 1
+  fi
+  sudo nsenter -n -t "$container_pid" ip addr show scope global
+}
+
+CONTAINER1_INTERFACE=$(get_container_interfaces "$CONTAINER1_NAME") #Getting network namespace for pause
+CONTAINER2_INTERFACE=$(get_container_interfaces "$CONTAINER2_NAME") #Getting network namespace for nginx-ironbank
+if [[ "$CONTAINER1_INTERFACE" == "$CONTAINER2_INTERFACE" ]]; then
+  echo "Both containers ($CONTAINER1_NAME and $CONTAINER2_NAME) share the same network namespace."
+else
+  echo "The containers have different network namespaces:"
+  echo "  - $CONTAINER1_NAME:"
+  echo "$CONTAINER1_INTERFACE"
+  echo "  - $CONTAINER2_NAME:"
+  echo "$CONTAINER2_INTERFACE"
+  exit 1
+fi
+
+docker stop "$CONTAINER2_NAME"
+docker remove "$CONTAINER2_NAME"
 

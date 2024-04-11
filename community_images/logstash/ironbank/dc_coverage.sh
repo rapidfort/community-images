a12f0df2d10baff445c513418abccbbc17a900b5
rajat <rajat@rapidfort.com>
2024-04-11 03:40:44 -0700

    logstash test
    Signed-off-by: rajat <rajat@rapidfort.com>

---
+++ community_images/logstash/ironbank/dc_coverage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+set -e
+
+JSON_PARAMS="$1"
+
+JSON=$(cat "$JSON_PARAMS")
+
+echo "Json params for docker compose coverage = $JSON"
+
+PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
+CONTAINER_NAME=${PROJECT_NAME}-logstash-1
+
+# shellcheck disable=SC1091
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+docker exec -i "${CONTAINER_NAME}" logstash --version
+
+docker exec  "${CONTAINER_NAME}" logstash -t -f /usr/share/logstash/pipeline/logstash.conf
+
+docker exec "${CONTAINER_NAME}" ps aux | grep logstash

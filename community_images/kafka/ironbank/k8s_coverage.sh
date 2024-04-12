3ff43fa56fed16b2a7aa45d54de75c0a2fa065df
Kanav Phull <kanav@rapidfort.com>
2024-04-12 11:27:08 -0700

    Added more coverage for bitnami/kafka
    Signed-off-by: Kanav Phull <kanav@rapidfort.com>

--- community_images/kafka/ironbank/k8s_coverage.sh
+++ community_images/kafka/ironbank/k8s_coverage.sh
@@ -3,6 +3,9 @@
 set -x
 set -e
 
+# shellcheck disable=SC1091
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 JSON_PARAMS="$1"
 
 JSON=$(cat "$JSON_PARAMS")
@@ -12,3 +15,14 @@ echo "Json params for k8s coverage = $JSON"
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
 
+sleep 60
+
+CONTAINER_NAME="${RELEASE_NAME}"
+# copy over the script to the pod
+kubectl cp "${SCRIPTPATH}"/scripts/zookeeper_coverage_script.sh "${CONTAINER_NAME}":/tmp/coverage_script.sh -n "${NAMESPACE}"
+
+kubectl exec -i ${CONTAINER_NAME} -n ${NAMESPACE} -- bash /tmp/coverage_script.sh
+
+kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/publish_commands.sh "${PUBLISHER_POD_NAME}":/tmp/publish_commands.sh
+
+    kubectl -n "${NAMESPACE}" exec -i "${PUBLISHER_POD_NAME}" -- bash -c "/tmp/publish_commands.sh"
--- community_images/kafka/ironbank/k8s_coverage_helper.sh
+++ community_images/kafka/ironbank/k8s_coverage_helper.sh
@@ -5,47 +5,47 @@ set -e
 
 cd /opt/bitnami/kafka/
 
-# # Quickstart coverage
-# # CREATE A TOPIC TO STORE YOUR EVENTS
-# bin/kafka-topics.sh --create --topic quickstart-events-rf --bootstrap-server localhost:9092
+# Quickstart coverage
+# CREATE A TOPIC TO STORE YOUR EVENTS
+bin/kafka-topics.sh --create --topic quickstart-events-rf --bootstrap-server localhost:9092
 
-# # Function to start the producer and produce two messages
-# start_producer() {
-#     echo "Starting producer..."
-#     # Start the console producer in the background
-#     bin/kafka-console-producer.sh --topic quickstart-events-rf --bootstrap-server localhost:9092 << EOF
-# This is my first event
-# This is my second event
-# EOF
-# }
+# Function to start the producer and produce two messages
+start_producer() {
+    echo "Starting producer..."
+    # Start the console producer in the background
+    bin/kafka-console-producer.sh --topic quickstart-events-rf --bootstrap-server localhost:9092 << EOF
+This is my first event
+This is my second event
+EOF
+}
 
-# # Function to start the consumer and consume two messages
-# start_consumer() {
-#     echo "Starting consumer..."
-#     # Start the console consumer
-#     timeout 5 bin/kafka-console-consumer.sh --topic quickstart-events-rf --from-beginning --bootstrap-server localhost:9092 > output.txt || true
+# Function to start the consumer and consume two messages
+start_consumer() {
+    echo "Starting consumer..."
+    # Start the console consumer
+    timeout 5 bin/kafka-console-consumer.sh --topic quickstart-events-rf --from-beginning --bootstrap-server localhost:9092 > output.txt || true
 
-#     # Wait for consumer to consume two messages
-#     sleep 2
+    # Wait for consumer to consume two messages
+    sleep 2
 
-#     # Verify consumer output
-#     if [ "$(cat output.txt)" != "This is my first event
-# This is my second event" ]; then
-#         echo "Error: Consumer output does not match producer input."
-#         exit 1
-#     else
-#         echo "Consumer received the expected messages."
-#     fi
+    # Verify consumer output
+    if [ "$(cat output.txt)" != "This is my first event
+This is my second event" ]; then
+        echo "Error: Consumer output does not match producer input."
+        exit 1
+    else
+        echo "Consumer received the expected messages."
+    fi
 
-# }
+}
 
-# # Start the producer and produce messages
-# start_producer
+# Start the producer and produce messages
+start_producer
 
-# # Start the consumer and consume messages
-# start_consumer
+# Start the consumer and consume messages
+start_consumer
 
-# rm output.txt
+rm output.txt
 
 
 # PROCESS YOUR EVENTS WITH KAFKA STREAMS
@@ -64,27 +64,18 @@ function run_kafka_streams_demo() {
         --topic streams-wordcount-output \
         --config cleanup.policy=compact
 
-    # Sleep for a moment to allow topics to be created
-    sleep 5
-
     # Start the Wordcount Application
-    bin/kafka-run-class.sh org.apache.kafka.streams.examples.wordcount.WordCountDemo &
-    wordcount_pid=$!  # Capture the PID of the Wordcount application
+    timeout 40 bin/kafka-run-class.sh org.apache.kafka.streams.examples.wordcount.WordCountDemo || true &
 
     # Sleep for a moment to allow Wordcount application to start
-    sleep 15
+    sleep 10
 
     # Produce some data
-    echo -e "all streams lead to kafka hello kafka streams join kafka summit" | \
-        bin/kafka-console-producer.sh --broker-list localhost:9092 --topic streams-plaintext-input &
-
-    producer_pid=$!  # Capture the PID of the producer
-
-    # Sleep for a moment to allow messages to be processed
-    sleep 5
+    timeout 10 echo -e "all streams lead to kafka hello kafka streams join kafka summit" | \
+        bin/kafka-console-producer.sh --broker-list localhost:9092 --topic streams-plaintext-input || true
 
     # Consume processed data
-    timeout 5 bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 \
+    timeout 10 bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 \
         --topic streams-wordcount-output \
         --from-beginning \
         --formatter kafka.tools.DefaultMessageFormatter \
@@ -93,14 +84,136 @@ function run_kafka_streams_demo() {
         --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
         --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer > output.txt || true
 
-    # Sleep for a moment to allow consumer to fetch data
-    sleep 10
-
-    # Clean up
-    kill $wordcount_pid  # Stop the Wordcount application
-    kill $producer_pid  # Stop the producer process
+    # Verify Stream output
+        if [ "$(cat output.txt)" != "all     1
+streams 1
+lead    1
+to      1
+kafka   1
+hello   1
+kafka   2
+streams 2
+join    1
+kafka   3
+summit  1" ]; then
+            echo "Error: WordCount Stream output does not match expected results."
+            exit 1
+        else
+            echo "Stream Output received the expected messages."
+        fi
 }
 
 run_kafka_streams_demo
+rm output.txt
 
-echo "Damn, all is perfect!"
+
+# Coverage for and related to bin/kafka-topics.sh module
+# Creating Topic with Config
+bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic my-topic --partitions 1 \
+  --replication-factor 1 --config max.message.bytes=64000 --config flush.messages=1
+
+# Updating Config
+bin/kafka-configs.sh --bootstrap-server localhost:9092 --entity-type topics --entity-name my-topic \
+  --alter --add-config max.message.bytes=128000
+
+# checking the configs
+bin/kafka-configs.sh --bootstrap-server localhost:9092 --entity-type topics --entity-name my-topic --describe
+
+# removing the config
+bin/kafka-configs.sh --bootstrap-server localhost:9092  --entity-type topics --entity-name my-topic \
+  --alter --delete-config max.message.bytes
+
+# Modifying the topics
+bin/kafka-topics.sh --bootstrap-server localhost:9092 --alter --topic my-topic --partitions 2
+
+
+# Manually restoring leadership to restored replicas
+bin/kafka-leader-election.sh --bootstrap-server localhost:9092 --election-type preferred --all-topic-partitions
+
+# Listing all consumer groups across all topics
+bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --list
+
+# Coverage for consumer group created by quickstart coverage of wordcount stream
+bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group streams-wordcount
+bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group streams-wordcount --members
+bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --delete --group streams-wordcount
+
+
+# Expanding your cluster (we create 2 new topics in broker 0and move them entirely to broker 1 and 2)
+expand_kafka_cluster() {
+
+    # Create topic foo
+    bin/kafka-topics.sh --create \
+        --bootstrap-server localhost:9092 \
+        --replication-factor 2 \
+        --partitions 6 \
+        --topic foo
+
+    # Create topic bar
+    bin/kafka-topics.sh --create \
+        --bootstrap-server localhost:9092 \
+        --replication-factor 2 \
+        --partitions 6 \
+        --topic bar
+
+    # Generate JSON file for topics to move
+    cat <<EOF > topics-to-move.json
+{
+  "topics": [
+    {"topic": "foo"},
+    {"topic": "bar"}
+  ],
+  "version": 1
+}
+EOF
+
+    # Generate candidate reassignment plan
+    bin/kafka-reassign-partitions.sh --bootstrap-server localhost:9092 --topics-to-move-json-file topics-to-move.json --broker-list "1,2" --generate > candidate-reassignment.json
+
+    # Parse the reassignment plan into expand-cluster-reassignment.json 
+    proposed_config=$(grep -A1 'Proposed partition reassignment configuration' candidate-reassignment.json | tail -n 1)
+    echo "$proposed_config" > expand-cluster-reassignment.json
+
+    # Execute partition reassignment (setting throttle to 500 B/s)
+    bin/kafka-reassign-partitions.sh --bootstrap-server localhost:9092 --reassignment-json-file expand-cluster-reassignment.json --execute --throttle 500
+
+    # increasing the throttle value (setting to 70 MB/s)
+    bin/kafka-reassign-partitions.sh --bootstrap-server localhost:9092 --reassignment-json-file expand-cluster-reassignment.json --additional --execute --throttle 700000000
+
+    # Give some time to reassignments to occur
+    sleep 5
+
+    # Verify partition reassignment status
+    bin/kafka-reassign-partitions.sh --bootstrap-server localhost:9092 --reassignment-json-file expand-cluster-reassignment.json --verify
+    # Can verify the output of status
+
+    # cleaning up
+    rm -f topics-to-move.json candidate-reassignment.json expand-cluster-reassignment.json 
+}
+
+expand_kafka_cluster
+
+
+# Viewing throttle limit config
+bin/kafka-configs.sh --describe --bootstrap-server localhost:9092 --entity-type brokers
+
+# View list of throttled replicas
+bin/kafka-configs.sh --describe --bootstrap-server localhost:9092 --entity-type topics
+
+# Configure Default Quota for User
+bin/kafka-configs.sh  --bootstrap-server localhost:9092 --alter --add-config 'producer_byte_rate=1024,consumer_byte_rate=2048,request_percentage=200' --entity-type users --entity-default
+
+# Configure Default Quota for Client ID
+bin/kafka-configs.sh  --bootstrap-server localhost:9092 --alter --add-config 'producer_byte_rate=1024,consumer_byte_rate=2048,request_percentage=200' --entity-type clients --entity-default
+
+# Describing Quota for all users
+bin/kafka-configs.sh  --bootstrap-server localhost:9092 --describe --entity-type users
+
+# Describing Quota for every (user, client)
+bin/kafka-configs.sh  --bootstrap-server localhost:9092 --describe --entity-type users --entity-type clients
+
+bin/zookeeper-security-migration.sh --help
+
+bin/connect-mirror-maker.sh --help
+
+bin/kafka-acls.sh --help

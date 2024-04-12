05cccfdbdaa65a76acb8f1895b4c8d46272a9e06
Kirandeep Singh <kirandeep@rapidfort.com>
2024-04-12 19:19:18 +0530

    Updated permissions of kubeconfig
    Signed-off-by: Kirandeep Singh <kirandeep@rapidfort.com>

--- community_images/chart-testing/ironbank/dc_before.sh
+++ community_images/chart-testing/ironbank/dc_before.sh
@@ -15,3 +15,6 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 sudo git clone https://github.com/helm/examples.git "${SCRIPTPATH}/examples"
 
 sudo yq -i '.version="0.1.2"' "${SCRIPTPATH}/examples/charts/hello-world/Chart.yaml"
+
+cat "${HOME}/.kube/config" > "${SCRIPTPATH}/kube-config"
+sudo chown root:root "${SCRIPTPATH}/kube-config"

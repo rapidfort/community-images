a6d834c31913d25af218db57585becb29eef14ac
Kanav Phull <kanav@rapidfort.com>
2024-04-16 13:52:28 -0700

    Revert "Docker compose coverage"
    This reverts commit b6c2bba83ed0724fa3606031c494ea865c49c648.

--- community_images/terraform/ironbank/terraform_coverage.sh
+++ community_images/terraform/ironbank/terraform_coverage.sh
@@ -1,21 +1,6 @@
-#!/bin/bash
-
-set -x
-set -e
-
-pwd 
 terraform --version
 terraform init
-terraform fmt
-terraform validate
-terraform plan || terraform destroy -auto-approve
-terraform apply -auto-approve || terraform destroy -auto-approve
+terrraform plan
+terraform apply 
+echo "yes"
 curl -I http://localhost:8000
-terraform show -json
-terraform refresh
-terraform output -json
-terraform state list 
-terraform state show || echo 0
-terraform graph
-echo 'split(",", "foo,bar,baz")' | terraform console
-terraform destroy -auto-approve

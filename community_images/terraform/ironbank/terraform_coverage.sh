4185a02d82c90e791d99ede3132b8e1c4254407a
Saarthak <saarthak@rapidfort.com>
2024-04-16 14:15:12 -0700

    Docker compose coverage
    Signed-off-by: Saarthak <saarthak@rapidfort.com>

--- community_images/terraform/ironbank/terraform_coverage.sh
+++ community_images/terraform/ironbank/terraform_coverage.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
+
+set -x
+set -e
+
+
 terraform --version
 terraform init
-terrraform plan
-terraform apply 
-echo "yes"
+terraform fmt
+terraform validate
+terraform plan || terraform destroy -auto-approve
+terraform apply -auto-approve || terraform destroy -auto-approve
 curl -I http://localhost:8000
+terraform show -json
+terraform refresh
+terraform output -json
+terraform state list 
+terraform state show || echo 0
+terraform graph
+echo 'split(",", "foo,bar,baz")' | terraform console
+terraform destroy -auto-approve

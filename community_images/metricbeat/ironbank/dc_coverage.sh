e3d90fbeec52c8af71fa7346ca591800a03a81e3
rajat <rajatbassi002@gmail.com>
2024-04-17 14:12:24 -0700

    changing permission
    Signed-off-by: rajat <rajatbassi002@gmail.com>

--- community_images/metricbeat/ironbank/dc_coverage.sh
+++ community_images/metricbeat/ironbank/dc_coverage.sh
@@ -12,11 +12,10 @@ echo "Json params for docker compose coverage = $JSON"
 PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
 CONTAINER_NAME=${PROJECT_NAME}-metricbeat-1
 
-
 sleep 40
 # indices present 
 curl -XGET 'http://localhost:9200/_cat/indices?v&pretty'
 #To verify that your server’s statistics are present in Elasticsearch
 curl -XGET 'http://localhost:9200/metricbeat-*/_search?pretty'
 # testing commands
-docker exec "${CONTAINER_NAME}" /tmp/test_commands.sh
+docker exec "${CONTAINER_NAME}" bin/bash /tmp/test_commands.sh

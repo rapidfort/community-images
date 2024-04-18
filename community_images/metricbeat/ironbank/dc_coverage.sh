0426fe2cbb4ad8ba542ab7d8955244ce62d47be0
rajat <rajatbassi002@gmail.com>
2024-04-18 00:32:00 -0700

    small testing
    Signed-off-by: rajat <rajatbassi002@gmail.com>

--- community_images/metricbeat/ironbank/dc_coverage.sh
+++ community_images/metricbeat/ironbank/dc_coverage.sh
@@ -9,13 +9,19 @@ JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker compose coverage = $JSON"
 
+# shellcheck disable=SC1091
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
 CONTAINER_NAME=${PROJECT_NAME}-metricbeat-1
 
 sleep 40
+# testing commands
+docker exec "${CONTAINER_NAME}" bin/bash /tmp/test_commands.sh
 # indices present 
 curl -XGET 'http://localhost:9200/_cat/indices?v&pretty'
 #To verify that your server’s statistics are present in Elasticsearch
 curl -XGET 'http://localhost:9200/metricbeat-*/_search?pretty'
-# testing commands
-docker exec "${CONTAINER_NAME}" bin/bash /tmp/test_commands.sh
+
+"${SCRIPTPATH}"/../../common/selenium_tests/runner-dc.sh "dummy" "dummy" "${SCRIPTPATH}"/selenium_tests 2>&1
+
--- community_images/metricbeat/ironbank/test_commands.sh
+++ community_images/metricbeat/ironbank/test_commands.sh
@@ -11,4 +11,7 @@ metricbeat modules enable kibana-xpack
 metricbeat modules enable elasticsearch-xpack
 metricbeat modules enable elasticsearch
 metricbeat modules enable kibana
+metricbeat modules enable beat-xpack
+metricbeat modules enable beat
+metricbeat modules enable http
 metricbeat run --help

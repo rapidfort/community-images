f28dad69c5f5bb2bfc8e098c71bb84af8445e62b
rajat <rajatbassi002@gmail.com>
2024-04-20 08:28:51 -0700

    enabling admin
    Signed-off-by: rajat <rajatbassi002@gmail.com>

--- community_images/kong/official/kong_setup.sh
+++ community_images/kong/official/kong_setup.sh
@@ -6,6 +6,6 @@ set -e
 NAMESPACE=$1
 RELEASE_NAME=$2
 
-kubectl wait deployment "${RELEASE_NAME}"-0 -n "${NAMESPACE}" \
+kubectl wait deployment "${RELEASE_NAME}"-kong -n "${NAMESPACE}" \
     --for=condition=Available=True --timeout=20m
 

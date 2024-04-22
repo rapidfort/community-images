c4ecae257f4cdf5e313578c9d725d10ecee62f53
Kanav Phull <kanav@rapidfort.com>
2024-04-22 10:36:05 -0700

    Removed unused variable
    Signed-off-by: Kanav Phull <kanav@rapidfort.com>

--- community_images/tika/ironbank/docker_coverage.sh
+++ community_images/tika/ironbank/docker_coverage.sh
@@ -9,8 +9,6 @@ JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker coverage = $JSON"
 
-# shellcheck disable=SC1091
-SCRIPTPATH=$(jq -r '.image_script_dir' < "$JSON_PARAMS")
 CONTAINER_NAME=$(jq -r '.container_details."tika-ib".name' < "$JSON_PARAMS")
 
 docker exec -i "${CONTAINER_NAME}" /bin/bash -c "./opt/tika/docker_coverage_assets/coverage.sh"

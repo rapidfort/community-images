8444b0ec235badf509108811e3fb73fa294f37aa
Kanav Phull <kanav@rapidfort.com>
2024-04-22 10:31:06 -0700

    Removed unused files
    Signed-off-by: Kanav Phull <kanav@rapidfort.com>

--- community_images/tika/ironbank/docker_coverage.sh
+++ community_images/tika/ironbank/docker_coverage.sh
@@ -9,7 +9,7 @@ JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker coverage = $JSON"
 
-
+# shellcheck disable=SC1091
 SCRIPTPATH=$(jq -r '.image_script_dir' < "$JSON_PARAMS")
 CONTAINER_NAME=$(jq -r '.container_details."tika-ib".name' < "$JSON_PARAMS")
 

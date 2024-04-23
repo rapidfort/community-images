ba55b761c5d13169809a3dce2d330d55f8ac9352
rajat <rajatbassi002@gmail.com>
2024-04-23 10:45:56 -0700

    modules are in rfignore
    Signed-off-by: rajat <rajatbassi002@gmail.com>

--- community_images/metricbeat/ironbank/test_commands.sh
+++ community_images/metricbeat/ironbank/test_commands.sh
@@ -8,11 +8,4 @@ metricbeat export --help
 metricbeat keystore --help
 metricbeat test --help
 metricbeat setup --help
-metricbeat modules enable kibana-xpack
-metricbeat modules enable elasticsearch-xpack
-metricbeat modules enable elasticsearch
-metricbeat modules enable kibana
-metricbeat modules enable beat-xpack
-metricbeat modules enable beat
-metricbeat modules enable http
 metricbeat run --help

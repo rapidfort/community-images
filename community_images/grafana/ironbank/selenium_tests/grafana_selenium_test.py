75ec42aef0426e41cafe953ed761c6836f62811b
saarthak <saarthak@rapidfort.com>
2024-05-06 02:37:19 -0700

    Grafana Selenium test Fix
    Signed-off-by: saarthak <saarthak@rapidfort.com>

--- community_images/grafana/ironbank/selenium_tests/grafana_selenium_test.py
+++ community_images/grafana/ironbank/selenium_tests/grafana_selenium_test.py
@@ -47,45 +47,40 @@ class TestGrafanatest1():
         self.driver.set_window_size(727, 785)
         # 3 | type | name=user | admin | 
         self.driver.find_element(By.NAME, "user").send_keys("admin")
-        # 4 | click | id=current-password |  | 
-        self.driver.find_element(By.ID, "current-password").click()
-        # 5 | type | id=current-password | admin | 
-        self.driver.find_element(By.ID, "current-password").send_keys("admin")
-        # 6 | click | css=.css-8csoim-button > .css-1mhnkuh |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-8csoim-button > .css-1mhnkuh").click()
-        # 7 | click | css=.css-oq8fy1-button > .css-1mhnkuh |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-oq8fy1-button > .css-1mhnkuh").click()
-        # 8 | click | css=.css-hj6vlq |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-hj6vlq").click()
-        # 9 | click | css=.css-fv3lde:nth-child(7) .css-1xnfi89 |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-fv3lde:nth-child(7) .css-1xnfi89").click()
-        # 10 | click | css=.css-1y9dsbx-button |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-1y9dsbx-button").click()
-        # 11 | click | linkText=Data sources |  | 
-        self.driver.find_element(By.LINK_TEXT, "Data sources").click()
-        # 12 | click | xpath=//span[contains(.,'Add data source')] |  | 
+        # 4 | click | id=:r1: |  | 
+        self.driver.find_element(By.ID, ":r1:").click()
+        # 5 | type | id=:r1: | admin | 
+        self.driver.find_element(By.ID, ":r1:").send_keys("admin")
+        # 6 | click | css=.css-1b7vft8-button > .css-1riaxdn |  | 
+        self.driver.find_element(By.CSS_SELECTOR, ".css-1b7vft8-button > .css-1riaxdn").click()
+        # 7 | click | .css-bhnz0e-button > .css-1riaxdn |  |
+        self.driver.find_element(By.CSS_SELECTOR, ".css-bhnz0e-button > .css-1riaxdn").click()
+        time.sleep(60)
+        # 8 | click | css=.css-fmaj2t-Icon > path |  |
+        self.driver.find_element(By.CSS_SELECTOR, ".css-fmaj2t-Icon > path").click()
+        # 9 | click | css=.css-1bht7sk:nth-child(6) .css-fmaj2t-Icon |  | 
+        self.driver.find_element(By.CSS_SELECTOR, ".css-1bht7sk:nth-child(6) .css-fmaj2t-Icon").click()
+        # 10 | click | xpath=//span[contains(.,'Data sources')] |  |
+        self.driver.find_element(By.XPATH, "//span[contains(.,'Data sources')]").click()
+        # 11 | click | xpath=//span[contains(.,\'Add data source\')]
         self.driver.find_element(By.XPATH, "//span[contains(.,\'Add data source\')]").click()
-
         self.driver.implicitly_wait(10)
-        # 17 | click | xpath=//button[contains(.,'Prometheus')] |  | 
+        # 12 | click | xpath=//button[contains(.,'Prometheus')] |  | 
         self.driver.find_element(By.XPATH, "//button[contains(.,\'Prometheus\')]").click()
-        # 18 | click | css=.css-y1sxu8 |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-y1sxu8").click()
-        # 19 | type | css=.width-20:nth-child(1) | http://10.10.0.165:9090 | 
-        self.driver.find_element(By.CSS_SELECTOR, ".width-20:nth-child(1)").send_keys("http://localhost:9090")
-        # 20 | click | css=.css-z53gi5-button > .css-1mhnkuh |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-z53gi5-button > .css-1mhnkuh").click()
+        # 13 | click | css=#connection-url |  | 
+        self.driver.find_element(By.CSS_SELECTOR, "#connection-url").click()
+        # 14 | type | css=#connection-url | http://prometheus:9090 | 
+        self.driver.find_element(By.CSS_SELECTOR, "#connection-url").send_keys("http://prometheus:9090")
+        # 15 | click | css=.css-td06pi-button > .css-1riaxdn |  | 
+        self.driver.find_element(By.CSS_SELECTOR, ".css-td06pi-button > .css-1riaxdn").click()
         
         ### Dashboards setup
-        # 1 | open | /datasources/edit/<hex>/dashboards |  | 
-        self.driver.get(
-            "http://localhost:{}/datasources/edit/{}/dashboards".format(
-                params['port'],
-                self.driver.current_url[39:]))
-        # 2 | setWindowSize | 727x785 |  | 
+        # 1 | click | xpath=//a[contains(.,'Dashboards')] |  |
+        self.driver.find_element(By.XPATH, "//a[contains(.,'Dashboards')]").click()
+        # 2 | setWindowSize | 727x785 |  |
         self.driver.set_window_size(727, 785)
-        # 3 | click | css=tr:nth-child(2) .css-1mhnkuh |  | 
-        self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(2) .css-1mhnkuh").click()
+        # 3 | click | css=tr:nth-child(2) .css-1riaxdn |  |
+        self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(2) .css-1riaxdn").click()
         # 4 | click | linkText=Prometheus 2.0 Stats |  | 
         self.driver.find_element(By.LINK_TEXT, "Prometheus 2.0 Stats").click()
         self.driver.implicitly_wait(10)
@@ -95,28 +90,24 @@ class TestGrafanatest1():
         ### Alert manager
         # 1 | open | /connections/your-connections/datasources/new |  | 
         self.driver.get(
-            "http://localhost:{}/connections/your-connections/datasources/new".format(
+            "http://localhost:{}/connections/add-new-connection".format(
                 params['port']))
         # 2 | setWindowSize | 727x785 |  | 
         self.driver.set_window_size(727, 785)
-        # 3 | click | css=.css-1mlczho-input-input |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-1mlczho-input-input").click()
-        # 4 | type | css=.css-1mlczho-input-input | alert | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-1mlczho-input-input").send_keys("alert")
-        # 5 | click | css=.css-1cqw476 |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-1cqw476").click()
-        # 6 | mouseDown | xpath=//div[@id='reactRoot']/div/main/div[2]/div[3]/div/div[2]/div/div/div/div[3]/form/div[3]/div/div/div/div/div/div/div[2] |  | 
-        element = self.driver.find_element(By.XPATH, "//div[@id=\'reactRoot\']/div/main/div[2]/div[3]/div/div[2]/div/div/div/div[3]/form/div[3]/div/div/div/div/div/div/div[2]")
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element).click_and_hold().perform()
-        # 7 | click | css=#react-select-2-option-2 .css-1gncicp-grafana-select-option-description |  | 
-        self.driver.find_element(By.CSS_SELECTOR, "#react-select-2-option-2 .css-1gncicp-grafana-select-option-description").click()
-        # 8 | click | css=.css-y1sxu8 |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-y1sxu8").click()
-        # 9 | type | css=.gf-form-input:nth-child(1) | http://10.10.0.165:9093 | 
-        self.driver.find_element(By.CSS_SELECTOR, ".gf-form-input:nth-child(1)").send_keys("http://localhost:9093")
-        # 10 | click | css=.css-z53gi5-button > .css-1mhnkuh |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-z53gi5-button > .css-1mhnkuh").click()
+        # 3 | type | xpath=//a[contains(.,'Alertmanager')] | alert | 
+        self.driver.find_element(By.XPATH, "//a[contains(.,'Alertmanager')]").click()
+        # 4 | click | xpath=//span[contains(.,'Add new data source')] |  | 
+        self.driver.find_element(By.XPATH, "//span[contains(.,'Add new data source')]").click()
+        # 5 | click | css=.css-zyjsuv-input-suffix |  | 
+        self.driver.find_element(By.CSS_SELECTOR, ".css-zyjsuv-input-suffix").click()
+        # 6 | click | xpath=//span[contains(.,'Prometheus')]
+        self.driver.find_element(By.XPATH, "//span[contains(.,'Prometheus')]").click()
+        # 7 | click | xpath=//div[@id='pageContent']/div[3]/div/div/div/div[3]/form/div[2]/div[2]/div/div/div/div/input
+        self.driver.find_element(By.XPATH, "//div[@id='pageContent']/div[3]/div/div/div/div[3]/form/div[2]/div[2]/div/div/div/div/input").click()
+        # 8 | type | css=//div[@id='pageContent']/div[3]/div/div/div/div[3]/form/div[2]/div[2]/div/div/div/div/input | 
+        self.driver.find_element(By.XPATH, "//div[@id='pageContent']/div[3]/div/div/div/div[3]/form/div[2]/div[2]/div/div/div/div/input").send_keys("http://alertmanager:9093")
+        # 9 | click | css=.css-td06pi-button > .css-1riaxdn |  | 
+        self.driver.find_element(By.CSS_SELECTOR, ".css-td06pi-button > .css-1riaxdn").click()
 
         ### User management
         # 1 | open | /admin/users/create |  | 
@@ -141,10 +132,8 @@ class TestGrafanatest1():
         self.driver.find_element(By.ID, "password-input").click()
         # 10 | type | id=password-input | newuser | 
         self.driver.find_element(By.ID, "password-input").send_keys("newuser")
-        # 11 | click | css=.css-1mhnkuh |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-1mhnkuh").click()
-        # 12 | click | css=.css-1fwxvu6 |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-1fwxvu6").click()
+        # 11 | click | css=.css-td06pi-button > .css-1riaxdn |  | 
+        self.driver.find_element(By.CSS_SELECTOR, ".css-td06pi-button > .css-1riaxdn").click()
         self.driver.implicitly_wait(5)
         
         ### New User login
@@ -155,11 +144,9 @@ class TestGrafanatest1():
         self.driver.find_element(By.NAME, "user").click()
         # 2 | type | name=user | new-user | 
         self.driver.find_element(By.NAME, "user").send_keys("new-user")
-        # 3 | click | id=current-password |  | 
-        self.driver.find_element(By.ID, "current-password").click()
-        # 4 | type | id=current-password | newuser | 
-        self.driver.find_element(By.ID, "current-password").send_keys("newuser")
-        # 5 | click | css=.css-8csoim-button > .css-1mhnkuh |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-8csoim-button > .css-1mhnkuh").click()
-        # 6 | click | css=.css-1fwxvu6 |  | 
-        self.driver.find_element(By.CSS_SELECTOR, ".css-1fwxvu6").click()
+        # 3 | click | id=:r1: |  | 
+        self.driver.find_element(By.ID, ":r1:").click()
+        # 4 | type | id=:r1: | newuser | 
+        self.driver.find_element(By.ID, ":r1:").send_keys("newuser")
+        # 5 | click | css=.css-1b7vft8-button > .css-1mhnkuh |  | 
+        self.driver.find_element(By.CSS_SELECTOR, ".css-1b7vft8-button").click()

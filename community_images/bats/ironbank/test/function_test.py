bc9a88f98ace99a1431215bcd724ae5f287e678f
rajat <rajatbassi002@gmail.com>
2024-05-07 12:27:11 -0700

    bats-ib-addition
    Signed-off-by: rajat <rajatbassi002@gmail.com>

---
+++ community_images/bats/ironbank/test/function_test.py
@@ -0,0 +1,11 @@
+# function_test.py
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    print("Factorial of", num, "is", factorial(num))

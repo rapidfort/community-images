441d0520357e9e09c00390c09e8f5c0624df1f37
kirandeep <kirandeep@rapidfort.com>
2024-05-07 12:59:53 -0700

    Cleanup
    Signed-off-by: kirandeep <kirandeep@rapidfort.com>

--- community_images/keycloak/official/dc_coverage.sh
+++ community_images/keycloak/official/dc_coverage.sh
@@ -28,4 +28,4 @@ docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8080/tcp
 PORT=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Ports.\"8080/tcp\"[0].HostPort")
 
 # Initiating Selenium tests
-("${SCRIPTPATH}"/../../common/selenium_tests/runner-dc.sh "${PROJECT_NAME}" "${PORT}" "${SCRIPTPATH}"/selenium_tests 2>&1) >&2
+"${SCRIPTPATH}"/../../common/selenium_tests/runner-dc.sh "${PROJECT_NAME}" "${PORT}" "${SCRIPTPATH}"/selenium_tests 2>&1

cbcee0908417cc2e6d37b264df2be6033b324566
kirandeep <kirandeep@rapidfort.com>
2024-05-08 07:40:45 -0700

    Added doc comments
    Signed-off-by: kirandeep <kirandeep@rapidfort.com>

--- community_images/minio-operator/ironbank/k8s_coverage.sh
+++ community_images/minio-operator/ironbank/k8s_coverage.sh
@@ -16,12 +16,16 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 sleep 10
 
+# Get JWT token for sign-in
 JWT=$(kubectl get secret/console-sa-secret -n ${NAMESPACE} -o json | jq -r ".data.token" | base64 -d)
 OPERATOR_PORT='9090'
 
+# Port forward for minio operator console
 kubectl port-forward svc/console -n ${NAMESPACE} --address 0.0.0.0 9090:9090 &
 PORT_FORWARD_PID=$!
 
-("${SCRIPTPATH}"/../../common/selenium_tests/runner-dc.sh "${NAMESPACE} ${JWT}" "${OPERATOR_PORT}" "${SCRIPTPATH}"/selenium_tests 2>&1) >&2
+# Run selenium tests
+"${SCRIPTPATH}"/../../common/selenium_tests/runner-dc.sh "${NAMESPACE} ${JWT}" "${OPERATOR_PORT}" "${SCRIPTPATH}"/selenium_tests 2>&1
 
+# Gracefully stop port-forwarding
 kill -s 15 "${PORT_FORWARD_PID}"

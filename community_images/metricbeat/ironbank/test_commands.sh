f10566389d83149069fe223d4eb8a73b103ff2fd
Rajat <91206945+Rajat0002@users.noreply.github.com>
2024-05-09 03:33:47 +0530

    comments in metricbeat, asset folder in bats addition (#584)
    Signed-off-by: rajat <rajatbassi002@gmail.com>

--- community_images/metricbeat/ironbank/test_commands.sh
+++ community_images/metricbeat/ironbank/test_commands.sh
@@ -3,9 +3,15 @@
 set -x
 set -e
 
+# Check Metricbeat version
 metricbeat version
+# Export Metricbeat available modules
 metricbeat export modules
+# testing keystore list subcommand
 metricbeat keystore list
+#Tests the output configuration.
 metricbeat test output
+# for initial env help command
 metricbeat setup --help
+# Runs Metricbeat
 metricbeat run --help

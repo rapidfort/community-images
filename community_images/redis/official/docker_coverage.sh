51b80cec4e72bed0ea06cc201d141e8bfe88a118
Anmol Virdi <anmolvirdi70@gmail.com>
2024-05-09 15:25:59 -0700

    Altered redis benchmark testing
    Signed-off-by: Anmol Virdi <anmolvirdi70@gmail.com>

--- community_images/redis/official/docker_coverage.sh
+++ community_images/redis/official/docker_coverage.sh
@@ -7,7 +7,7 @@ JSON_PARAMS="$1"
 
 JSON=$(cat "$JSON_PARAMS")
 
-echo "Json params for docker compose coverage = $JSON"
+echo "Json params for docker coverage = $JSON"
 
 RAPIDFORT_ACCOUNT="${RAPIDFORT_ACCOUNT:-rapidfort}"
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
@@ -19,4 +19,4 @@ REDIS_CONTAINER= $(jq -r '.container_details."redis-official".name' < "$JSON_PAR
 # TAG=$(jq -r '.image_tag_details."redis-official".tag' < "$JSON_PARAMS")
 
 # run redis-client tests
-docker exec -it "${REDIS_CONTAINER}" redis-benchmark -h "${REDIS_HOST}" -p 6379 -n 1000 -c 10
+docker exec -i "${REDIS_CONTAINER}" redis-benchmark -h "${REDIS_HOST}" -p 6379 -n 1000 -c 10

8f4bbb4fd6471b7927c31fb9a9ee9fb5aacbc9f4
Anmol Virdi <anmolvirdi70@gmail.com>
2024-05-09 15:20:03 -0700

    Altered redis benchmark testing
    Signed-off-by: Anmol Virdi <anmolvirdi70@gmail.com>

--- community_images/redis/official/docker_coverage.sh
+++ community_images/redis/official/docker_coverage.sh
@@ -14,11 +14,9 @@ NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 
 # get docker host ip
 REDIS_HOST=$(jq -r '.container_details."redis-official".ip_address' < "$JSON_PARAMS")
-REPO_PATH=$(jq -r '.image_tag_details."redis-official".repo_path' < "$JSON_PARAMS")
-TAG=$(jq -r '.image_tag_details."redis-official".tag' < "$JSON_PARAMS")
+REDIS_CONTAINER= $(jq -r '.container_details."redis-official".name' < "$JSON_PARAMS")
+# REPO_PATH=$(jq -r '.image_tag_details."redis-official".repo_path' < "$JSON_PARAMS")
+# TAG=$(jq -r '.image_tag_details."redis-official".tag' < "$JSON_PARAMS")
 
 # run redis-client tests
-docker run --rm -i --cap-add=SYS_PTRACE \
-    --network="${NAMESPACE}" \
-    "${REPO_PATH}:${TAG}" \
-    redis-benchmark -h "${REDIS_HOST}" -p 6379 -n 1000 -c 10
+docker exec -it "${REDIS_CONTAINER}" redis-benchmark -h "${REDIS_HOST}" -p 6379 -n 1000 -c 10

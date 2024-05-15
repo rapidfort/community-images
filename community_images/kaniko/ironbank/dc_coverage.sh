bad46215d40ce55c26e2ef9197721396a85de3d7
saarthak <saarthak@rapidfort.com>
2024-05-15 12:02:13 -0700

    Kaniko Ironbank Addition
    Signed-off-by: saarthak <saarthak@rapidfort.com>

---
+++ community_images/kaniko/ironbank/dc_coverage.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+set -e
+
+JSON_PARAMS="$1"
+
+JSON=$(cat "$JSON_PARAMS")
+
+echo "Json params for docker compose coverage = $JSON"
+NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
+SCRIPTPATH=$(jq -r '.image_script_dir' < "$JSON_PARAMS")
+
+# Extracting names for ansible and ubuntu_host
+KANIKO_CONTAINER_NAME="${NAMESPACE}"-kaniko-1
+REGISTRY_CONTAINER_NAME="${NAMESPACE}"-registry-1
+
+
+#Executing command inside kaniko container to build and push the image inside registry container
+docker exec -i "${KANIKO_CONTAINER_NAME}"  /kaniko/executor --dockerfile=/workspace/Dockerfile --context=/workspace --destination=registry:5000/alpine:latest --insecure --insecure-pull
+
+#Confirming if the registry got pushed or not
+docker pull localhost:5001/alpine:latest
+
+#cleaning up the pulled image
+docker image remove localhost:5001/alpine:latest

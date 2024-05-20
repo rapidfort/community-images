048a7e25cf4c66cfe7a9131b4778b49d8c8aa87d
rajat <rajatbassi002@gmail.com>
2024-05-20 00:23:35 -0700

    rabbitmq official addition
    Signed-off-by: rajat <rajatbassi002@gmail.com>

---
+++ community_images/rabbitmq/official/dc_coverage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+set -e
+
+JSON_PARAMS="$1"
+
+JSON=$(cat "$JSON_PARAMS")
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# shellcheck disable=SC1091
+. "${SCRIPTPATH}"/coverage.sh
+
+PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
+
+RABBITMQ_SERVER=${PROJECT_NAME}-rabbitmq-1
+RABBITMQ_NETWORK=${PROJECT_NAME}_default
+
+RABBITMQ_USERNAME='user'
+RABBITMQ_PASSWORD='password'
+
+sleep 20
+test_rabbitmq_docker_compose ${RABBITMQ_SERVER} ${RABBITMQ_NETWORK} ${RABBITMQ_USERNAME} ${RABBITMQ_PASSWORD}

338041c9fe2ffe2a3e21b5abef231b167e1176cb
Kanav Phull <kanav@rapidfort.com>
2024-05-31 13:55:07 -0700

    Added coverage for postgres-exporter
    Signed-off-by: Kanav Phull <kanav@rapidfort.com>

--- community_images/postgres-exporter/ironbank/dc_coverage.sh
+++ community_images/postgres-exporter/ironbank/dc_coverage.sh
@@ -9,4 +9,31 @@ JSON=$(cat "$JSON_PARAMS")
 
 echo "Json params for docker compose coverage = $JSON"
 
-# PROJECT_NAME=$(jq -r '.project_name' < "$JSON_PARAMS")
+PG_CONTAINER="postgres"
+
+# Define the SQL queries
+SQL_QUERIES="
+CREATE TABLE IF NOT EXISTS test_table (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    age INT
+);
+
+INSERT INTO test_table (name, age) VALUES ('Alice', 30);
+INSERT INTO test_table (name, age) VALUES ('Bob', 25);
+
+SELECT * FROM test_table;
+
+UPDATE test_table SET age = 31 WHERE name = 'Alice';
+DELETE FROM test_table WHERE name = 'Bob';
+"
+
+# Exec into the PostgreSQL container and execute the SQL queries
+docker exec -i "${PG_CONTAINER}" psql -U postgres -d mydatabase <<EOF
+$SQL_QUERIES
+EOF
+
+echo "SQL queries executed successfully."
+
+# Now lets check the logs received in postgres-exporter
+curl http://localhost:9187/metrics

525990df1cacc1f76ba015f88504643b1157be1c
Kanav Phull <kanav@rapidfort.com>
2024-06-01 03:24:35 -0700

    Added improvements based on code review
    Signed-off-by: Kanav Phull <kanav@rapidfort.com>

--- community_images/postgres-exporter/ironbank/dc_coverage.sh
+++ community_images/postgres-exporter/ironbank/dc_coverage.sh
@@ -11,7 +11,23 @@ echo "Json params for docker compose coverage = $JSON"
 
 PG_CONTAINER="postgres"
 
-# Define the SQL queries
+# Get the container ID of the postgres-exporter container
+PG_EXP_CONTAINER_ID=$(docker ps -qf "name=postgres-exporter")
+
+if [ -z "$PG_EXP_CONTAINER_ID" ]; then
+  echo "postgres-exporter container is not running."
+  exit 1
+fi
+
+# Get the dynamically assigned port for postgres-exporter
+PG_EXP_PORT=$(docker port "$PG_EXP_CONTAINER_ID" 9187 | head -n 1 | awk -F: '{print $2}')
+
+if [ -z "$PG_EXP_PORT" ]; then
+  echo "Failed to retrieve the port for postgres-exporter. Ensure the service is running."
+  exit 1
+fi
+
+# Defining SQL queries to exercise CRUD operations in postgresql
 SQL_QUERIES="
 CREATE TABLE IF NOT EXISTS test_table (
     id SERIAL PRIMARY KEY,
@@ -35,5 +51,5 @@ EOF
 
 echo "SQL queries executed successfully."
 
-# Now lets check the logs received in postgres-exporter
-curl http://localhost:9187/metrics
+# checking corresponding logs received in postgres-exporter
+curl http://localhost:"$PG_EXP_PORT"/metrics

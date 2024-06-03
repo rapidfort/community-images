46ee50483da655aec4941de6db25625b55a8940e
Kanav Phull <kanav@rapidfort.com>
2024-06-03 12:59:42 -0700

    Added coverage for kafka-exporter
    Signed-off-by: Kanav Phull <kanav@rapidfort.com>

---
+++ community_images/kafka-exporter/ironbank/k8s_coverage.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# shellcheck disable=SC1091
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+JSON_PARAMS="$1"
+
+JSON=$(cat "$JSON_PARAMS")
+
+echo "Json params for k8s coverage = $JSON"
+
+NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
+
+sleep 60
+
+# copy over the script to the kafka pod
+kubectl cp "${SCRIPTPATH}"/k8s_coverage_helper.sh \
+  rf-kafka-ib-0:/opt/bitnami/kafka/k8s_coverage_helper.sh \
+  -n "${NAMESPACE}"
+
+# execute the script in the kafka pod
+kubectl exec -i rf-kafka-ib-0 -n "${NAMESPACE}" -- bash ./opt/bitnami/kafka/k8s_coverage_helper.sh
+
+# check the corresponding logs in kafka-exporter
+kubectl exec -i rf-kafka-exporter -n "${NAMESPACE}" -- bash curl http://localhost:9308/metrics

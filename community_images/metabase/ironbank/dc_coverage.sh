492bf84c2838c0519d907e14feff257c5a2d90ca
rajat <rajatbassi002@gmail.com>
2024-06-07 04:31:49 -0700

    conflicts resolved
    Signed-off-by: rajat <rajatbassi002@gmail.com>

--- community_images/metabase/ironbank/dc_coverage.sh
+++ community_images/metabase/ironbank/dc_coverage.sh
@@ -19,5 +19,7 @@ CONTAINER_NAME="${PROJECT_NAME}"-metabase-1
 sleep 10
 METABASE_HOST=$(docker inspect "${CONTAINER_NAME}" | jq -r ".[].NetworkSettings.Networks.\"${PROJECT_NAME}_internal\".IPAddress")
 METABASE_PORT='3000'
+# request to the Metabase setup endpoint
 curl http://localhost:3000/setup/
+# ui-testing
 ("${SCRIPTPATH}"/../../common/selenium_tests/runner-dc.sh "${METABASE_HOST}" "${METABASE_PORT}" "${SCRIPTPATH}"/selenium_tests 2>&1) >&2

2b3ad08c3106f9d8a806687ec4a261952c6dcd0a
kirandeep <kirandeep@rapidfort.com>
2024-06-16 23:54:27 -0700

    Printing pagesource
    Signed-off-by: kirandeep <kirandeep@rapidfort.com>

--- community_images/gitlab-runner/ironbank/selenium_tests/gitlab_runner_selenium_test.py
+++ community_images/gitlab-runner/ironbank/selenium_tests/gitlab_runner_selenium_test.py
@@ -77,9 +77,10 @@ class TestGrafanatest1():
             self.driver.find_element(By.ID, "user_password").send_keys(root_user_password)
             self.driver.find_element(By.XPATH, "//label[contains(.,\'Remember me\')]").click()
             self.driver.find_element(By.XPATH, "//span[contains(.,\'Sign in\')]").click()
-        
+
         time.sleep(5)
         print("URL  : ", self.driver.current_url)
+        print(self.driver.page_source)
         print("Token: ", self.driver.find_element(By.XPATH, "//code").text)
         print("Token: ", self.driver.find_element(By.CSS_SELECTOR, "code:nth-child(3)").text)
 

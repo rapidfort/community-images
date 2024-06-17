773330a700335615bf574c55f4815528813ed373
saarthak <saarthak@rapidfort.com>
2024-06-17 03:01:57 -0700

    coverage updated
    Signed-off-by: saarthak <saarthak@rapidfort.com>

--- community_images/velero/ironbank/coverage.sh
+++ community_images/velero/ironbank/coverage.sh
@@ -31,9 +31,9 @@ velero snapshot-location create vsl --provider aws --config region=us-east-2
 velero backup-location create bsl --provider aws --bucket velero-migration-demo --config region=us-east-2 --access-mode=ReadOnly
 
 # Create a restore from the backup named "backup1"
-velero restore create restore1 --from-backup backup1
+# velero restore create restore1 --from-backup backup1
 
-velero restore delete restore1 --confirm
+# velero restore delete restore1 --confirm
 
 # Install Velero with specific settings: using node agent, enabling privileged node agent, using AWS as the provider, specifying the bucket "bucket1", not providing a secret, and using the AWS plugin
 velero install --use-node-agent --privileged-node-agent --provider aws --bucket bucket1 --no-secret --plugins velero/velero-plugin-for-aws

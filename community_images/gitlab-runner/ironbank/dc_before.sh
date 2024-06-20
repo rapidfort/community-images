8fa1e6d7ecac08250c1d0393b898257d86dc357d
kirandeep <kirandeep@rapidfort.com>
2024-06-20 12:14:44 -0700

    Cleanup
    Signed-off-by: kirandeep <kirandeep@rapidfort.com>

--- community_images/gitlab-runner/ironbank/dc_before.sh
+++ community_images/gitlab-runner/ironbank/dc_before.sh
@@ -12,8 +12,11 @@ echo "Json params for docker compose coverage = $JSON"
 # shellcheck disable=SC1091
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+# Cleanup volumes(mounted directories) from previous run
 rm -rf "${SCRIPTPATH}/config" "${SCRIPTPATH}/logs" "${SCRIPTPATH}/data" "${SCRIPTPATH}"/id_*
+# Create directories to mount
 mkdir "${SCRIPTPATH}/config" "${SCRIPTPATH}/logs" "${SCRIPTPATH}/data"
 
+# Generate key pair for runner to ssh into another container(for covering ssh executor)
 ssh-keygen -f "${SCRIPTPATH}/id_rsa" -N ""
 

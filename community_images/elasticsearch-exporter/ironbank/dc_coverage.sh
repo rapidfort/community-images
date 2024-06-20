bad2e66d88c2f1bc7c7ecf95006979e2a055fc98
Kanav Phull <kanav@rapidfort.com>
2024-06-20 07:49:09 -0700

    Added fix fin docker-compose.yml for correct command generation with correct flags
    Signed-off-by: Kanav Phull <kanav@rapidfort.com>

--- community_images/elasticsearch-exporter/ironbank/dc_coverage.sh
+++ community_images/elasticsearch-exporter/ironbank/dc_coverage.sh
@@ -77,4 +77,4 @@ curl -X DELETE "${ES_BASE_URL}/my_index?pretty"
 curl "${ES_EXPORTER_BASE_URL}/metrics"
 
 # CLI Coverage for elasticsearch-exporter
-docker exec -i "$ES_EXPORTER_CONTAINER_NAME" /bin/bash -c "elasticsearch-exporter --help"
+docker exec -i "$ES_EXPORTER_CONTAINER_NAME" elasticsearch-exporter --help || true

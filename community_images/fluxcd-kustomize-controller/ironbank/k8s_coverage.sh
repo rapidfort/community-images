158f03ade6b5683b1cc7135a2df44a6b77be58aa
rajat <rajatbassi002@gmail.com>
2024-06-20 02:38:45 -0700

    addition of notification, source and kustomize controller
    Signed-off-by: rajat <rajatbassi002@gmail.com>

---
+++ community_images/fluxcd-kustomize-controller/ironbank/k8s_coverage.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# shellcheck disable=SC1091
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+JSON_PARAMS="$1"
+
+JSON=$(cat "$JSON_PARAMS")
+
+echo "Json params for k8s coverage = $JSON"
+
+NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
+RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
+
+# A Flux Kustomization named podinfo is created that watches the GitRepository for Artifact changes.
+kubectl apply -f "$SCRIPTPATH"/podinfo.yml
+sleep 15
+# source status
+kubectl get gitrepository
+# reconciliation status
+kubectl get kustomizations
+# conditions and events
+kubectl describe kustomizations podinfo
+kubectl delete -f "$SCRIPTPATH"/podinfo.yml

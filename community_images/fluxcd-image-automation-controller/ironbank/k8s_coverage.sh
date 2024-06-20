15b5bb57ddb8afe1075bd37f3df0abbb254ac4db
Rajat <91206945+Rajat0002@users.noreply.github.com>
2024-06-21 02:30:24 +0530

    fluxcd controller addition  (#648)
    * fluxcd controller addition
    
    Signed-off-by: rajat <rajatbassi002@gmail.com>
    
    * addition with comments
    
    Signed-off-by: rajat <rajatbassi002@gmail.com>
    
    * changes
    
    Signed-off-by: rajat <rajatbassi002@gmail.com>
    
    * adding required change
    
    Signed-off-by: rajat <rajatbassi002@gmail.com>
    
    ---------
    
    Signed-off-by: rajat <rajatbassi002@gmail.com>

---
+++ community_images/fluxcd-image-automation-controller/ironbank/k8s_coverage.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# shellcheck disable=SC1091
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+JSON_PARAMS="$1"
+
+JSON=$(cat "$JSON_PARAMS")
+
+echo "Json params for k8s coverage = $JSON"
+
+NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
+RELEASE_NAME=$(jq -r '.release_name' < "$JSON_PARAMS")
+
+echo "$NAMESPACE"
+echo "$RELEASE_NAME"
+# Apply the resource on the cluster
+kubectl apply -f "$SCRIPTPATH"/imageupdateautomation.yaml
+sleep 30
+#  to see the ImageUpdateAutomation
+kubectl get imagepolicy
+kubectl describe imagepolicy podinfo-policy
+kubectl get imagerepository
+kubectl describe imagerepository podinfo
+kubectl get imageupdateautomation
+# to verify event
+kubectl describe imageupdateautomation podinfo-update
+# cleanup
+kubectl delete -f "$SCRIPTPATH"/imageupdateautomation.yaml

6d8b5cc922e2fe8f227b90a4f328c2a803858231
rajat <rajatbassi002@gmail.com>
2024-06-28 11:56:44 -0700

    updating fluentd coverage
    Signed-off-by: rajat <rajatbassi002@gmail.com>

--- community_images/fluentd/official/dc_coverage.sh
+++ community_images/fluentd/official/dc_coverage.sh
@@ -20,3 +20,28 @@ docker exec -i "$CONTAINER_NAME" gem install fluent-plugin-elasticsearch
 
 # list fluent gem list
 docker exec -i "$CONTAINER_NAME" fluent-gem list
+# for fluent-debug
+docker exec -i "$CONTAINER_NAME" gem install fiber-storage
+# version
+docker exec -i "$CONTAINER_NAME" fluentd --version
+# List available formats for fluent-binlog-reader
+docker exec -i "$CONTAINER_NAME" fluent-binlog-reader formats
+docker exec -i "$CONTAINER_NAME" fluent-binlog-reader cat fluentd/etc/fluent.conf
+
+# Get the PIDs
+PID=$(docker exec -i "$CONTAINER_NAME" pgrep -f fluentd | head -n 2)
+# fluent-ctl using dump
+docker exec -i "$CONTAINER_NAME" fluent-ctl dump "$PID"
+docker exec -i "$CONTAINER_NAME" fluent-cat --help
+# checking fluent-cap-ctl
+docker exec -i "$CONTAINER_NAME" fluent-cap-ctl || echo 0
+# format  with output set to null
+docker exec -i "$CONTAINER_NAME" fluent-plugin-config-format output null
+docker exec -i "$CONTAINER_NAME" fluent-ca-generate -help
+# checking Generating Plugin Project Skeleton
+docker exec -i "$CONTAINER_NAME" fluent-plugin-generate --help
+# testing fluent-debug
+# docker exec -i "$CONTAINER_NAME" fluent-debug <<EOF
+# irb_quit
+# EOF
+docker exec -i "$CONTAINER_NAME" fluent-debug --help

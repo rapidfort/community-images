b3ff77e9df24628f13ea0645e7fe90fa425e2b80
Kanav Phull <kanav@rapidfort.com>
2024-07-05 07:57:51 -0700

    Imrproved overrides files for helm charts
    Signed-off-by: Kanav Phull <kanav@rapidfort.com>

--- community_images/thanos/ironbank/k8s_before.sh
+++ community_images/thanos/ironbank/k8s_before.sh
@@ -13,6 +13,10 @@ echo "Json params for docker compose coverage = $JSON"
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 
+# extracting thanos image details from JSON params to use in prometheus helm chart (which deploys thanos as a sidecar process)
+THANOS_IMAGE_REPO_PATH=$(jq -r '.image_tag_details."thanos-ib".repo_path' < "$JSON_PARAMS")
+THANOS_IMAGE_TAG=$(jq -r '.image_tag_details."thanos-ib".tag' < "$JSON_PARAMS")
+
 RELEASE_NAME="prometheus"
 PROMETHEUS_CHART="oci://registry-1.docker.io/bitnamicharts/prometheus"
 OVERRIDES_FILE="${SCRIPTPATH}/prometheus_chart_overrides.yml"
@@ -22,9 +26,10 @@ kubectl create namespace "${NAMESPACE}"
 
 # Deploying Prometheus Helm Chart
 echo "Deploying Helm chart: $PROMETHEUS_CHART"
-helm upgrade --install "${RELEASE_NAME}" "${PROMETHEUS_CHART}" -n "${NAMESPACE}" --set prometheus.thanos.create=true -f "${OVERRIDES_FILE}"
+helm upgrade --install "${RELEASE_NAME}" "${PROMETHEUS_CHART}" -n "${NAMESPACE}" --set thanos.image.repository="${THANOS_IMAGE_REPO_PATH}" --set thanos.image.tag="${THANOS_IMAGE_TAG}" -f "${OVERRIDES_FILE}"
 
-echo "Helm chart '$PROMETHEUS_CHART' deployed successfully in namespace '$NAMESPACE' with release name '$RELEASE_NAME'."
+echo "Helm chart '$PROMETHEUS_CHART' deployed successfully in namespace '$NAMESPACE' with release name '$RELEASE_NAME'"
+echo "Image used for thanos sidecar container is '${THANOS_IMAGE_REPO_PATH}':'${THANOS_IMAGE_TAG}'"
 
 # Waiting for pods to come up
 echo "Waiting for pods to be ready..."

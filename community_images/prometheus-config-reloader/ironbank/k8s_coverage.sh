9703dab1a31cd7e1f35d4617a0748dab912e2561
Mridul Verma <mridulverma478@gmail.com>
2024-07-17 04:43:46 -0700

    add shellcheck suggestions#3
    Signed-off-by: Mridul Verma <mridulverma478@gmail.com>

--- community_images/prometheus-config-reloader/ironbank/k8s_coverage.sh
+++ community_images/prometheus-config-reloader/ironbank/k8s_coverage.sh
@@ -7,7 +7,7 @@ JSON_PARAMS="$1"
 
 JSON=$(cat "$JSON_PARAMS")
 
-echo "Json params for k8s coverage = "$JSON""
+echo "Json params for k8s coverage = $JSON"
 
 NAMESPACE=$(jq -r '.namespace_name' < "$JSON_PARAMS")
 

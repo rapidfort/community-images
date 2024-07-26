b84fe722fdde6bb63222ec6d343d42931eab618a
pranav <pranav@rapidfort.com>
2024-07-26 07:19:51 -0700

    add coverage scripts
    Signed-off-by: pranav <pranav@rapidfort.com>

---
+++ community_images/gitlab-workhorse/ironbank/test_files/test_workhorse.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Get template from gitlab-workhorse proxy
+curl http://localhost:8181
+
+# find the contents of file1.txt depending on ARCHIVE_PATH
+gitlab-zip-cat
+
+touch /usr/local/bin/gitlab-resize-image
+
+# extract metadata from the archive
+gitlab-zip-metadata test.zip > /dev/null
+
+# metadata from stripped jpg image
+exiftool test_image.jpg
+
+# templating test
+gomplate -f template.tpl
+
+echo "All tests completed successfully!"

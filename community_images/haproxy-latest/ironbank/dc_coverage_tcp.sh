2115367b12dfdeecf253e05520f7fa1a99f740ba
vasumanas <vasumanas@rapidfort.com>
2024-07-26 16:37:32 -0700

    addressed review dog errors
    Signed-off-by: vasumanas <vasumanas@rapidfort.com>

--- community_images/haproxy-latest/ironbank/dc_coverage_tcp.sh
+++ community_images/haproxy-latest/ironbank/dc_coverage_tcp.sh
@@ -21,48 +21,42 @@ CONTAINER2_NAME=haproxy-2
 # deflate mode
 CONTAINER3_NAME=haproxy-3
 
-
 # log for debugging
 docker inspect "${CONTAINER1_NAME}"
 docker inspect "${CONTAINER2_NAME}"
 docker inspect "${CONTAINER3_NAME}"
 
 # finding ports
-docker inspect "${CONTAINER1_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort"
 PORT1=$(docker inspect "${CONTAINER1_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort")
-docker inspect "${CONTAINER2_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort"
 PORT2=$(docker inspect "${CONTAINER2_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort")
-docker inspect "${CONTAINER3_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort"
 PORT3=$(docker inspect "${CONTAINER3_NAME}" | jq -r ".[].NetworkSettings.Ports.\"80/tcp\"[0].HostPort")
 
 # run curl in loop (identity)
-for i in {1..5};
-do 
+for i in {1..5}; do 
     echo "Attempt $i"
     curl http://localhost:"${PORT1}"
 done
 
 # run curl in loop (gzip)
-for i in {1..5};
-do 
+for i in {1..5}; do 
     echo "Attempt $i"
     curl http://localhost:"${PORT2}"
 done
 
 # run curl in loop (deflate)
-for i in {1..5};
-do 
+for i in {1..5}; do 
     echo "Attempt $i"
     curl http://localhost:"${PORT3}"
 done
 
-# Additional tests for version 3.0+
-if [[ "$VERSION" =~ ^3 ]]; then
-    # Test dynamic SSL certificate updates
-    echo 'set ssl cert /etc/ssl/private/haproxy.pem <<\n$(cat /path/to/new/cert.pem)\n' | socat stdio /var/run/haproxy.sock
-    echo 'commit ssl cert /etc/ssl/private/haproxy.pem' | socat stdio /var/run/haproxy.sock
+# Test dynamic SSL certificate updates
+printf "set ssl cert /etc/ssl/private/haproxy.pem <<\n$(cat /path/to/new/cert.pem)\n" | socat stdio /var/run/haproxy.sock
+printf "commit ssl cert /etc/ssl/private/haproxy.pem" | socat stdio /var/run/haproxy.sock
 
-    # Test dynamic server weight adjustment
-    echo "set server be_servers/server1 weight 20" | socat stdio /var/run/haproxy.sock
-    echo "set server be_servers/server2 weight 5" | socat stdio /var/run/haproxy.sock
-fi
+# Test dynamic server weight adjustment
+printf "set server be_servers/server1 weight 20" | socat stdio /var/run/haproxy.sock
+printf "set server be_servers/server2 weight 5" | socat stdio /var/run/haproxy.sock
+
+# Observability Testing (Metrics)
+echo "Fetching metrics"
+curl http://localhost:8404/metrics

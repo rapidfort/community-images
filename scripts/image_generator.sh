230c0225043315681a59f8e7f32a5858626425e6
anmol <anmol@rapidfort.com>
2024-11-05 08:53:14 -0800

    Fixed image gen
    Signed-off-by: anmol <anmol@rapidfort.com>

--- scripts/image_generator.sh
+++ scripts/image_generator.sh
@@ -8,7 +8,7 @@ set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-# pip install jinja-cli PyYAML ruamel.yaml
+pip install jinja-cli PyYAML ruamel.yaml
 
 gen_image_readme()
 {
